e3b664d090fdddbd93e44810dca7eb1b7f64858a
sandreza <andrenogueirasouza@gmail.com>
2021-06-17 14:56:59 -0400

    add more abstract backends

--- src/backends/backends.jl
+++ src/backends/backends.jl
@@ -1,3 +1,5 @@
 abstract type AbstractBackend end
 
 struct ClimateMachineBackend <: AbstractBackend end
+struct ClimateMachineCoreBackend <: AbstractBackend end
+struct OceananigansBackend <: AbstractBackend end
