b29674460c3eeaf1971955db36900c76b710ab00
sandreza <andrenogueirasouza@gmail.com>
2021-06-22 16:35:15 -0400

    add climatemachinecore to backend hooks

--- src/backends/backends.jl
+++ src/backends/backends.jl
@@ -4,6 +4,7 @@ Base.@kwdef struct DiscontinuousGalerkinBackend{𝒜,ℬ} <: AbstractBackend
     grid::𝒜
     numerics::ℬ
 end
+
 Base.@kwdef struct CoreBackend{𝒜,ℬ} <: AbstractBackend
     grid::𝒜
     numerics::ℬ
