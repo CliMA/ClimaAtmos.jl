bb4f5bdcab4cea597a99e8fabe091abbf9a51706
bischtob <tobias.bischoff@pm.me>
2021-08-16 16:14:38 -0700

    refactored hydrostatic model to parcel out bcs

--- src/backends/climacore/ode_problems.jl
+++ src/backends/climacore/ode_problems.jl
@@ -1,3 +1,4 @@
+# Default behavior for this backend
 function create_ode_problem(backend::ClimaCoreBackend, model::AbstractModel, timestepper::AbstractTimestepper)
     function_space = create_function_space(backend, model.domain)
     rhs! = create_rhs(backend, model, function_space)
