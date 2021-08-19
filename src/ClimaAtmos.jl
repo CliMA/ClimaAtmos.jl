379c74cee6599cce2c7b14e9e3b18cebefa36320
Toby Bischoff <12664448+bischtob@users.noreply.github.com>
2021-08-19 16:39:32 -0700

    Global module style bugfix

--- src/ClimaAtmos.jl
+++ src/ClimaAtmos.jl
@@ -1,8 +1,10 @@
 module ClimaAtmos
-    include("Domains/Domains.jl")
-    include("BoundaryConditions/BoundaryConditions.jl")
-    include("Timesteppers/Timesteppers.jl")
-    include("Models/Models.jl")
-    include("Simulations/Simulations.jl")
-    include("Utils/Utils.jl")
-end
+
+include("Domains/Domains.jl")
+include("BoundaryConditions/BoundaryConditions.jl")
+include("Timesteppers/Timesteppers.jl")
+include("Models/Models.jl")
+include("Simulations/Simulations.jl")
+include("Utils/Utils.jl")
+
+end # module
