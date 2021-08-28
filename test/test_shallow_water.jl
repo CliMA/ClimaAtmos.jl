fbd7414e46ff63d8d0f63190e760bcb18411a8b3
bischtob <tobias.bischoff@pm.me>
2021-08-28 14:19:45 -0700

    added shallow water instantiation test

---
+++ test/test_shallow_water.jl
@@ -0,0 +1,44 @@
+using ClimaAtmos.Domains: Plane
+using ClimaAtmos.Models: ShallowWaterModel
+
+function instantiate_shallow_water_model(FT)
+    parameters = (
+        ϵ  = 0.1,  # perturbation size for initial condition
+        l  = 0.5,  # Gaussian width
+        k  = 0.5,  # sinusoidal wavenumber
+        ρ₀ = 1.0,  # reference density
+        c  = 2.0,  
+        g  = 9.8,  # gravitational constant
+        D₄ = 1e-4, # hyperdiffusion coefficient
+    )
+    domain = Plane(
+        FT, 
+        xlim = (0.0, 1.0), 
+        ylim = (0.0, 2.0), 
+        nelements = (3, 2),
+        npolynomial = 16,
+        periodic = (false, false),
+    )
+    swm = ShallowWaterModel(
+        domain = domain,
+        boundary_conditions = nothing,
+        initial_conditions = nothing,
+        parameters = parameters,
+    )
+    check1 = swm.domain == domain
+    check2 = swm.boundary_conditions === nothing
+    check3 = swm.initial_conditions === nothing
+    check4 = swm.parameters == parameters
+
+    return check1 && check2 && check3 && check4
+end
+
+@testset "ShallowWaterModels" begin
+    @info "Testing ClimaAtmos.ShallowWaterModels..."
+
+    @testset "ShallowWaterModels" begin
+        for FT in float_types
+            @test instantiate_shallow_water_model(FT)
+        end
+    end
+end
