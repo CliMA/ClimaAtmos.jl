bd5c388f1a9863eae4bc882e77a472bc65af8840
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-04-11 13:31:25 -0700

    Add pipeline generation script

--- examples/hybrid/cli_options.jl
+++ examples/hybrid/cli_options.jl
@@ -18,3 +18,57 @@ ArgParse.@add_arg_table s begin
 end
 
 parsed_args = ArgParse.parse_args(ARGS, s)
+
+function cli_defaults(s::ArgParse.ArgParseSettings)
+    defaults = Dict()
+    # TODO: Don't use ArgParse internals
+    for arg in s.args_table.fields
+        defaults[arg.dest_name] = arg.default
+    end
+    return defaults
+end
+
+#= Use the job ID for the output folder =#
+function output_directory(
+    s::ArgParse.ArgParseSettings,
+    parsed_args = ArgParse.parse_args(ARGS, s),
+)
+    return job_id_from_parsed_args(s, parsed_args)
+end
+
+"""
+    job_id_from_parsed_args(
+        s::ArgParseSettings,
+        parsed_args = ArgParse.parse_args(ARGS, s)
+    )
+
+Returns a unique name (`String`) given
+ - `s::ArgParse.ArgParseSettings` The arg parse settings
+ - `parsed_args` The parse arguments
+
+The `ArgParseSettings` are used for truncating
+this string based on the default values.
+"""
+job_id_from_parsed_args(s, parsed_args = ArgParse.parse_args(ARGS, s)) =
+    job_id_from_parsed_args(cli_defaults(s), parsed_args)
+
+function job_id_from_parsed_args(defaults::Dict, parsed_args)
+    _parsed_args = deepcopy(parsed_args)
+    s = ""
+    for k in keys(_parsed_args)
+        # Skip defaults to alleviate verbose names
+        defaults[k] == _parsed_args[k] && continue
+
+        if _parsed_args[k] isa String
+            # We don't need keys if the value is a string
+            # (alleviate verbose names)
+            s *= _parsed_args[k]
+        else
+            s *= k * "=" * string(_parsed_args[k])
+        end
+        s *= "_"
+    end
+    s = replace(s, "/" => "_")
+    s = strip(s, '_')
+    return s
+end
