1e0fd12e48ce6b6ce697eeaf9c826ae023d082e8
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-04-15 14:17:36 -0700

    Add .dev/up_deps.jl script

---
+++ .dev/up_deps.jl
@@ -0,0 +1,27 @@
+#=
+A simple script for updating the manifest
+files in all of our environments.
+=#
+
+root = dirname(@__DIR__)
+dirs = (
+    root,
+    joinpath(root, "test"),
+    joinpath(root, "perf"),
+    joinpath(root, "docs"),
+    joinpath(root, "examples"),
+)
+
+cd(root) do
+    for dir in dirs
+        cmd = `$(Base.julia_cmd()) --project=$dir -e 'import Pkg; Pkg.update()'`
+        run(cmd)
+    end
+end
+
+# https://github.com/JuliaLang/Pkg.jl/issues/3014
+for dir in dirs
+    cd(dir) do
+        rm("LocalPreferences.toml"; force = true)
+    end
+end
