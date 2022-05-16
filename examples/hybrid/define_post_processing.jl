5095c68788a617c3ef9703617f4c89377a789674
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-05-15 17:53:23 -0700

    Swap x-y vars in profiles, ribbon -> xerror

--- examples/hybrid/define_post_processing.jl
+++ examples/hybrid/define_post_processing.jl
@@ -42,21 +42,20 @@ function profile_animation(sol, output_dir, fps)
                     sqrt.((vec(ϕ_col) .- ϕ_col_ave) .^ 2 ./ n_columns)
             end
 
-            # TODO: use xribbon when supported: https://github.com/JuliaPlots/Plots.jl/issues/2702
-            # Plots.plot(ϕ_col_ave, z ./ 1000; label = "Mean & Variance", xerror=ϕ_col_std)
-            # Plots.plot!(; ylabel = "z [km]", xlabel = "$var_name", markershape = :circle)
+            # TODO: use xribbon when supported:
+            # https://github.com/JuliaPlots/Plots.jl/issues/2702
 
             Plots.plot(
-                z ./ 1000,
-                ϕ_col_ave;
+                ϕ_col_ave,
+                z ./ 1000;
                 label = "Mean & Std",
                 grid = false,
-                ribbon = ϕ_col_std,
+                xerror = ϕ_col_std,
                 fillalpha = 0.5,
             )
             Plots.plot!(;
-                ylabel = "$var_name",
-                xlabel = "z [km]",
+                ylabel = "z [km]",
+                xlabel = "$var_name",
                 markershape = :circle,
             )
             Plots.title!("$(space_string(var_space))")
