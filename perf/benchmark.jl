fd8cad863dee1728fa01d4dee470fc1680e1f92c
Simon Byrne <simonbyrne@gmail.com>
2022-07-15 10:31:39 -0700

    fix benchmarks

--- perf/benchmark.jl
+++ perf/benchmark.jl
@@ -27,8 +27,8 @@ trials = OrderedCollections.OrderedDict()
 #! format: off
 trials["Wfact"] = get_trial(integrator.f.f1.Wfact, (W, u, p, dt, t), "Wfact");
 trials["linsolve"] = get_trial(integrator.cache.linsolve, (X, W, u), "linsolve");
-trials["implicit_tendency!"] = get_trial(implicit_tendency!, (X, u, p, t), "implicit_tendency!");
-trials["remaining_tendency!"] = get_trial(remaining_tendency!, (X, u, p, t), "remaining_tendency!");
+trials["implicit_tendency!"] = get_trial(integrator.f.f1, (X, u, p, t), "implicit_tendency!");
+trials["remaining_tendency!"] = get_trial(integrator.f.f2, (X, u, p, t), "remaining_tendency!");
 trials["default_remaining_tendency!"] = get_trial(default_remaining_tendency!, (X, u, p, t), "default_remaining_tendency!");
 trials["hyperdiffusion_tendency!"] = get_trial(hyperdiffusion_tendency!, (X, u, p, t), "hyperdiffusion_tendency!");
 trials["step!"] = get_trial(ODE.step!, (integrator, ), "step!");
