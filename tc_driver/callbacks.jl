c4bd11fe86b71e657ed7368a529db0c1f5531d39
yairchn <yairchn@caltech.edu>
2022-07-18 11:05:59 -0700

    add turb_conv_affect_filter in callbacks

--- tc_driver/callbacks.jl
+++ tc_driver/callbacks.jl
@@ -113,26 +113,6 @@ function affect_io!(integrator)
     ODE.u_modified!(integrator, false) # We're legitamately not mutating `u` (the state vector)
 end
 
-function affect_filter!(integrator)
-    UnPack.@unpack edmf, param_set, aux, case, surf_params = integrator.p
-    t = integrator.t
-    prog = integrator.u
-    prog = integrator.u
-
-    for inds in TC.iterate_columns(prog.cent)
-        state = TC.column_prog_aux(prog, aux, inds...)
-        grid = TC.Grid(state)
-        surf = get_surface(surf_params, grid, state, t, param_set)
-        TC.affect_filter!(edmf, grid, state, param_set, surf, t)
-    end
-
-    # We're lying to OrdinaryDiffEq.jl, in order to avoid
-    # paying for an additional `∑tendencies!` call, which is required
-    # to support supplying a continuous representation of the
-    # solution.
-    ODE.u_modified!(integrator, false)
-end
-
 function adaptive_dt!(integrator)
     UnPack.@unpack edmf, TS, dt_min = integrator.p
     TS.dt = min(TS.dt_max, max(TS.dt_max_edmf, dt_min))
