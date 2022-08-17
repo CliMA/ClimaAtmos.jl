0355e685931585e6b451caf96b4b9d6d3ab7ce97
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-08-17 16:07:13 -0700

    Optimize some pow calls

--- examples/hybrid/utilities.jl
+++ examples/hybrid/utilities.jl
@@ -78,3 +78,7 @@ function verify_callbacks(t)
         )
     end
 end
+
+# we may be hitting a slow path:
+# https://stackoverflow.com/questions/14687665/very-slow-stdpow-for-bases-very-close-to-1
+fast_pow(x::FT, y::FT) where {FT <: AbstractFloat} = exp(y * log(x))
