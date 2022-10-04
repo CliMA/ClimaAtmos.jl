b08c4274cb8744485ebce076712296e856ff1a69
Simon Byrne <simonbyrne@gmail.com>
2022-10-04 14:48:05 -0700

    fix logging and comms logic

---
+++ examples/hybrid/comms.jl
@@ -0,0 +1,25 @@
+import ClimaComms, Logging
+
+if haskey(ENV, "CLIMACORE_DISTRIBUTED")
+    if ENV["CLIMACORE_DISTRIBUTED"] == "MPI"
+        import ClimaCommsMPI
+        const comms_ctx = ClimaCommsMPI.MPICommsContext()
+    else
+        error("ENV[\"CLIMACORE_DISTRIBUTED\"] only supports the \"MPI\" option")
+    end
+else
+    const comms_ctx = ClimaComms.SingletonCommsContext()
+end
+ClimaComms.init(comms_ctx)
+
+if ClimaComms.iamroot(comms_ctx)
+    Logging.global_logger(Logging.ConsoleLogger(stderr, Logging.Info))
+else
+    Logging.global_logger(Logging.NullLogger())
+end
+
+if comms_ctx isa ClimaComms.SingletonCommsContext
+    @info "Setting up single-process run"
+else
+    @info "Setting up distributed run" nprocs = ClimaComms.nprocs(comms_ctx)
+end
