11c1bbcea32df6f63d99ff446b69c181bdf4ac7a
Simon Byrne <simonbyrne@gmail.com>
2022-10-05 09:53:50 -0700

    only define comms_ctx if not already defined

--- examples/hybrid/comms.jl
+++ examples/hybrid/comms.jl
@@ -1,25 +1,29 @@
 import ClimaComms, Logging
 
-if haskey(ENV, "CLIMACORE_DISTRIBUTED")
-    if ENV["CLIMACORE_DISTRIBUTED"] == "MPI"
-        import ClimaCommsMPI
-        const comms_ctx = ClimaCommsMPI.MPICommsContext()
+if !@isdefined comms_ctx # for compatibility with coupler
+    if haskey(ENV, "CLIMACORE_DISTRIBUTED")
+        if ENV["CLIMACORE_DISTRIBUTED"] == "MPI"
+            import ClimaCommsMPI
+            const comms_ctx = ClimaCommsMPI.MPICommsContext()
+        else
+            error(
+                "ENV[\"CLIMACORE_DISTRIBUTED\"] only supports the \"MPI\" option",
+            )
+        end
     else
-        error("ENV[\"CLIMACORE_DISTRIBUTED\"] only supports the \"MPI\" option")
+        const comms_ctx = ClimaComms.SingletonCommsContext()
     end
-else
-    const comms_ctx = ClimaComms.SingletonCommsContext()
-end
-ClimaComms.init(comms_ctx)
+    ClimaComms.init(comms_ctx)
 
-if ClimaComms.iamroot(comms_ctx)
-    Logging.global_logger(Logging.ConsoleLogger(stderr, Logging.Info))
-else
-    Logging.global_logger(Logging.NullLogger())
+    if ClimaComms.iamroot(comms_ctx)
+        Logging.global_logger(Logging.ConsoleLogger(stderr, Logging.Info))
+    else
+        Logging.global_logger(Logging.NullLogger())
+    end
 end
-
 if comms_ctx isa ClimaComms.SingletonCommsContext
-    @info "Setting up single-process run"
+    @info "Setting up single-process ClimaAtmos run"
 else
-    @info "Setting up distributed run" nprocs = ClimaComms.nprocs(comms_ctx)
+    @info "Setting up distributed ClimaAtmos run" nprocs =
+        ClimaComms.nprocs(comms_ctx)
 end
