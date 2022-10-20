e8765931c8b84145192cdd1cad5734659e4a5183
asraero <asridhar@caltech.edu>
2022-10-20 10:45:17 -0700

    Enforce u(^3) bc

--- examples/hybrid/staggered_nonhydrostatic_model.jl
+++ examples/hybrid/staggered_nonhydrostatic_model.jl
@@ -401,7 +401,7 @@ function precomputed_quantities!(Y, p, t, colidx)
     ᵇᶜuₕ = Fields.level(ᶠinterp.(ᶜuₕ[colidx]), ClimaCore.Utilities.half)
     @. ᵇᶜu₃ = Geometry.Covariant3Vector(-1 * g¹³ / g³³ * ᵇᶜuₕ.components.data.:1)
     enforce_boundary = Operators.SetBoundaryOperator(bottom = Operators.SetValue(ᵇᶜu₃))
-    @. ᶠw = apply_boundary_w(fw)
+    @. ᶠw = enforce_boundary(fw)
     
 
     @. ᶜuvw[colidx] = C123(ᶜuₕ[colidx]) + C123(ᶜinterp(ᶠw[colidx]))
