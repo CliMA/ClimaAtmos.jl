8917d09ed985c920d643f21783f7b9f3ff6f8c2b
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-10-28 21:17:28 -0700

    Add parallel remap script and job
    Activate examples dir
    
    Fix stringcat, add debug for path
    
    Use correct pipeline
    
    Fix relative path
    
    Fix more paths
    
    Avoid ArgParse in Distributed logic
    
    Use out_dir for base path
    
    Make temp path in parallel mode
    
    Try spawnat all
    
    Use interpolation to implicitly transfer data

---
+++ examples/hybrid/sphere/remap_pipeline_parallel.jl
@@ -0,0 +1,31 @@
+#=
+Run with, for example:
+```
+julia -p 4 remap_pipeline_parallel.jl
+```
+=#
+using Distributed
+@everywhere using Pkg
+@everywhere begin
+    ca_dir = joinpath(@__DIR__, "..", "..", "..")
+    Pkg.activate(joinpath(ca_dir, "examples"))
+    include(
+        joinpath(ca_dir, "examples", "hybrid", "sphere", "remap_pipeline.jl"),
+    )
+end
+(; remap_tmpdir, data_files, out_dir, nlat, nlon) = get_params()
+
+weightfile_0 = create_weightfile(data_files[1], remap_tmpdir, nlat, nlon)
+
+@everywhere begin
+    weightfile = $weightfile_0
+    remap_tmpdir = $remap_tmpdir
+    out_dir = $out_dir
+    nlat = $nlat
+    nlon = $nlon
+end
+pmap(data_files) do data_file
+    @info "Processor $(myid()) proccessing: $data_file"
+    remap2latlon(data_file, out_dir, remap_tmpdir, weightfile, nlat, nlon)
+end
+rm(remap_tmpdir; recursive = true)
