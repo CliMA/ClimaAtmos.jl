bafeb62f2ae9d5b3184c9f86d35db4c6a3acc341
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-11-10 13:21:02 -0800

    Fix access to temperature in large-scale advection

--- src/tendencies/forcing/large_scale_advection.jl
+++ src/tendencies/forcing/large_scale_advection.jl
@@ -38,10 +38,6 @@ function large_scale_advection_tendency!(
     @. ᶜdTdt_hadv = prof_dTdt(thermo_params, ᶜts, t, z)
     @. ᶜdqtdt_hadv = prof_dqtdt(thermo_params, ᶜts, t, z)
 
-    # TODO: unify dycore and edmf temperature
-    ᶜT = p.ᶜT[colidx]
-    # ᶜT = p.edmf_cache.cent.ᶜT[colidx]
-
     T_0 = TD.Parameters.T_0(thermo_params)
     Lv_0 = TD.Parameters.LH_v0(thermo_params)
     cv_v = TD.Parameters.cv_v(thermo_params)
@@ -53,7 +49,10 @@ function large_scale_advection_tendency!(
     @. Yₜ.c.ρe_tot[colidx] +=
         Y.c.ρ[colidx] * (
             TD.cp_m(thermo_params, ᶜts) * ᶜdTdt_hadv +
-            (cv_v * (ᶜT - T_0) + Lv_0 - R_v * T_0) * ᶜdqtdt_hadv
+            (
+                cv_v * (TD.air_temperature(thermo_params, ᶜts) - T_0) + Lv_0 -
+                R_v * T_0
+            ) * ᶜdqtdt_hadv
         )
     return nothing
 end
