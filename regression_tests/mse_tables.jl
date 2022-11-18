c9b63b180d4d1c37ffdd352995ad354946cdbe55
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-11-17 22:08:51 -0800

    Add mse entry and increment ref counter

--- regression_tests/mse_tables.jl
+++ regression_tests/mse_tables.jl
@@ -69,6 +69,18 @@ all_best_mse["edmf_bomex"][(:c, :turbconv, :up, 1, :ρaθ_liq_ice)] = 0.0
 all_best_mse["edmf_bomex"][(:c, :turbconv, :up, 1, :ρaq_tot)] = 0.0
 all_best_mse["edmf_bomex"][(:f, :turbconv, :up, 1, :w, :components, :data, 1)] = 0.0
 #
+all_best_mse["edmf_life_cycle_tan2018"] = OrderedCollections.OrderedDict()
+all_best_mse["edmf_life_cycle_tan2018"][(:c, :ρ)] = 0.0
+all_best_mse["edmf_life_cycle_tan2018"][(:c, :ρe_tot)] = 0.0
+all_best_mse["edmf_life_cycle_tan2018"][(:c, :uₕ, :components, :data, 1)] = 0.0
+all_best_mse["edmf_life_cycle_tan2018"][(:c, :uₕ, :components, :data, 2)] = 0.0
+all_best_mse["edmf_life_cycle_tan2018"][(:c, :ρq_tot)] = 0.0
+all_best_mse["edmf_life_cycle_tan2018"][(:c, :turbconv, :en, :ρatke)] = 0.0
+all_best_mse["edmf_life_cycle_tan2018"][(:c, :turbconv, :up, 1, :ρarea)] = 0.0
+all_best_mse["edmf_life_cycle_tan2018"][(:c, :turbconv, :up, 1, :ρaθ_liq_ice)] = 0.0
+all_best_mse["edmf_life_cycle_tan2018"][(:c, :turbconv, :up, 1, :ρaq_tot)] = 0.0
+all_best_mse["edmf_life_cycle_tan2018"][(:f, :turbconv, :up, 1, :w, :components, :data, 1)] = 0.0
+#
 all_best_mse["edmf_arm_sgp"] = OrderedCollections.OrderedDict()
 all_best_mse["edmf_arm_sgp"][(:c, :ρ)] = 0.0
 all_best_mse["edmf_arm_sgp"][(:c, :ρe_tot)] = 0.0
