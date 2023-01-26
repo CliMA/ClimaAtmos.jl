608f643f2ca955efdd02dfb632aa807f75680b55
Akshay Sridhar <asridhar@caltech.edu>
2023-01-26 15:53:07 -0800

    +format

--- src/tendencies/advection.jl
+++ src/tendencies/advection.jl
@@ -91,8 +91,10 @@ function explicit_vertical_advection_tendency!(Yₜ, Y, p, t, colidx)
 
     # Momentum conservation
     @. ᶠω¹²[colidx] += ᶠcurlᵥ(ᶜuₕ[colidx])
-    @. ᶠu¹²[colidx] =
-        Geometry.project(Geometry.Contravariant12Axis(), ᶠwinterp(ᶜρ[colidx] * J, ᶜuvw[colidx]))
+    @. ᶠu¹²[colidx] = Geometry.project(
+        Geometry.Contravariant12Axis(),
+        ᶠwinterp(ᶜρ[colidx] * J, ᶜuvw[colidx]),
+    )
     @. ᶠu³[colidx] = Geometry.project(
         Geometry.Contravariant3Axis(),
         C123(ᶠinterp(ᶜuₕ[colidx])) + C123(ᶠw[colidx]),
