874e721b246d1c33e4356d83f91d167b0d471413
Zhaoyi Shen <11598433+szy21@users.noreply.github.com>
2023-02-24 15:21:56 -0800

    wip

--- examples/hybrid/idealized_convection.jl
+++ examples/hybrid/idealized_convection.jl
@@ -50,9 +50,9 @@ function convection_tendency!(
     
     cape_calculation()
     if cape >= 0
-        set_reference_profiles!()
-        pq_calculation()
-        pt_calculation()
+        set_reference_profiles!(Y, p, Tp, zLZB, rp, deltaq, deltaT, qref, Tref, colidx)
+        pq_calculation!(Y, p, zLZB, qref, deltaq, Pq, colidx)
+        pt_calculation!(Y, p, )
         if (pq > 0) & (pt > 0)
             do_deep_convection()
         elseif (pt > 0)
@@ -78,18 +78,63 @@ end
 function cape_calculation()
 end
 
-function set_reference_profiles!(Y, p, Tp, kLZB, rp, deltaq, deltaT, qref, Tref, colidx)
-    (; rh_bm, ᶜp, params) = p
-    R_d = CAP.R_d(params)
-    R_v = CAP.R_v(params)
-    q_tot
-    Tref .= Tp
-    eref = @. rh_bm * ᶜp * rp / (rp + R_d / R_v)
-    rp = @. R_d * eref / R_v / (ᶜp - eref)
-    qref = @. rp / (FT(1) + rp)
-    ᶜα = @. ifelse(ᶜz < zLZB, FT(1), FT(0))
-    @. Tref = ᶜα * Tref + (FT(1) - ᶜα) * ᶜT
-    @. qref = ᶜα * qref + (FT(1) - ᶜα) * q_tot
-    @. deltaT = ᶜα * deltaT
-    @. deltaq = ᶜα * deltaq
+function set_reference_profiles!(Y, p, Tp, zLZB, rp, deltaq, deltaT, qref, Tref, colidx)
+    FT = Spaces.undertype(axes(Y.c))
+    (; rh_bm, ᶜp, ᶜts, ᶜT, params) = p
+    R_d = FT(CAP.R_d(params))
+    R_v = FT(CAP.R_v(params))
+    thermo_params = CAP.thermodynamics_params(params)
+    @. ᶜT[colidx] = TD.air_temperature(thermo_params, ᶜts[colidx])
+
+    Tref[colidx] .= Tp[colidx]
+    eref = @. rh_bm * ᶜp[colidx] * rp[colidx] / (rp[colidx] + R_d / R_v)
+    @. rp[colidx] = R_d * eref / R_v / (ᶜp[colidx] - eref)
+    @. qref[colidx] = rp[colidx] / (FT(1) + rp[colidx])
+    ᶜα = @. ifelse(ᶜz[colidx] < zLZB[colidx], FT(1), FT(0))
+    @. Tref[colidx] = ᶜα * Tref[colidx] + (FT(1) - ᶜα) * ᶜT[colidx]
+    #@. Tref = ifelse(ᶜz < zLZB, Tref, ᶜT)
+    @. qref[colidx] = ᶜα * qref[colidx] + (FT(1) - ᶜα) * (Y.c.ρq_tot[colidx] / Y.c.ρ[colidx])
+    @. deltaT[colidx] = ᶜα * deltaT[colidx]
+    @. deltaq[colidx] = ᶜα * deltaq[colidx]
 end
+
+```
+   Calculate the precipitation rate Pq
+```
+function pq_calculation!(Y, p, zLZB, qref, deltaq, Pq, colidx)
+    (; τ_bm, params) = p
+    (; dt) = p.simulation
+    @. deltaq[colidx] = -(Y.c.ρq_tot[colidx] / Y.c.ρ[colidx] - qref[colidx]) * dt / τ_bm 
+    @. deltaq[colidx] = ifelse(ᶜz < zLZB, deltaq[colidx], FT(0))
+    Operators.column_integral_definite!(
+        Pq[colidx],
+        -deltaq[colidx] * Y.c.ρ[colidx],
+    )
+end
+
+```
+   Calculate the humidity change that would be necessary
+   to balance temperature change by latent heat release
+```
+function pt_calculation!(Y, p, zLZB, Tref, deltaT, Pt, colidx)
+    FT = Spaces.undertype(axes(Y.c))
+
+    (; τ_bm, ᶜts, ᶜT, params) = p
+    (; dt) = p.simulation
+    
+    thermo_params = CAP.thermodynamics_params(params)
+    cp_m = @. TD.cp_m(thermo_params, ᶜts[colidx])
+    L_v = @. TD.latent_heat_vapor(thermo_params, ᶜts[colidx])
+    @. ᶜT[colidx] = TD.air_temperature(thermo_params, ᶜts[colidx])
+
+    @. deltaT[colidx] = -(ᶜT[colidx] - Tref[colidx]) * dt / τ_bm 
+    @. deltaT[colidx] = ifelse(ᶜz < zLZB, deltaT[colidx], FT(0))
+    Operators.column_integral_definite!(
+        Pt[colidx],
+        deltaT[colidx] * cp_m / L_v * Y.c.ρ[colidx],
+    )
+end
+
+function do_deep_convection!()
+end
+
