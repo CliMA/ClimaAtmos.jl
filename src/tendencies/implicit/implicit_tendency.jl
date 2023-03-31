af2dcef66b620aac3fbc00675dce7c93f1c4ec59
Valeria Barra <valeriabarra21@gmail.com>
2023-03-31 14:11:02 -0700

    Add support to upwind density

--- src/tendencies/implicit/implicit_tendency.jl
+++ src/tendencies/implicit/implicit_tendency.jl
@@ -89,7 +89,7 @@ end
 function implicit_vertical_advection_tendency!(Yₜ, Y, p, t, colidx)
     ᶜρ = Y.c.ρ
     (; ᶠgradᵥ_ᶜΦ, ᶜp, ᶠu³, ᶜρ_ref, ᶜp_ref) = p
-    (; energy_upwinding, tracer_upwinding) = p
+    (; energy_upwinding, tracer_upwinding, density_upwinding) = p
     (; ᶠgradᵥ, ᶠinterp) = p.operators
 
     vertical_transport!(
@@ -98,7 +98,7 @@ function implicit_vertical_advection_tendency!(Yₜ, Y, p, t, colidx)
         ᶜρ[colidx],
         ᶜρ[colidx],
         p,
-        Val(:none),
+        density_upwinding,
     )
 
     if :ρθ in propertynames(Y.c)
