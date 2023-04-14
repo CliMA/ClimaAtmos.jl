ab8ef69f7aaf14b8d9e8e01535c7158cba59b4fc
Zhaoyi Shen <11598433+szy21@users.noreply.github.com>
2023-04-13 23:39:34 -0700

    add filter

--- src/TurbulenceConvection/EDMF_functions.jl
+++ src/TurbulenceConvection/EDMF_functions.jl
@@ -659,6 +659,15 @@ function filter_updraft_vars(
             ),
             prog_up[i].ρaq_tot,
         )
+        @. prog_up[i].ρah_tot = ifelse(
+            z > Δz1 / 2,
+            ifelse(
+                prog_up[i].ρarea / ρ_c < a_min,
+                0,
+                prog_up[i].ρah_tot,
+            ),
+            prog_up[i].ρah_tot,
+        )
     end
 
     Ic = CCO.InterpolateF2C()
