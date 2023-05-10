2454975c98acd3796399ec80890222dc9d4bc32a
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-05-09 23:00:24 -0700

    Fixes

--- artifacts/download_artifacts.jl
+++ artifacts/download_artifacts.jl
@@ -1,4 +1,5 @@
 include(joinpath(@__DIR__, "artifact_funcs.jl"))
+import Insolation
 
 # Trigger download if data doesn't exist locally
 function trigger_download(lazy_download = true)
@@ -8,6 +9,7 @@ function trigger_download(lazy_download = true)
     @info "MiMA convective gravity wave path:`$(mima_gwf_path())`"
     @info "ETOPO1 arc-minute relief model:`$(topo_elev_dataset_path())`"
     @info "GFDL OGWD test data:`$(gfdl_ogw_data_path())`"
+    @info "Insolation orbital parameters:`$(Insolation.orbital_parameters_dataset_path())`"
     return nothing
 end
 trigger_download()
