ef9eb7fc5d5657d1a0707e841d299ea5b5932a77
Tobiwan <tobiwan@Tobiwans-MacBook-Air.local>
2023-05-24 16:48:35 -0700

    renaming folders in src

--- src/ClimaAtmos.jl
+++ src/ClimaAtmos.jl
@@ -16,10 +16,12 @@ include(joinpath("utils", "classify_case.jl"))
 include(joinpath("utils", "topography_helper.jl"))
 include(joinpath("utils", "variable_manipulations.jl"))
 
-include(joinpath("parameterizations", "radiation", "radiation_utilities.jl"))
-include(joinpath("parameterizations", "radiation", "RRTMGPInterface.jl"))
+include(
+    joinpath("parameterized_tendencies", "radiation", "radiation_utilities.jl"),
+)
+include(joinpath("parameterized_tendencies", "radiation", "RRTMGPInterface.jl"))
 import .RRTMGPInterface as RRTMGPI
-include(joinpath("parameterizations", "radiation", "radiation.jl"))
+include(joinpath("parameterized_tendencies", "radiation", "radiation.jl"))
 
 include("TurbulenceConvection/TurbulenceConvection.jl")
 import .TurbulenceConvection as TC
@@ -28,52 +30,60 @@ include("precomputed_quantities.jl")
 
 include(joinpath("InitialConditions", "InitialConditions.jl"))
 include(
-    joinpath("parameterizations", "TurbulenceConvection", "tc_functions.jl"),
+    joinpath(
+        "parameterized_tendencies",
+        "TurbulenceConvection",
+        "tc_functions.jl",
+    ),
 )
 include(joinpath("SurfaceStates", "SurfaceStates.jl"))
 include(joinpath("utils", "discrete_hydrostatic_balance.jl"))
 
-include(joinpath("tendencies", "pressure_work.jl"))
-include(joinpath("tendencies", "zero_velocity.jl"))
+include(joinpath("prognostic_equations", "pressure_work.jl"))
+include(joinpath("prognostic_equations", "zero_velocity.jl"))
 
-include(joinpath("tendencies", "implicit", "wfact.jl"))
-include(joinpath("tendencies", "implicit", "schur_complement_W.jl"))
-include(joinpath("tendencies", "implicit", "implicit_tendency.jl"))
+include(joinpath("prognostic_equations", "implicit", "wfact.jl"))
+include(joinpath("prognostic_equations", "implicit", "schur_complement_W.jl"))
+include(joinpath("prognostic_equations", "implicit", "implicit_tendency.jl"))
 
-include(joinpath("tendencies", "remaining_tendency.jl"))
-include(joinpath("tendencies", "forcing", "large_scale_advection.jl")) # TODO: should this be in tendencies/?
-include(joinpath("tendencies", "forcing", "subsidence.jl"))
-include(joinpath("parameterizations", "held_suarez", "held_suarez.jl"))
+include(joinpath("prognostic_equations", "remaining_tendency.jl"))
+include(joinpath("prognostic_equations", "forcing", "large_scale_advection.jl")) # TODO: should this be in tendencies/?
+include(joinpath("prognostic_equations", "forcing", "subsidence.jl"))
+include(joinpath("parameterized_tendencies", "held_suarez", "held_suarez.jl"))
 
 include(
     joinpath(
-        "parameterizations",
+        "parameterized_tendencies",
         "gravity_wave_drag",
         "non_orographic_gravity_wave.jl",
     ),
 )
 include(
     joinpath(
-        "parameterizations",
+        "parameterized_tendencies",
         "gravity_wave_drag",
         "orographic_gravity_wave_helper.jl",
     ),
 )
 include(
     joinpath(
-        "parameterizations",
+        "parameterized_tendencies",
         "gravity_wave_drag",
         "orographic_gravity_wave.jl",
     ),
 )
-include(joinpath("tendencies", "hyperdiffusion.jl"))
-include(joinpath("tendencies", "edmf_coriolis.jl"))
-include(joinpath("tendencies", "edmfx_closures.jl"))
-include(joinpath("parameterizations", "microphysics", "precipitation.jl"))
-include(joinpath("tendencies", "vertical_diffusion_boundary_layer.jl"))
-include(joinpath("parameterizations", "sponge", "rayleigh_sponge.jl"))
-include(joinpath("parameterizations", "sponge", "viscous_sponge.jl"))
-include(joinpath("tendencies", "advection.jl"))
+include(joinpath("prognostic_equations", "hyperdiffusion.jl"))
+include(joinpath("prognostic_equations", "edmf_coriolis.jl"))
+include(joinpath("prognostic_equations", "edmfx_closures.jl"))
+include(
+    joinpath("parameterized_tendencies", "microphysics", "precipitation.jl"),
+)
+include(
+    joinpath("prognostic_equations", "vertical_diffusion_boundary_layer.jl"),
+)
+include(joinpath("parameterized_tendencies", "sponge", "rayleigh_sponge.jl"))
+include(joinpath("parameterized_tendencies", "sponge", "viscous_sponge.jl"))
+include(joinpath("prognostic_equations", "advection.jl"))
 include(joinpath("dycore_equations", "sgs_flux_tendencies.jl"))
 
 include("staggered_nonhydrostatic_model.jl")
