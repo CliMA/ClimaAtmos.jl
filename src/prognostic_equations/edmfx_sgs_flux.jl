4d949afc2e931b7a177a58d7e3bd71d82c4db076
szy21 <11598433+szy21@users.noreply.github.com>
2023-06-08 22:27:53 -0700

    modify vertical transport

--- src/prognostic_equations/edmfx_sgs_flux.jl
+++ src/prognostic_equations/edmfx_sgs_flux.jl
@@ -15,7 +15,6 @@ function edmfx_sgs_flux_tendency!(Yₜ, Y, p, t, colidx, turbconv_model::EDMFX)
     ᶜJ = Fields.local_geometry_field(Y.c).J
 
     thermo_params = CAP.thermodynamics_params(params)
-    ρe_tot_tendency = ρq_tot_tendency = p.ᶜtemp_scalar[colidx]
     if p.atmos.edmfx_sgs_flux
         ᶠu³_diff_colidx = p.ᶠtemp_CT3[colidx]
         ᶜh_tot_diff_colidx = ᶜq_tot_diff_colidx = p.ᶜtemp_scalar[colidx]
@@ -23,7 +22,7 @@ function edmfx_sgs_flux_tendency!(Yₜ, Y, p, t, colidx, turbconv_model::EDMFX)
             @. ᶠu³_diff_colidx = ᶠu³ʲs.:($$j)[colidx] - ᶠu³[colidx]
             @. ᶜh_tot_diff_colidx = ᶜh_totʲs.:($$j)[colidx] - ᶜh_tot[colidx]
             vertical_transport!(
-                ρe_tot_tendency,
+                Yₜ.c.ρe_tot[colidx],
                 ᶜJ[colidx],
                 Y.c.sgsʲs.:($j).ρa[colidx],
                 ᶠu³_diff_colidx,
@@ -31,7 +30,6 @@ function edmfx_sgs_flux_tendency!(Yₜ, Y, p, t, colidx, turbconv_model::EDMFX)
                 dt,
                 edmfx_upwinding,
             )
-            @. Yₜ.c.ρe_tot[colidx] += ρe_tot_tendency
         end
         @. ᶠu³_diff_colidx = ᶠu³⁰[colidx] - ᶠu³[colidx]
         @. ᶜh_tot_diff_colidx =
@@ -41,7 +39,7 @@ function edmfx_sgs_flux_tendency!(Yₜ, Y, p, t, colidx, turbconv_model::EDMFX)
                 ᶜspecific⁰.e_tot[colidx],
             ) - ᶜh_tot[colidx]
         vertical_transport!(
-            ρe_tot_tendency,
+            Yₜ.c.ρe_tot[colidx],
             ᶜJ[colidx],
             ᶜρa⁰[colidx],
             ᶠu³_diff_colidx,
@@ -49,7 +47,6 @@ function edmfx_sgs_flux_tendency!(Yₜ, Y, p, t, colidx, turbconv_model::EDMFX)
             dt,
             edmfx_upwinding,
         )
-        @. Yₜ.c.ρe_tot[colidx] += ρe_tot_tendency
 
         if !(p.atmos.moisture_model isa DryModel)
             for j in 1:n
@@ -57,7 +54,7 @@ function edmfx_sgs_flux_tendency!(Yₜ, Y, p, t, colidx, turbconv_model::EDMFX)
                 @. ᶜq_tot_diff_colidx =
                     ᶜspecificʲs.:($$j).q_tot[colidx] - ᶜspecific.q_tot[colidx]
                 vertical_transport!(
-                    ρq_tot_tendency,
+                    Yₜ.c.ρq_tot[colidx],
                     ᶜJ[colidx],
                     Y.c.sgsʲs.:($j).ρa[colidx],
                     ᶠu³_diff_colidx,
@@ -65,13 +62,12 @@ function edmfx_sgs_flux_tendency!(Yₜ, Y, p, t, colidx, turbconv_model::EDMFX)
                     dt,
                     edmfx_upwinding,
                 )
-                @. Yₜ.c.ρq_tot[colidx] += ρq_tot_tendency
             end
             @. ᶠu³_diff_colidx = ᶠu³⁰[colidx] - ᶠu³[colidx]
             @. ᶜq_tot_diff_colidx =
                 ᶜspecific⁰.q_tot[colidx] - ᶜspecific.q_tot[colidx]
             vertical_transport!(
-                ρq_tot_tendency,
+                Yₜ.c.ρq_tot[colidx],
                 ᶜJ[colidx],
                 ᶜρa⁰[colidx],
                 ᶠu³_diff_colidx,
@@ -79,7 +75,6 @@ function edmfx_sgs_flux_tendency!(Yₜ, Y, p, t, colidx, turbconv_model::EDMFX)
                 dt,
                 edmfx_upwinding,
             )
-            @. Yₜ.c.ρq_tot[colidx] += ρq_tot_tendency
         end
     end
 
