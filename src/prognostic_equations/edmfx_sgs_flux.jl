419fbf580076581bd718a7cb0bc7e1b0a58ad53a
Zhaoyi Shen <11598433+szy21@users.noreply.github.com>
2023-06-21 14:14:21 -0700

    add subgrid scale mass flux for diagnostic edmf

--- src/prognostic_equations/edmfx_sgs_flux.jl
+++ src/prognostic_equations/edmfx_sgs_flux.jl
@@ -84,3 +84,61 @@ function edmfx_sgs_flux_tendency!(Yₜ, Y, p, t, colidx, turbconv_model::EDMFX)
 
     return nothing
 end
+
+function edmfx_sgs_flux_tendency!(
+    Yₜ,
+    Y,
+    p,
+    t,
+    colidx,
+    turbconv_model::DiagnosticEDMFX,
+)
+
+    n = n_mass_flux_subdomains(turbconv_model)
+    (; edmfx_upwinding) = p
+    (; ᶠu³, ᶜh_tot, ᶜspecific) = p
+    (; ᶜρaʲs, ᶠu³ʲs, ᶜh_totʲs, ᶜq_totʲs) = p
+    (; dt) = p.simulation
+    ᶜJ = Fields.local_geometry_field(Y.c).J
+
+    if p.atmos.edmfx_sgs_flux
+        ᶠu³_diff_colidx = p.ᶠtemp_CT3[colidx]
+        ᶜh_tot_diff_colidx = ᶜq_tot_diff_colidx = p.ᶜtemp_scalar[colidx]
+        for j in 1:n
+            @. ᶠu³_diff_colidx = ᶠu³ʲs.:($$j)[colidx] - ᶠu³[colidx]
+            @. ᶜh_tot_diff_colidx = ᶜh_totʲs.:($$j)[colidx] - ᶜh_tot[colidx]
+            vertical_transport!(
+                Yₜ.c.ρe_tot[colidx],
+                ᶜJ[colidx],
+                ᶜρaʲs.:($j)[colidx],
+                ᶠu³_diff_colidx,
+                ᶜh_tot_diff_colidx,
+                dt,
+                edmfx_upwinding,
+            )
+        end
+
+        if !(p.atmos.moisture_model isa DryModel)
+            for j in 1:n
+                @. ᶠu³_diff_colidx = ᶠu³ʲs.:($$j)[colidx] - ᶠu³[colidx]
+                @. ᶜq_tot_diff_colidx =
+                    ᶜq_totʲs.:($$j)[colidx] - ᶜspecific.q_tot[colidx]
+                vertical_transport!(
+                    Yₜ.c.ρq_tot[colidx],
+                    ᶜJ[colidx],
+                    ᶜρaʲs.:($j)[colidx],
+                    ᶠu³_diff_colidx,
+                    ᶜq_tot_diff_colidx,
+                    dt,
+                    edmfx_upwinding,
+                )
+            end
+        end
+    end
+
+    # TODO: Add momentum flux
+
+    # TODO: Add tracer flux
+
+    return nothing
+end
