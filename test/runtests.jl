dbdc8c571f7802e8d04eb2857f926ae5608dfddf
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-07-14 14:50:43 -0700

    Fix leaky tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
 using SafeTestsets
 using Test
 
-@safetestset "Aqua" begin
-    @time include("utilities.jl")
-    @time include("parameter_tests.jl")
-    @time include("aqua.jl")
-end
+#! format: off
+@safetestset "Aqua" begin @time include("aqua.jl") end
+@safetestset "Utilities" begin @time include("utilities.jl") end
+@safetestset "Parameter tests" begin @time include("parameter_tests.jl") end
+#! format: on
 
 nothing
