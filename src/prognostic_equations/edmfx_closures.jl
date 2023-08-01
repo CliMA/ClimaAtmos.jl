f55690ee8a58ae27c396ba1f3bceac57cefaec18
Zhaoyi Shen <11598433+szy21@users.noreply.github.com>
2023-07-31 22:43:11 -0700

    limit detrainment

--- src/prognostic_equations/edmfx_closures.jl
+++ src/prognostic_equations/edmfx_closures.jl
@@ -184,7 +184,7 @@ function pi_groups_entr_detr(
         # (kinematic, bubble, Bomex)
         # and/or we can calibrate things in ClimaAtmos
         entr = max(0, min(entr_coeff * ᶜwʲ / ᶜz, 1 / dt))
-        detr = max(0, detr_coeff * ᶜwʲ)
+        detr = max(0, min(detr_coeff * ᶜwʲ, 1 / dt))
 
         return (; entr, detr)
     end
