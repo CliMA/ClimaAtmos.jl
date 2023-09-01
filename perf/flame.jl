94a62ab4235c622da67dab1d3dd446544fab0c00
Anna Jaruga <ajaruga@caltech.edu>
2023-08-31 20:23:48 -0700

    update Thermodynamics, CloudMicrophysics and SurfaceFluxes

--- perf/flame.jl
+++ perf/flame.jl
@@ -56,12 +56,12 @@ allocs = @allocated OrdinaryDiffEq.step!(integrator)
 allocs_limit = Dict()
 allocs_limit["flame_perf_target"] = 4384
 allocs_limit["flame_perf_target_tracers"] = 204016
-allocs_limit["flame_perf_target_edmfx"] = 298688
-allocs_limit["flame_perf_target_diagnostic_edmfx"] = 664336
-allocs_limit["flame_perf_target_edmf"] = 8282903376
+allocs_limit["flame_perf_target_edmfx"] = 301760
+allocs_limit["flame_perf_target_diagnostic_edmfx"] = 678672
+allocs_limit["flame_perf_target_edmf"] = 8953312080
 allocs_limit["flame_perf_target_threaded"] = 6175664
 allocs_limit["flame_perf_target_callbacks"] = 49850536
-allocs_limit["flame_perf_gw"] = 4937136096
+allocs_limit["flame_perf_gw"] = 4964581344
 
 if allocs < allocs_limit[job_id] * buffer
     @info "TODO: lower `allocs_limit[$job_id]` to: $(allocs)"
