0a9dd4b58cfe560ba303d003d5ad379147f4b464
Gabriele Bozzola <sbozzolator@gmail.com>
2023-09-01 13:37:21 -0700

    Add ScheduledDiagnosticIterations

--- docs/src/diagnostics.md
+++ docs/src/diagnostics.md
@@ -1,5 +1,92 @@
 # Computing and saving diagnostics
 
+## I want to compute and output a diagnostic variable
+
+### The low-level interface
+
+Diagnostics are computed and output through callbacks to the main integrator.
+`ClimaAtmos` produces the list of callbacks from a ordered list of
+`ScheduledDiagnostic`s.
+
+A `ScheduledDiagnostic` is an instruction on how to compute and output a given
+`DiagnosticVariable` (see below), along with specific choices regarding
+reductions, compute/output frequencies, and so on. It can be point-wise in space
+and time, or can be the result of a reduction in a period that is defined by
+`output_every` (e.g., the daily average temperature).
+
+More specifically, a `ScheduledDiagnostic` contains the following pieces of data
+
+- `variable`: The diagnostic variable that has to be computed and output.
+- `output_every`: Frequency of how often to save the results to disk.
+- `output_writer`: Function that controls out to save the computed diagnostic
+  variable to disk.
+- `reduction_time_func`: If not `nothing`, the `ScheduledDiagnostic` receives an
+  area of scratch space `acc` where to accumulate partial results. Then, at
+  every `compute_every`, `reduction_time_func` is computed between the
+  previously stored value in `acc` and the new value. This implements a running
+  reduction. For example, if `reduction_time_func = max`, the space `acc` will
+  hold the running maxima of the diagnostic. `acc` is reset after output.
+- `reduction_space_func`: NOT IMPLEMENTED YET
+- `compute_every`: Run the computations every `compute_every`. This is not
+  particularly useful for point-wise diagnostics, where we enforce that
+  `compute_every` = `output_every`. `compute_every` has to evenly divide
+  `output_every`.
+- `pre_output_hook!`: Function that has to be run before saving to disk for
+   reductions (mostly used to implement averages). The function
+   `pre_output_hook!` is called with two arguments: the value accumulated during
+   the reduction, and the number of times the diagnostic was computed from the
+   last time it was output.
+
+To implement operations like the arithmetic average, the `reduction_time_func`
+has to be chosen as `sum`, and a `pre_output_hook!` that renormalize `acc` by
+the number of samples has to be provided. `pre_output_hook!` should mutate the
+accumulator in place. The return value of `pre_output_hook!` is discarded. An
+example of `pre_output_hook!` to compute the arithmetic average is
+`pre_output_hook!(acc, N) = @. acc = acc / N`.
+
+For custom reductions, it is necessary to also specify the identity of operation
+by defining a new method to `identity_of_reduction`. `identity_of_reduction` is
+a function that takes a `Val{op}` argument, where `op` is the operation for
+which we want to define the identity. For instance, for the `max`,
+`identity_of_reduction` would be `identity_of_reduction(::Val{max}) = -Inf`. The
+identities known to `ClimaAtmos` are defined in the
+`diagnostics/reduction_identities.jl` file. The identity is needed to ensure
+that we have a neutral state for the accumulators that are used in the
+reductions.
+
+A key entry in a `ScheduledDiagnostic` is the `output_writer`, the function
+responsible for saving the output to disk. `output_writer` is called with three
+arguments: the value that has to be output, the `ScheduledDiagnostic`, and the
+integrator. Internally, the integrator contains extra information (such as the
+current timestep), so the `output_writer` can implement arbitrarily complex
+behaviors. It is responsibility of the `output_writer` to properly use the
+provided information for meaningful output. `ClimaAtmos` provides functions that
+return `output_writer`s for standard operations (e.g., for writing to HDF5
+files).
+
+There are two flavors of `ScheduledDiagnostic`s:
+`ScheduledDiagnosticIterations`, and `ScheduledDiagnosticTime`. The main
+difference between the two is the domain of definition of their frequencies,
+which is measured in timesteps for the first one, and in seconds for the other
+one. `ScheduledDiagnosticTime`s offer a more natural way to set up physically
+meaningful reductions (e.g., we want a daily average). However, it is not clear
+what to do when the period does not line up with the timestep. What is the
+expected behavior when we want a daily average but our timestep is of 10 hours?
+There are multiple possible answer to this question. In `ClimaAtmos`, we enforce
+that all the periods are multiples of the timestep. With this restriction, a
+`ScheduledDiagnosticTime` can be translated to a
+`ScheduledDiagnosticIterations`, where the problem is perfectly represented (in
+this sense, one can think of `ScheduledDiagnosticIterations` as being as
+internal representation and as `ScheduledDiagnosticTime` being the user-facing
+one).
+
+`ScheduledDiagnosticTime` behave like as `ScheduledDiagnosticIterations`, with
+the exception that they can take a special value for `compute_every`, which can
+be set to `:timestep` (the symbol) to ensure that the diagnostic is computed at
+the end of every integration step. This is particularly convenient when defining
+default diagnostics, as they should be largely independent on the choice of the
+specific simulation being run (and its timestep).
+
 ## I want to add a new diagnostic variable
 
 Diagnostic variables are represented in `ClimaAtmos` with a `DiagnosticVariable`
--- src/diagnostics/Diagnostics.jl
+++ src/diagnostics/Diagnostics.jl
@@ -12,6 +12,26 @@
 #   over their long name. If you want to add more diagnostics, look at the included files.
 #   You can add your own file if you want to define several new diagnostics that are
 #   conceptually related.
+#
+# - The definition of what a ScheduledDiagnostics is. Morally, a ScheduledDiagnostics is a
+#   DiagnosticVariable we want to compute in a given simulation. For example, it could be
+#   the temperature averaged over a day. We can have multiple ScheduledDiagnostics for the
+#   same DiagnosticVariable (e.g., daily and monthly average temperatures).
+#
+#   We provide two types of ScheduledDiagnostics: ScheduledDiagnosticIterations and
+#   ScheduledDiagnosticTime, with the difference being only in what domain the recurrence
+#   time is defined (are we doing something at every N timesteps or every T seconds?). It is
+#   much cleaner and simpler to work with ScheduledDiagnosticIterations because iterations
+#   are well defined and consistent. On the other hand, working in the time domain requires
+#   dealing with what happens when the timestep is not lined up with the output period.
+#   Possible solutions to this problem include: uneven output, interpolation, or restricting
+#   the user from picking specific combinations of timestep/output period. In the current
+#   implementation, we choose the third option. So, ScheduledDiagnosticTime is provided
+#   because it is the physically interesting quantity. If we know what is the timestep, we
+#   can convert between the two and check if the diagnostics are well-posed in terms of the
+#   relationship between the periods and the timesteps. In some sense, you can think of
+#   ScheduledDiagnosticIterations as an internal representation and ScheduledDiagnosticTime
+#   as the external interface.
 
 """
     DiagnosticVariable
@@ -123,3 +143,129 @@ end
 
 # Do you want to define more diagnostics? Add them here
 include("core_diagnostics.jl")
+
+
+# ScheduledDiagnostics
+
+# NOTE: The definitions of ScheduledDiagnosticTime and ScheduledDiagnosticIterations are
+# nearly identical except for the fact that one is assumed to use units of seconds the other
+# units of integration steps. However, we allow for this little repetition of code to avoid
+# adding an extra layer of abstraction just to deal with these two objects (some people say
+# that "duplication is better than over-abstraction"). Most users will only work with
+# ScheduledDiagnosticTime. (It would be nice to have defaults fields in abstract types, as
+# proposed in 2013 in https://github.com/JuliaLang/julia/issues/4935) Having two distinct
+# types allow us to implement different checks and behaviors (e.g., we allow
+# ScheduledDiagnosticTime to have placeholders values for {compute, output}_every so that we
+# can plug the timestep in it).
+
+struct ScheduledDiagnosticIterations{T1, T2, OW, F1, F2, PO}
+    variable::DiagnosticVariable
+    output_every::T1
+    output_writer::OW
+    reduction_time_func::F1
+    reduction_space_func::F2
+    compute_every::T2
+    pre_output_hook!::PO
+
+    """
+        ScheduledDiagnosticIterations(; variable::DiagnosticVariable,
+                                        output_every,
+                                        output_writer,
+                                        reduction_time_func = nothing,
+                                        reduction_space_func = nothing,
+                                        compute_every = isa_reduction ? 1 : output_every,
+                                        pre_output_hook! = (accum, count) -> nothing)
+
+
+    A `DiagnosticVariable` that has to be computed and output during a simulation with a cadence
+    defined by the number of iterations, with an optional reduction applied to it (e.g., compute
+    the maximum temperature over the course of every 10 timesteps). This object is turned into
+    two callbacks (one for computing and the other for output) and executed by the integrator.
+
+    Keyword arguments
+    =================
+
+    - `variable`: The diagnostic variable that has to be computed and output.
+
+    - `output_every`: Save the results to disk every `output_every` iterations.
+
+    - `output_writer`: Function that controls out to save the computed diagnostic variable to
+                       disk. `output_writer` has to take three arguments: the value that has to
+                       be output, the `ScheduledDiagnostic`, and the integrator. Internally, the
+                       integrator contains extra information (such as the current timestep). It
+                       is responsibility of the `output_writer` to properly use the provided
+                       information for meaningful output.
+
+    - `reduction_time_func`: If not `nothing`, this `ScheduledDiagnostic` receives an area of
+                             scratch space `acc` where to accumulate partial results. Then, at
+                             every `compute_every`, `reduction_time_func` is computed between
+                             the previously stored value in `acc` and the new value. This
+                             implements a running reduction. For example, if
+                             `reduction_time_func = max`, the space `acc` will hold the running
+                             maxima of the diagnostic. To implement operations like the
+                             arithmetic average, the `reduction_time_func` has to be chosen as
+                             `sum`, and a `pre_output_hook!` that renormalize `acc` by the
+                             number of samples has to be provided. For custom reductions, it is
+                             necessary to also specify the identity of operation by defining a
+                             new method to `identity_of_reduction`.
+
+    - `reduction_space_func`: NOT IMPLEMENTED YET
+
+    - `compute_every`: Run the computations every `compute_every` iterations. This is not
+                       particularly useful for point-wise diagnostics, where we enforce that
+                       `compute_every` = `output_every`. For time reductions, `compute_every` is
+                       set to 1 (compute at every timestep) by default. `compute_every` has to
+                       evenly divide `output_every`.
+
+    - `pre_output_hook!`: Function that has to be run before saving to disk for reductions
+                          (mostly used to implement averages). The function `pre_output_hook!`
+                          is called with two arguments: the value accumulated during the
+                          reduction, and the number of times the diagnostic was computed from
+                          the last time it was output. `pre_output_hook!` should mutate the
+                          accumulator in place. The return value of `pre_output_hook!` is
+                          discarded. An example of `pre_output_hook!` to compute the arithmetic
+                          average is `pre_output_hook!(acc, N) = @. acc = acc / N`.
+
+    """
+    function ScheduledDiagnosticIterations(;
+        variable::DiagnosticVariable,
+        output_every,
+        output_writer,
+        reduction_time_func = nothing,
+        reduction_space_func = nothing,
+        compute_every = isnothing(reduction_time_func) ? output_every : 1,
+        pre_output_hook! = (accum, count) -> nothing,
+    )
+
+        # We provide an inner constructor to enforce some constraints
+
+        output_every % compute_every == 0 || error(
+            "output_every should be multiple of compute_every for variable $(variable.long_name)",
+        )
+
+        isa_reduction = !isnothing(reduction_time_func)
+
+        # If it is not a reduction, we compute only when we output
+        if !isa_reduction && compute_every != output_every
+            @warn "output_every != compute_every for $(variable.long_name), changing compute_every to match"
+            compute_every = output_every
+        end
+
+        T1 = typeof(output_every)
+        T2 = typeof(compute_every)
+        OW = typeof(output_writer)
+        F1 = typeof(reduction_time_func)
+        F2 = typeof(reduction_space_func)
+        PO = typeof(pre_output_hook!)
+
+        new{T1, T2, OW, F1, F2, PO}(
+            variable,
+            output_every,
+            output_writer,
+            reduction_time_func,
+            reduction_space_func,
+            compute_every,
+            pre_output_hook!,
+        )
+    end
+end
