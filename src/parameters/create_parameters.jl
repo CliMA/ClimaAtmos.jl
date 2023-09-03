bf0f1ae75b54af47685850064cddc2bcff97abb4
Anna Jaruga <ajaruga@caltech.edu>
2023-09-03 11:06:14 -0700

    Get rid of MSLP overwrite

--- src/parameters/create_parameters.jl
+++ src/parameters/create_parameters.jl
@@ -161,7 +161,7 @@ function create_parameter_set(config::AtmosConfig)
     dt = FT(CA.time_to_seconds(parsed_args["dt"]))
 
     return if CA.is_column_edmf(parsed_args)
-        overrides = (; MSLP = 100000.0, τ_precip = dt)
+        overrides = (; τ_precip = dt)
         create_climaatmos_parameter_set(toml_dict, parsed_args, overrides)
     elseif CA.is_column_without_edmf(parsed_args)
         overrides = (; τ_precip = dt)
@@ -169,7 +169,6 @@ function create_parameter_set(config::AtmosConfig)
     else
         overrides = (;
             R_d = 287.0,
-            MSLP = 1.0e5,
             grav = 9.80616,
             Omega = 7.29212e-5,
             planet_radius = 6.371229e6,
