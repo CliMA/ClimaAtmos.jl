cf959246cc55bc66a21ae01865ea383e1e48c7b8
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-09-07 13:45:39 -0700

    Update njet failures

--- perf/jet_test_nfailures.jl
+++ perf/jet_test_nfailures.jl
@@ -26,7 +26,7 @@ using Test
     # inference. By increasing this counter, we acknowledge that
     # we have introduced an inference failure. We hope to drive
     # this number down to 0.
-    n_allowed_failures = 256
+    n_allowed_failures = 680
     @test n ≤ n_allowed_failures
     if n < n_allowed_failures
         @info "Please update the n-failures to $n"
