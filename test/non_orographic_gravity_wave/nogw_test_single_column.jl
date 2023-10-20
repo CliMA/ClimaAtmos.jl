779c71876d68eef5fb0d1dbced7c8e06eaeb58a2
Simon Byrne <simonbyrne@gmail.com>
2023-10-19 17:13:13 -0700

    test not ismissing

--- test/non_orographic_gravity_wave/nogw_test_single_column.jl
+++ test/non_orographic_gravity_wave/nogw_test_single_column.jl
@@ -12,43 +12,43 @@ const FT = Float64
 # zonal mean monthly wind, temperature 1958-1973; at 40N in latitude for Jan, April, July, Oct.
 
 face_z = FT.(0:1e3:0.5e5)
-center_z = FT(0.5) .* (face_z[1:(end - 1)] .+ face_z[2:end])
+center_z = FT(0.5) .* (face_z[1:(end-1)] .+ face_z[2:end])
 
 # compute the source parameters
 function non_orographic_gravity_wave_cache(
     ::Type{FT};
-    source_height = FT(15000),
-    Bw = FT(1.2),
-    Bt_0 = FT(4e-3),
-    dc = FT(0.6),
-    cmax = FT(99.6),
-    c0 = FT(0),
-    kwv = FT(2π / 100e5),
-    cw = FT(40.0),
+    source_height=FT(15000),
+    Bw=FT(1.2),
+    Bt_0=FT(4e-3),
+    dc=FT(0.6),
+    cmax=FT(99.6),
+    c0=FT(0),
+    kwv=FT(2π / 100e5),
+    cw=FT(40.0)
 ) where {FT}
 
     nc = Int(floor(FT(2 * cmax / dc + 1)))
     c = [FT((n - 1) * dc - cmax) for n in 1:nc]
 
     return (;
-        gw_source_height = source_height,
-        gw_source_ampl = Bt_0,
-        gw_Bw = Bw,
-        gw_Bn = FT(0),
-        gw_c = c,
-        gw_cw = cw,
-        gw_cn = FT(1),
-        gw_flag = FT(1),
-        gw_c0 = c0,
-        gw_nk = length(kwv),
+        gw_source_height=source_height,
+        gw_source_ampl=Bt_0,
+        gw_Bw=Bw,
+        gw_Bn=FT(0),
+        gw_c=c,
+        gw_cw=cw,
+        gw_cn=FT(1),
+        gw_flag=FT(1),
+        gw_c0=c0,
+        gw_nk=length(kwv)
     )
 end
 
 params = non_orographic_gravity_wave_cache(
     FT;
-    Bw = 0.4,
-    cmax = 150,
-    kwv = 2π / 100e3,
+    Bw=0.4,
+    cmax=150,
+    kwv=2π / 100e3
 )
 source_level = argmin(abs.(center_z .- params.gw_source_height))
 damp_level = length(center_z)
@@ -86,10 +86,10 @@ Z = gZ ./ grav
 dTdz = zeros(size(T))
 @. dTdz[:, 1, :] = (T[:, 2, :] - T[:, 1, :]) / (Z[:, 2, :] - Z[:, 1, :])
 @. dTdz[:, end, :] =
-    (T[:, end, :] - T[:, end - 1, :]) / (Z[:, end, :] - Z[:, end - 1, :])
-@. dTdz[:, 2:(end - 1), :] =
-    (T[:, 3:end, :] - T[:, 1:(end - 2), :]) /
-    (Z[:, 3:end, :] - Z[:, 1:(end - 2), :])
+    (T[:, end, :] - T[:, end-1, :]) / (Z[:, end, :] - Z[:, end-1, :])
+@. dTdz[:, 2:(end-1), :] =
+    (T[:, 3:end, :] - T[:, 1:(end-2), :]) /
+    (Z[:, 3:end, :] - Z[:, 1:(end-2), :])
 bf = @. (grav / T) * (dTdz + grav / cp_d)
 bf = @. ifelse(bf < 2.5e-5, sqrt(2.5e-5), sqrt(abs(bf)))
 
@@ -102,30 +102,30 @@ for i in 1:length(lon)
         interp_linear = LinearInterpolation(
             Z[i, :, it][end:-1:1],
             u[i, :, it][end:-1:1],
-            extrapolation_bc = Line(),
+            extrapolation_bc=Line(),
         )
         center_u[i, :, it] = interp_linear.(center_z)
 
         interp_linear = LinearInterpolation(
             Z[i, :, it][end:-1:1],
             bf[i, :, it][end:-1:1],
-            extrapolation_bc = Line(),
+            extrapolation_bc=Line(),
         )
         center_bf[i, :, it] = interp_linear.(center_z)
 
         interp_linear = LinearInterpolation(
             Z[i, :, it][end:-1:1],
             ρ[i, :, it][end:-1:1],
-            extrapolation_bc = Line(),
+            extrapolation_bc=Line(),
         )
         center_ρ[i, :, it] = interp_linear.(center_z)
     end
 end
 
 # zonal mean
-center_u_mean = mean(center_u, dims = 1)[1, :, :]
-center_bf_mean = mean(center_bf, dims = 1)[1, :, :]
-center_ρ_mean = mean(center_ρ, dims = 1)[1, :, :]
+center_u_mean = mean(center_u, dims=1)[1, :, :]
+center_bf_mean = mean(center_bf, dims=1)[1, :, :]
+center_ρ_mean = mean(center_ρ, dims=1)[1, :, :]
 
 # monthly ave Jan, April, July, Oct
 month = Dates.month.(time)
@@ -135,9 +135,9 @@ output_dir = "nonorographic_gravity_wave_test_single_column"
 mkpath(output_dir)
 
 # Jan
-Jan_u = mean(center_u_mean[:, month .== 1], dims = 2)[:, 1]
-Jan_bf = mean(center_bf_mean[:, month .== 1], dims = 2)[:, 1]
-Jan_ρ = mean(center_ρ_mean[:, month .== 1], dims = 2)[:, 1]
+Jan_u = mean(center_u_mean[:, month.==1], dims=2)[:, 1]
+Jan_bf = mean(center_bf_mean[:, month.==1], dims=2)[:, 1]
+Jan_ρ = mean(center_ρ_mean[:, month.==1], dims=2)[:, 1]
 Jan_uforcing = CA.non_orographic_gravity_wave_forcing(
     Jan_u,
     Jan_bf,
@@ -157,16 +157,16 @@ Jan_uforcing = CA.non_orographic_gravity_wave_forcing(
 )
 png(
     plot(
-        Jan_uforcing[source_level:(end - 1)] * 86400,
-        center_z[source_level:(end - 1)],
+        Jan_uforcing[source_level:(end-1)] * 86400,
+        center_z[source_level:(end-1)],
     ),
     joinpath(output_dir, "fig6jan.png"),
 )
 
 # April
-April_u = mean(center_u_mean[:, month .== 4], dims = 2)[:, 1]
-April_bf = mean(center_bf_mean[:, month .== 4], dims = 2)[:, 1]
-April_ρ = mean(center_ρ_mean[:, month .== 4], dims = 2)[:, 1]
+April_u = mean(center_u_mean[:, month.==4], dims=2)[:, 1]
+April_bf = mean(center_bf_mean[:, month.==4], dims=2)[:, 1]
+April_ρ = mean(center_ρ_mean[:, month.==4], dims=2)[:, 1]
 April_uforcing = CA.non_orographic_gravity_wave_forcing(
     April_u,
     April_bf,
@@ -186,16 +186,16 @@ April_uforcing = CA.non_orographic_gravity_wave_forcing(
 )
 png(
     plot(
-        April_uforcing[source_level:(end - 1)] * 86400,
-        center_z[source_level:(end - 1)],
+        April_uforcing[source_level:(end-1)] * 86400,
+        center_z[source_level:(end-1)],
     ),
     joinpath(output_dir, "fig6apr.png"),
 )
 
 # July
-July_u = mean(center_u_mean[:, month .== 7], dims = 2)[:, 1]
-July_bf = mean(center_bf_mean[:, month .== 7], dims = 2)[:, 1]
-July_ρ = mean(center_ρ_mean[:, month .== 7], dims = 2)[:, 1]
+July_u = mean(center_u_mean[:, month.==7], dims=2)[:, 1]
+July_bf = mean(center_bf_mean[:, month.==7], dims=2)[:, 1]
+July_ρ = mean(center_ρ_mean[:, month.==7], dims=2)[:, 1]
 July_uforcing = CA.non_orographic_gravity_wave_forcing(
     July_u,
     July_bf,
@@ -215,16 +215,16 @@ July_uforcing = CA.non_orographic_gravity_wave_forcing(
 )
 png(
     plot(
-        July_uforcing[source_level:(end - 1)] * 86400,
-        center_z[source_level:(end - 1)],
+        July_uforcing[source_level:(end-1)] * 86400,
+        center_z[source_level:(end-1)],
     ),
     joinpath(output_dir, "fig6jul.png"),
 )
 
 # Oct
-Oct_u = mean(center_u_mean[:, month .== 10], dims = 2)[:, 1]
-Oct_bf = mean(center_bf_mean[:, month .== 10], dims = 2)[:, 1]
-Oct_ρ = mean(center_ρ_mean[:, month .== 10], dims = 2)[:, 1]
+Oct_u = mean(center_u_mean[:, month.==10], dims=2)[:, 1]
+Oct_bf = mean(center_bf_mean[:, month.==10], dims=2)[:, 1]
+Oct_ρ = mean(center_ρ_mean[:, month.==10], dims=2)[:, 1]
 Oct_uforcing = CA.non_orographic_gravity_wave_forcing(
     Oct_u,
     Oct_bf,
@@ -244,8 +244,8 @@ Oct_uforcing = CA.non_orographic_gravity_wave_forcing(
 )
 png(
     plot(
-        Oct_uforcing[source_level:(end - 1)] * 86400,
-        center_z[source_level:(end - 1)],
+        Oct_uforcing[source_level:(end-1)] * 86400,
+        center_z[source_level:(end-1)],
     ),
     joinpath(output_dir, "fig6oct.png"),
 )
