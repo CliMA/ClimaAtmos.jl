50174eebee612754e436633eb8d4755b818135ef
Gabriele Bozzola <gbozzola@caltech.edu>
2023-10-27 13:18:28 -0700

    Cache: limiters

--- src/prognostic_equations/limited_tendencies.jl
+++ src/prognostic_equations/limited_tendencies.jl
@@ -1,17 +1,3 @@
-using LinearAlgebra: ×, norm, dot
-
-import .Parameters as CAP
-using ClimaCore: Operators, Fields, Limiters, Geometry, Spaces
-
-import ClimaComms
-using ClimaCore.Geometry: ⊗
-
-import Thermodynamics as TD
-
-using ClimaCore.Utilities: half
-
-import ClimaCore.Fields: ColumnField
-
 NVTX.@annotate function limited_tendency!(Yₜ, Y, p, t)
     Yₜ .= zero(eltype(Yₜ))
     horizontal_tracer_advection_tendency!(Yₜ, Y, p, t)
@@ -20,8 +6,7 @@ NVTX.@annotate function limited_tendency!(Yₜ, Y, p, t)
 end
 
 NVTX.@annotate function limiters_func!(Y, p, t, ref_Y)
-    (; limiter) = p
-    n = n_mass_flux_subdomains(p.atmos.turbconv_model)
+    (; limiter) = p.numerics
     if !isnothing(limiter)
         for ρχ_name in filter(is_tracer_var, propertynames(Y.c))
             Limiters.compute_bounds!(limiter, ref_Y.c.:($ρχ_name), ref_Y.c.ρ)
