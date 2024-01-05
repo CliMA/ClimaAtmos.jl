c80f05ba2714247115bfad411484ac960737b16f
Gabriele Bozzola <gbozzola@caltech.edu>
2024-01-05 09:48:34 -0800

    Add show method for AtmosSimulation

--- src/simulation/AtmosSimulations.jl
+++ src/simulation/AtmosSimulations.jl
@@ -12,3 +12,14 @@ struct AtmosSimulation{
     output_writers::OW
     integrator::OD
 end
+
+function Base.show(io::IO, sim::AtmosSimulation)
+    return print(
+        io,
+        "Simulation $(sim.job_id)\n",
+        "├── Output folder: $(sim.output_dir)\n",
+        "├── Start date: $(sim.start_date)\n",
+        "├── Current time: $(sim.integrator.t) seconds\n",
+        "└── Stop time: $(sim.t_end) seconds",
+    )
+end
