cdd54ee0bf12351178c9d4729fcea3b05d0600ee
Gabriele Bozzola <gbozzola@caltech.edu>
2024-01-05 13:32:28 -0800

    z -> 1.5km in plots for dry baroclinic waves

--- post_processing/ci_plots.jl
+++ post_processing/ci_plots.jl
@@ -288,7 +288,7 @@ function make_plots(::DryBaroWavePlots, simulation_path)
     simdir = SimDir(simulation_path)
     short_names = ["pfull", "va", "wa", "rv"]
     vars = [get(simdir; short_name) for short_name in short_names]
-    make_plots_generic(simulation_path, vars, z = 3000, time = LAST_SNAP)
+    make_plots_generic(simulation_path, vars, z = 1500, time = LAST_SNAP)
 end
 
 function make_plots(::Val{:longrun_bw_rhoe_highres}, simulation_path)
@@ -297,7 +297,7 @@ function make_plots(::Val{:longrun_bw_rhoe_highres}, simulation_path)
     vars = [
         get(simdir; short_name, reduction, period) for short_name in short_names
     ]
-    make_plots_generic(simulation_path, vars, z = 3000, time = 10days)
+    make_plots_generic(simulation_path, vars, z = 1500, time = 10days)
 end
 
 function make_plots(
@@ -307,7 +307,7 @@ function make_plots(
     simdir = SimDir(simulation_path)
     short_names = ["pfull", "va", "wa", "rv", "hus"]
     vars = [get(simdir; short_name) for short_name in short_names]
-    make_plots_generic(simulation_path, vars, z = 3000, time = LAST_SNAP)
+    make_plots_generic(simulation_path, vars, z = 1500, time = LAST_SNAP)
 end
 
 MoistBaroWavePlots = Union{
