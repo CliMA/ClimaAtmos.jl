966081d7a0f64eb00cd455e01b68dcbf8513f45e
Gabriele Bozzola <gbozzola@caltech.edu>
2024-01-08 08:49:54 -0800

    Add context/device for AtmosSimulation
    Add device used to show as well

--- src/simulation/AtmosSimulations.jl
+++ src/simulation/AtmosSimulations.jl
@@ -13,10 +13,16 @@ struct AtmosSimulation{
     integrator::OD
 end
 
+ClimaComms.context(sim::AtmosSimulation) =
+    ClimaComms.context(sim.integrator.u.c)
+ClimaComms.device(sim::AtmosSimulation) = ClimaComms.device(sim.integrator.u.c)
+
 function Base.show(io::IO, sim::AtmosSimulation)
+    device_type = nameof(typeof(ClimaComms.device(sim)))
     return print(
         io,
         "Simulation $(sim.job_id)\n",
+        "├── Running on: $(device_type)\n",
         "├── Output folder: $(sim.output_dir)\n",
         "├── Start date: $(sim.start_date)\n",
         "├── Current time: $(sim.integrator.t) seconds\n",
