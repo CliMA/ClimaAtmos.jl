e1e189c4270209b4bf1bd01dacf0188112854881
Zhaoyi Shen <11598433+szy21@users.noreply.github.com>
2024-01-14 23:05:25 -0800

    remove default cl diagnostics

--- src/diagnostics/default_diagnostics.jl
+++ src/diagnostics/default_diagnostics.jl
@@ -214,7 +214,7 @@ function default_diagnostics(
     ::T;
     output_writer,
 ) where {T <: Union{EquilMoistModel, NonEquilMoistModel}}
-    moist_diagnostics = ["hur", "hus", "cl", "clw", "cli", "hussfc", "evspsbl"]
+    moist_diagnostics = ["hur", "hus", "clw", "cli", "hussfc", "evspsbl"]
 
     return [daily_averages(moist_diagnostics...; output_writer)...]
 end
