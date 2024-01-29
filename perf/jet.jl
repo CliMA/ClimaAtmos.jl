ba1f2e312e884492084cbbace4720a57ac9ed5b3
Charles Kawczynski <kawczynski.charles@gmail.com>
2024-01-29 09:37:17 -0500

    wip

--- perf/jet.jl
+++ perf/jet.jl
@@ -6,6 +6,9 @@ using Revise; if !("--config_file" in ARGS)
 end; include("perf/jet.jl")
 =#
 redirect_stderr(IOContext(stderr, :stacktrace_types_limited => Ref(false)))
+import ClimaCore
+ClimaCore.Fields.truncate_printing_field_types() = true
+ClimaCore.MatrixFields.truncate_printing_field_types() = true
 if !("--config_file" in ARGS)
     push!(ARGS, "--config_file")
     push!(ARGS, "config/default_configs/default_perf.yml")
@@ -28,5 +31,16 @@ simulation = CA.get_simulation(config)
 import JET
 
 import SciMLBase
-SciMLBase.step!(integrator) # Make sure no errors
-JET.@test_opt SciMLBase.step!(integrator)
+# SciMLBase.step!(integrator) # Make sure no errors
+# JET.@test_opt SciMLBase.step!(integrator)
+# JET.@test_opt CA.precipitation_tendency!(Yₜ, Y, p, t, colidx, ::NoPrecipitation)
+(; u, p, t) = integrator;
+Yₜ = copy(u);
+Y = u;
+colidx = Fields.ColumnIndex((1,1),1);
+@show p.atmos.precip_model
+CA.precipitation_tendency!(Yₜ, Y, p, t, colidx, p.atmos.precip_model)
+JET.@test_opt CA.precipitation_tendency!(Yₜ, Y, p, t, colidx, p.atmos.precip_model)
+
+CA.compute_precipitation_cache!(Y, p, colidx, p.atmos.precip_model, nothing)
+    JET.@test_opt CA.compute_precipitation_cache!(Y, p, colidx, p.atmos.precip_model, nothing)
