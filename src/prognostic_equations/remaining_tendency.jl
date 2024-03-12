509dc8a2eefba77ad5dbd12f813ffd97708b85c0
Anna Jaruga <ajaruga@caltech.edu>
2024-03-12 14:37:45 -0700

    Separate precipitation sources and sinks

--- src/prognostic_equations/remaining_tendency.jl
+++ src/prognostic_equations/remaining_tendency.jl
@@ -81,6 +81,7 @@ NVTX.@annotate function additional_tendency!(Yₜ, Y, p, t)
             p.atmos.turbconv_model,
         )
         edmfx_tke_tendency!(Yₜ, Y, p, t, colidx, p.atmos.turbconv_model)
+        # TODO - add the 1-moment precipitation microphysics here
         edmfx_precipitation_tendency!(
             Yₜ,
             Y,
