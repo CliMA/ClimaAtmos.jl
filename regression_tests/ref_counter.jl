3aaacf660497623727aebf09ef37f55a63ef8f5f
Anna Jaruga <ajaruga@caltech.edu>
2024-03-28 15:54:06 -0700

    Bump the ref counter

--- regression_tests/ref_counter.jl
+++ regression_tests/ref_counter.jl
@@ -1,4 +1,10 @@
-156
+157
+
+# 157:
+#  - For the grid mean precipitation tendency in the 0-moment scheme:
+#     - added limiting by q_tot/dt
+#     - switched the precipitation threshold defintion
+#       from specific humidity based to supersaturation based
 
 # 156:
 #  - Changed start date (changes insolation)
