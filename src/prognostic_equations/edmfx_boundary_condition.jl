cb5c37c364568fd348d7177a153a1e9493e5f9a3
Zhaoyi Shen <11598433+szy21@users.noreply.github.com>
2024-04-05 23:04:04 -0700

    prognostic surface area

--- src/prognostic_equations/edmfx_boundary_condition.jl
+++ src/prognostic_equations/edmfx_boundary_condition.jl
@@ -2,9 +2,12 @@
 ##### EDMFX SGS boundary condition
 #####
 
+import Distributions
+
 function sgs_scalar_first_interior_bc(
     ᶜz_int::FT,
     ᶜρ_int::FT,
+    ᶜaʲ_int::FT,
     ᶜscalar_int::FT,
     sfc_buoyancy_flux,
     sfc_ρ_flux_scalar,
@@ -26,7 +29,7 @@ function sgs_scalar_first_interior_bc(
     #     1 - a_total + (i - 1) * a_,
     #     1 - a_total + i * a_,
     # )
-    surface_scalar_coeff = FT(1.75)
+    surface_scalar_coeff = percentile_bounds_mean_norm(1 - ᶜaʲ_int, FT(1))
     return ᶜscalar_int + surface_scalar_coeff * sqrt(scalar_var)
 end
 
@@ -45,4 +48,17 @@ function get_first_interior_variance(
     else
         return 4 * Geometry._norm_sqr(c_star, local_geometry)
     end
+end
+
+function percentile_bounds_mean_norm(
+    low_percentile::FT,
+    high_percentile::FT,
+) where {FT <: Real}
+    gauss_int(x) = -exp(-x * x / 2) / sqrt(2 * pi)
+    xp_low = Distributions.quantile(Distributions.Normal(), low_percentile)
+    xp_high = Distributions.quantile(Distributions.Normal(), high_percentile)
+    return (gauss_int(xp_high) - gauss_int(xp_low)) / (
+        Distributions.cdf(Distributions.Normal(), xp_high) -
+        Distributions.cdf(Distributions.Normal(), xp_low)
+    )
 end
