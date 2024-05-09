00d7c4e242387ec286695f0956d7ca94fd9765cc
Akshay Sridhar <asridhar@clima.gps.caltech.edu>
2024-05-09 15:53:53 -0700

    modified:   config/model_configs/les_bomex_box.yml 	modified:   src/parameterized_tendencies/smagorinsky_lilly.jl

--- src/parameterized_tendencies/smagorinsky_lilly.jl
+++ src/parameterized_tendencies/smagorinsky_lilly.jl
@@ -35,11 +35,7 @@ function horizontal_smagorinsky_lilly_tendency!(Yₜ, Y, p, t, sl::SmagorinskyLi
     (; Cs) = sl
     (; v_t, Δ_filter) = p.smagorinsky_lilly
     (; ᶜu, ᶠu³) = p.precomputed 
-
-    @show "start horizontal tendency_ SUCCESS"
-    @show Y.c.ρq_tot
-    @show "start horizontal tendency_ SUCCESS"
-
+    
     # Operators
     FT = eltype(v_t)
     ᶜJ = Fields.local_geometry_field(Y.c).J 
@@ -115,18 +111,6 @@ function horizontal_smagorinsky_lilly_tendency!(Yₜ, Y, p, t, sl::SmagorinskyLi
     # as well)
     for (ᶜρχₜ, ᶜχ, χ_name) in CA.matching_subfields(Yₜ.c, ᶜspecific)
         χ_name == :e_tot && continue
-        @show ("--------h-------") 
-        @show (ᶜρχₜ)
-        @show (ᶜχ)
-        @show (χ_name) 
-        @show ("--------h-------") 
-        @show (Y.c.ρ) 
-        @show (Y.c.ρq_tot)
-        @show (Y.c.ρq_tot ./ Y.c.ρ)
-        @show ("ρq_tot") 
-        @show (ᶜD)
-        @show ("--------h------") 
-        #@. Yₜ.c.ρq_tot = wdivₕ(Y.c.ρ * ᶜD * gradₕ(Y.c.ρq_tot ./ Y.c.ρ))
         @. ᶜρχₜ += wdivₕ(Y.c.ρ * ᶜD * gradₕ(ᶜχ)) 
         @. Yₜ.c.ρ += wdivₕ(Y.c.ρ * ᶜD * gradₕ(ᶜχ)) 
     end
@@ -256,126 +240,4 @@ function vertical_smagorinsky_lilly_tendency!(Yₜ, Y, p, t, sl::SmagorinskyLill
         @. Yₜ.c.ρ -= ᶜdivᵥ_ρχ(-(ᶠinterp(Y.c.ρ) * ᶠinterp(ᶜD) * ᶠgradᵥ(ᶜχ)))
     end
     any(isnan, Yₜ) && error("Found NaN in vertical sgs tendency after qtot")
-    @show "end vertical tendency_ SUCCESS"
-    @show Y.c.ρq_tot
-    @show "end vertical tendency_ SUCCESS"
 end
-
-# function vertical_smagorinsky_lilly_tendency!(Yₜ, Y, p, t, colidx, sl::SmagorinskyLilly) 
-#     if !(hasproperty(p.precomputed, :ᶜspecific))
-#         throw(ErrorException("p does not have the property ᶜspecific."))
-#     end
-#     (; Cs) = sl
-#     (; v_t, ᶜD, Δ_filter) = p.smagorinsky_lilly
-#     (; ᶜu, ᶠu³, sfc_conditions, ᶜspecific) = p.precomputed 
-
-#     # Operators
-#     FT = eltype(v_t)
-#     wdivₕ = Operators.WeakDivergence()
-#     ᶜJ = Fields.local_geometry_field(Y.c).J 
-
-#     ᶜS  = p.scratch.ᶜtemp_strain
-#     ᶠS  = p.scratch.ᶠtemp_strain
-#     ᶜϵ = p.scratch.ᶜtemp_UVWxUVW
-#     ᶠϵ = p.scratch.ᶠtemp_UVWxUVW
-    
-#     localu = @. Geometry.UVWVector(ᶜu[colidx])
-#     ᶠu = @. Geometry.UVWVector(ᶠwinterp(ᶜJ[colidx] * Y.c.ρ[colidx], Y.c.uₕ[colidx])) + Geometry.UVWVector(Y.f.u₃[colidx])
-
-#     c1 = @. Geometry.UVWVector(gradₕ(localu[colidx].components.data.:1))
-#     c2 = @. Geometry.UVWVector(gradₕ(localu[colidx].components.data.:2))
-#     c3 = @. Geometry.UVWVector(gradₕ(localu[colidx].components.data.:3))
-#     @. ᶜS[colidx].components.data.:1 = c1[colidx].components.data.:1
-#     @. ᶜS[colidx].components.data.:2 = c2[colidx].components.data.:1
-#     @. ᶜS[colidx].components.data.:3 = c3[colidx].components.data.:1
-#     @. ᶜS[colidx].components.data.:4 = c1[colidx].components.data.:2
-#     @. ᶜS[colidx].components.data.:5 = c2[colidx].components.data.:2
-#     @. ᶜS[colidx].components.data.:6 = c3[colidx].components.data.:2
-#     CA.compute_strain_rate_center!(ᶜϵ[colidx], Geometry.Covariant123Vector.(ᶠu[colidx]))
-#     @. ᶜS[colidx] = (ᶜS[colidx] + adjoint(ᶜS[colidx])) + ᶜϵ[colidx]
-
-#     t1 = @. Geometry.UVWVector(gradₕ(ᶠu[colidx].components.data.:1))
-#     t2 = @. Geometry.UVWVector(gradₕ(ᶠu[colidx].components.data.:2))
-#     t3 = @. Geometry.UVWVector(gradₕ(ᶠu[colidx].components.data.:3))
-#     @. ᶠS[colidx].components.data.:1 = t1[colidx].components.data.:1
-#     @. ᶠS[colidx].components.data.:2 = t2[colidx].components.data.:1
-#     @. ᶠS[colidx].components.data.:3 = t3[colidx].components.data.:1
-#     @. ᶠS[colidx].components.data.:4 = t1[colidx].components.data.:2
-#     @. ᶠS[colidx].components.data.:5 = t2[colidx].components.data.:2
-#     @. ᶠS[colidx].components.data.:6 = t3[colidx].components.data.:2
-#     CA.compute_strain_rate_face!(ᶠϵ[colidx], ᶜu[colidx])
-#     @. ᶠS[colidx] = (ᶠS[colidx] + adjoint(ᶠS[colidx])) + ᶠϵ[colidx]
-
-#     ᶜv_t = @. (Cs * Δ_filter)^2 * sqrt(2 * CA.norm_sqr(ᶜS[colidx]))
-#     ᶠv_t = @. ᶠinterp(ᶜv_t[colidx])
-#     @. ᶜD[colidx] = @. FT(3) * ᶜv_t[colidx]
-#     @. v_t[colidx] = ᶜv_t[colidx]
-    
-#     @. Yₜ.c.uₕ[colidx] -= C12(
-#         ᶜdivᵥ(
-#             -2 *
-#             ᶠinterp(Y.c.ρ[colidx]) *
-#             ᶠv_t[colidx] *
-#             ᶠS[colidx],
-#         ) / Y.c.ρ[colidx],
-#     )
-
-#     any(isnan, Yₜ) && error("Found NaN in vertical sgs tendency")
-    
-#     # apply boundary condition for momentum flux
-#     #ᶜdivᵥ_uₕ = Operators.DivergenceF2C(
-#     #    top = Operators.SetValue(C3(FT(0)) ⊗ C12(FT(0), FT(0))),
-#     #    bottom = Operators.SetValue(sfc_conditions.ρ_flux_uₕ[colidx]),
-#     #)
-#     #@. Yₜ.c.uₕ[colidx] -=
-#     #C12(ᶜdivᵥ_uₕ(-(FT(0) * ᶠgradᵥ(Y.c.uₕ[colidx]))) / Y.c.ρ[colidx])
-
-#     ᶜdivᵥ_u3 = Operators.DivergenceC2F(
-#         top = Operators.SetValue(C3(FT(0)) ⊗ C3(FT(0))),
-#         bottom = Operators.SetValue(C3(FT(0)) ⊗ C3(FT(0))), 
-#     )
-    
-#     @. Yₜ.f.u₃[colidx] -= C3(
-#         ᶠinterp(ᶜdivᵥ(
-#             -2 *
-#             ᶠinterp(Y.c.ρ[colidx]) *
-#             ᶠv_t[colidx] *
-#             ᶠS[colidx],
-#         ) / Y.c.ρ[colidx]),
-#     )
-
-#     any(isnan, Yₜ) && error("Found NaN in vertical sgs tendency")
-    
-#     if :ρe_tot in propertynames(Yₜ.c)
-#         (; ᶜh_tot) = p.precomputed
-#         ᶜdivᵥ_ρe_tot = Operators.DivergenceF2C(
-#             top = Operators.SetValue(C3(FT(0))),
-#             bottom = Operators.SetValue(sfc_conditions.ρ_flux_h_tot[colidx]), 
-#         )
-#         @. Yₜ.c.ρe_tot[colidx] -= ᶜdivᵥ_ρe_tot(
-# 					-(ᶠinterp(Y.c.ρ[colidx]) * 
-# 					ᶠinterp(ᶜD[colidx]) * 
-# 					ᶠgradᵥ(ᶜh_tot[colidx]))
-# 				  )
-#     end
-#     any(isnan, Yₜ) && error("Found NaN in vertical sgs tendency")
-
-#     ρ_flux_χ = p.scratch.sfc_temp_C3
-#     for (ᶜρχₜ, ᶜχ, χ_name) in matching_subfields(Yₜ.c, ᶜspecific)
-#         χ_name == :e_tot && continue
-#         if χ_name == :q_tot
-#             @. ρ_flux_χ[colidx] = sfc_conditions.ρ_flux_q_tot[colidx]
-#         else
-#             @. ρ_flux_χ[colidx] = C3(FT(0))
-#         end
-#         ᶜdivᵥ_ρχ = Operators.DivergenceF2C(
-#             top = Operators.SetValue(C3(FT(0))),
-#             bottom = Operators.SetValue(ρ_flux_χ[colidx]), 
-#         )
-#         # The code below adjusts the tendency by -div(\rho * d_qtot), where
-#         # d_qtot = -(D * grad(qtot)). The two negatives cancel out, so we have a +=
-#         @. ᶜρχₜ[colidx] -= ᶜdivᵥ_ρχ(-(ᶠinterp(Y.c.ρ[colidx]) * ᶠinterp(ᶜD[colidx]) * ᶠgradᵥ(ᶜχ[colidx])))
-#     end
-#     any(isnan, Yₜ) && error("Found NaN in vertical sgs tendency")
-
-# end
