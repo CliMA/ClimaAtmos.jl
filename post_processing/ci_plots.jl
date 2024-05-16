afead5ed9fdf9f24450a4031b3d55b8ccebfcdbc
Gabriele Bozzola <gbozzola@caltech.edu>
2024-05-16 10:06:01 -0700

    Plot spectrum for moist baro wave

--- post_processing/ci_plots.jl
+++ post_processing/ci_plots.jl
@@ -677,11 +677,21 @@ function make_plots(
 )
     simdirs = SimDir.(output_paths)
     short_names, reduction = ["pfull", "va", "wa", "rv", "hus"], "inst"
+    short_names_spectra = ["ke"]
     vars = map_comparison(simdirs, short_names) do simdir, short_name
-        return get(simdir; short_name, reduction)
+        return slice(get(simdir; short_name, reduction), time = 10days)
     end
-    make_plots_generic(output_paths, vars, z = 1500, time = LAST_SNAP)
+    vars_spectra =
+        map_comparison(simdirs, short_names_spectra) do simdir, short_name
+            compute_spectrum(
+                slice(get(simdir; short_name, reduction), time = 10days),
+            )
+        end
+    vars = vcat(vars..., vars_spectra...)
+
+    make_plots_generic(output_paths, vars, z = 1500)
 end
+
 
 function make_plots(
     ::Val{:sphere_baroclinic_wave_rhoe_equilmoist_expvdiff},
