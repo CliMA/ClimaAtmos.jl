2066be56cf281edc462db5cb649757a2c02805a5
nefrathenrici <nat.henrici@gmail.com>
2024-07-05 16:17:56 -0700

    more work

--- calibration/experiments/aquaplanet_rhoe_equil_precip_toa/observation_map.jl
+++ calibration/experiments/aquaplanet_rhoe_equil_precip_toa/observation_map.jl
@@ -1,16 +1,15 @@
-import EnsembleKalmanProcesses: TOMLInterface
-import ClimaCalibrate: observation_map, ExperimentConfig
+import ClimaCalibrate: observation_map, path_to_ensemble_member
 import ClimaAnalysis: SimDir, get, slice, average_lat, average_lon
 
 function observation_map(iteration)
     ensemble_size = 10
     output_dir = joinpath("output", "aquaplanet_rhoe_equil_precip_toa")
-    single_member_dims = (1,)
+    single_member_dims = (3,)
     G_ensemble = Array{Float64}(undef, single_member_dims..., ensemble_size)
 
     for m in 1:ensemble_size
         member_path =
-            TOMLInterface.path_to_ensemble_member(output_dir, iteration, m)
+            path_to_ensemble_member(output_dir, iteration, m)
         simdir_path = joinpath(member_path, "output_active")
         if isdir(simdir_path)
             simdir = SimDir(simdir_path)
