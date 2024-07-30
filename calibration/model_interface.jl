04c52ec6fd4d1eda3fd3fd2558e4c674650586ba
Julian Schmitt <jschmitt@caltech.edu>
2024-07-30 14:35:44 -0700

    hotfix model_interface file

--- calibration/model_interface.jl
+++ calibration/model_interface.jl
@@ -7,18 +7,6 @@ redirect_stderr(IOContext(stderr, :stacktrace_types_limited => Ref(false)))
 
 import ClimaAtmos as CA
 import YAML
-<<<<<<< HEAD
-import ClimaCalibrate:
-    set_up_forward_model, run_forward_model, path_to_ensemble_member
-
-"""
-    set_up_forward_model(member, iteration, experiment_dir::AbstractString)
-
-Returns an AtmosConfig object for the given member and iteration.
-If given an experiment id string, it will load the config from the corresponding YAML file.
-Turns off default diagnostics and sets the TOML parameter file to the member's path.
-This assumes that the config dictionary has `output_dir` and `restart_file` keys.
-=======
 import ClimaComms
 @static pkgversion(ClimaComms) >= v"0.6" && ClimaComms.@import_required_backends
 using ClimaUtilities.ClimaArtifacts
@@ -37,20 +25,10 @@ Return an AtmosConfig object for the given member and iteration.
 
 Turns off default diagnostics and sets the TOML parameter file to the member's path.
 This assumes that the  config dictionary has an `output_dir` key.
->>>>>>> 3199df12 (Add calibration framework, perfectmodel experiment)
 """
 function set_up_forward_model(
     member,
     iteration,
-<<<<<<< HEAD
-    experiment_dir::AbstractString,
-)
-    config_dict = YAML.load_file(joinpath(experiment_dir, "model_config.yml"))
-    if haskey(config_dict, "restart_file")
-        config_dict["restart_file"] =
-            joinpath(experiment_dir, config_dict["restart_file"])
-    end
-=======
     ::ExperimentConfig;
     experiment_dir = dirname(Base.active_project()),
 )
@@ -65,7 +43,6 @@ function set_up_forward_model(member, iteration, experiment_dir::AbstractString)
 end
 
 function set_up_forward_model(member, iteration, config_dict::AbstractDict)
->>>>>>> 3199df12 (Add calibration framework, perfectmodel experiment)
     output_dir = config_dict["output_dir"]
     member_path = path_to_ensemble_member(output_dir, iteration, member)
     config_dict["output_dir"] = member_path
@@ -78,41 +55,21 @@ function set_up_forward_model(member, iteration, config_dict::AbstractDict)
 
     # Turn off default diagnostics
     config_dict["output_default_diagnostics"] = false
-<<<<<<< HEAD
-
-=======
->>>>>>> 3199df12 (Add calibration framework, perfectmodel experiment)
     return CA.AtmosConfig(config_dict)
 end
 
 """
     run_forward_model(atmos_config::CA.AtmosConfig)
 
-<<<<<<< HEAD
-Runs the atmosphere model with the given an AtmosConfig object.
-=======
 Run the atmosphere model with the given an AtmosConfig object.
->>>>>>> 3199df12 (Add calibration framework, perfectmodel experiment)
 Currently only has basic error handling.
 """
 function run_forward_model(atmos_config::CA.AtmosConfig)
     simulation = CA.get_simulation(atmos_config)
     sol_res = CA.solve_atmos!(simulation)
     if sol_res.ret_code == :simulation_crashed
-<<<<<<< HEAD
-<<<<<<< HEAD
-        !isnothing(sol_res.sol) && sol_res.sol .= eltype(sol_res.sol)(NaN)
-=======
-        !isnothing(sol_res.sol) && sol_res.sol .= NaN
->>>>>>> 3199df12 (Add calibration framework, perfectmodel experiment)
-=======
->>>>>>> 39f292de (Remove conditional in calibration model interface)
         error(
             "The ClimaAtmos simulation has crashed. See the stack trace for details.",
         )
     end
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 3199df12 (Add calibration framework, perfectmodel experiment)
