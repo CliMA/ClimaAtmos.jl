1265b075acbef75dd0ab8c6a83f1397af0c3f618
costachris <christopouloscosta@gmail.com>
2024-09-25 19:29:02 -0700

    Add external forcing types to gcm-driven scm calibration, allowing for both shallow and deep convection. Includes option for defining stretched calibration grid.
    Add cal grid to edmf_ensemble_stats
    
    gcm driven calibration updates: restart, plotting support for variable grids, add plot prior and normalize var scripts, add microphysics cal option
    
    Add NN mixing length closure, add load pretrain weights logic in prior
    
    Add clippings for NN inputs, dev calibration
    
    Add scm_runner tools to run SCMs (in parallel) across cases for a given parameter set
    
    scm runner updates
    
    limit nn mixing length by smag length + (1/z), and add new dz input variable. Remove problematic deep convective cases from library. Update minibatcher to specify cases for each epoch. Add serialize_std_model with default in run_cal to allow different priors for NN weights and biases. Increase request limits for top-level slurm sbatch script, preventing minor memory leak from killing calibration. Increase noise by an order of mag. Update, improve restart calibration + allow to work with NNs. Limit number of processes per ensemble member by 5 to avoid queue waits .
    
    Add back deep deep conv cases, clip ql, qi above 0 in les cases. Fix get_optimal_particle nearest neighbor mean getter. Simplify nn prior creation at beginning of run_calibration. Request less time on nodes for scm runs.
    
    Limit num_cpu to 5 in restart script. Increase noise, use batch size = 20. And cfsite info to plot_ensemble plots. Add radiation metrics to runner model config. Add nn_helper functions. Add precal prior toml.
    
    Increase noise and batch size (30). Increase prior sigma for NN (0.05 for weights). Lower prior std for mixing_length_diss_coeff. Turn off accelerator.
    
    Make shallow library default in get_les_metadata. Make leaky relu NN default. Change default noise with leaky relu (std_weight = 0.1, std_bias = 0.00001). Increase default runtime for restart for ens members to 180 mins. Increase resource requests for scm_runner.
    
    Make leaky relu NN default (and write to scratch). Pass args to scm_runner in sbatch script. Add CLI option for scm_runner, more flexible parsing of cfsite (for different AMIP experiments).
    
    Switch between NN and standard priors in restart calibration script
    
    Add batch size logic in edmf_ensemble_stats

--- calibration/experiments/gcm_driven_scm/model_interface.jl
+++ calibration/experiments/gcm_driven_scm/model_interface.jl
@@ -3,8 +3,9 @@ import YAML
 import ClimaComms
 @static pkgversion(ClimaComms) >= v"0.6" && ClimaComms.@import_required_backends
 using ClimaUtilities.ClimaArtifacts
-import ClimaCalibrate:
-    set_up_forward_model, run_forward_model, path_to_ensemble_member
+import ClimaCalibrate: path_to_ensemble_member
+
+import ClimaCalibrate as CAL
 import EnsembleKalmanProcesses as EKP
 using JLD2
 
@@ -18,19 +19,20 @@ const output_dir = experiment_config_dict["output_dir"]
 const model_config = experiment_config_dict["model_config"]
 const batch_size = experiment_config_dict["batch_size"]
 
+@everywhere function run_atmos_simulation(atmos_config)
+    simulation = CA.get_simulation(atmos_config)
+    sol_res = CA.solve_atmos!(simulation)
+    if sol_res.ret_code == :simulation_crashed
+        !isnothing(sol_res.sol) && sol_res.sol .= eltype(sol_res.sol)(NaN)
+        error(
+            "The ClimaAtmos simulation has crashed. See the stack trace for details.",
+        )
+    end
+end
 
-"""
-    set_up_forward_model(member, iteration, experiment_dir::AbstractString)
-    set_up_forward_model(member, iteration, config_dict::AbstractDict)
-
-Return an AtmosConfig object for the given member and iteration.
-
-Turns off default diagnostics and sets the TOML parameter file to the member's path.
-This assumes that the  config dictionary has an `output_dir` key.
-"""
-function set_up_forward_model(member, iteration, experiment_dir::AbstractString)
+function CAL.forward_model(iteration, member; config_dict =  YAML.load_file(joinpath(dirname(Base.active_project()), "experiment_config.yml")))
     experiment_config_dict =
-        YAML.load_file(joinpath(experiment_dir, model_config))
+    YAML.load_file(joinpath(experiment_dir, model_config))
     config_dict = YAML.load_file(joinpath(experiment_dir, model_config))
     iter_path = CAL.path_to_iteration(output_dir, iteration)
     eki = JLD2.load_object(joinpath(iter_path, "eki_file.jld2"))
@@ -50,41 +52,20 @@ function set_up_forward_model(member, iteration, experiment_dir::AbstractString)
         config["external_forcing_file"] = get_forcing_file(i, ref_paths)
         config["cfsite_number"] = get_cfsite_id(i, cfsite_numbers)
         config["output_dir"] = joinpath(member_path, "config_$i")
+        config["external_forcing_type"] = get_cfsite_type(i, cfsite_numbers)
         comms_ctx = ClimaComms.SingletonCommsContext()
         CA.AtmosConfig(config; comms_ctx)
     end
 
-    return atmos_configs
-end
-
-
-addprocs(batch_size)
-@everywhere begin
-    import ClimaAtmos as CA
-    using JLD2
-end
-
-@everywhere function run_atmos_simulation(atmos_config)
-    simulation = CA.get_simulation(atmos_config)
-    sol_res = CA.solve_atmos!(simulation)
-    if sol_res.ret_code == :simulation_crashed
-        !isnothing(sol_res.sol) && sol_res.sol .= eltype(sol_res.sol)(NaN)
-        error(
-            "The ClimaAtmos simulation has crashed. See the stack trace for details.",
-        )
-    end
-end
-
-function run_forward_model(atmos_configs)
     @info "Preparing to run $(length(atmos_configs)) model simulations in parallel."
     println("Number of workers: ", nprocs())
 
     start_time = time()
-
-    pmap(run_atmos_simulation, atmos_configs)
-
+    map(run_atmos_simulation, atmos_configs)
     end_time = time()
+
     elapsed_time = (end_time - start_time) / 60.0
 
     @info "Finished all model simulations. Total time taken: $(elapsed_time) minutes."
+    
 end
