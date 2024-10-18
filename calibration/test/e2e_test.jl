54b9a00b64764c2ad124ec9dcdf668e81ee82d27
nefrathenrici <nat.henrici@gmail.com>
2024-10-18 12:19:53 -0700

    Fix calibration test for 1.11, clean up

--- calibration/test/e2e_test.jl
+++ calibration/test/e2e_test.jl
@@ -18,6 +18,87 @@ using Test
 
 using Dates
 
+# Debug plots
+function scatter_plot(eki::EKP.EnsembleKalmanProcess)
+    f = CairoMakie.Figure(resolution = (800, 600))
+    ax = CairoMakie.Axis(
+        f[1, 1],
+        ylabel = "Parameter Value",
+        xlabel = "Top of atmosphere radiative SW flux",
+    )
+
+    g = vec.(EKP.get_g(eki; return_array = true))
+    params = vec.((EKP.get_ϕ(prior, eki)))
+
+    for (gg, uu) in zip(g, params)
+        CairoMakie.scatter!(ax, gg, uu)
+    end
+
+    CairoMakie.hlines!(ax, [astronomical_unit], linestyle = :dash)
+    CairoMakie.vlines!(ax, observations, linestyle = :dash)
+
+    output = joinpath(output_dir, "scatter.png")
+    CairoMakie.save(output, f)
+    return output
+end
+
+function param_versus_iter_plot(eki::EKP.EnsembleKalmanProcess)
+    f = CairoMakie.Figure(resolution = (800, 600))
+    ax = CairoMakie.Axis(
+        f[1, 1],
+        ylabel = "Parameter Value",
+        xlabel = "Iteration",
+    )
+    params = EKP.get_ϕ(prior, eki)
+    for (i, param) in enumerate(params)
+        CairoMakie.scatter!(ax, fill(i, length(param)), vec(param))
+    end
+
+    CairoMakie.hlines!(ax, [astronomical_unit]; color = :red, linestyle = :dash)
+
+    output = joinpath(output_dir, "param_vs_iter.png")
+    CairoMakie.save(output, f)
+    return output
+end
+
+# Observation map
+function CAL.observation_map(iteration)
+    single_member_dims = (1,)
+    G_ensemble = Array{Float64}(undef, single_member_dims..., ensemble_size)
+
+    for m in 1:ensemble_size
+        member_path = CAL.path_to_ensemble_member(output_dir, iteration, m)
+        simdir_path = joinpath(member_path, "output_active")
+        if isdir(simdir_path)
+            simdir = SimDir(simdir_path)
+            G_ensemble[:, m] .= process_member_data(simdir)
+        else
+            G_ensemble[:, m] .= NaN
+        end
+    end
+    return G_ensemble
+end
+
+function process_member_data(simdir::SimDir)
+    isempty(simdir.vars) && return NaN
+    rsut =
+        get(simdir; short_name = "rsut", reduction = "average", period = "30d")
+    return slice(average_xy(rsut); time = 30).data
+end
+
+# EKI test
+function minimal_eki_test(eki)
+    params = EKP.get_ϕ(prior, eki)
+    spread = map(var, params)
+
+    # Spread should be heavily decreased as particles have converged
+    @test last(spread) / first(spread) < 0.1
+    # Parameter should be close to true value
+    @test mean(last(params)) ≈ astronomical_unit rtol = 0.02
+end
+
+# Script:
+
 if !(@isdefined backend)
     backend = CAL.get_backend()
 end
@@ -54,32 +135,7 @@ const model_interface =
     joinpath(pkgdir(CA), "calibration", "model_interface.jl")
 const output_dir = "calibration_end_to_end_test"
 include(model_interface)
-ensemble_size = 10
-
-# Observation map
-function CAL.observation_map(iteration)
-    single_member_dims = (1,)
-    G_ensemble = Array{Float64}(undef, single_member_dims..., ensemble_size)
-
-    for m in 1:ensemble_size
-        member_path = CAL.path_to_ensemble_member(output_dir, iteration, m)
-        simdir_path = joinpath(member_path, "output_active")
-        if isdir(simdir_path)
-            simdir = SimDir(simdir_path)
-            G_ensemble[:, m] .= process_member_data(simdir)
-        else
-            G_ensemble[:, m] .= NaN
-        end
-    end
-    return G_ensemble
-end
-
-function process_member_data(simdir::SimDir)
-    isempty(simdir.vars) && return NaN
-    rsut =
-        get(simdir; short_name = "rsut", reduction = "average", period = "30d")
-    return slice(average_xy(rsut); time = 30).data
-end
+ensemble_size = 15
 
 # Generate observations
 obs_path = joinpath(experiment_dir, "observations.jld2")
@@ -97,7 +153,7 @@ end
 astronomical_unit = 149_597_870_000
 observations = JLD2.load_object(obs_path)
 noise = 0.1 * I
-n_iterations = 3
+n_iterations = 4
 prior = CAL.get_prior(joinpath(experiment_dir, "prior.toml"))
 experiment_config = CAL.ExperimentConfig(;
     n_iterations,
@@ -107,17 +163,6 @@ experiment_config = CAL.ExperimentConfig(;
     output_dir,
     prior,
 )
-
-# Minimal EKI test
-function minimal_eki_test(eki)
-    params = EKP.get_u(eki)
-    spread = map(x -> var(abs.(x)), params)
-
-    # Spread should be heavily decreased as particles have converged
-    @test last(spread) / first(spread) < 0.1
-    # Parameter should be close to true value
-    @test mean(last(params)) ≈ astronomical_unit rtol = 0.02
-end
 
 @info "Running calibration E2E test" backend
 if backend <: CAL.HPCBackend
@@ -131,6 +176,10 @@ if backend <: CAL.HPCBackend
 else
     test_eki = CAL.calibrate(backend, experiment_config)
 end
+
+scatter_plot(test_eki)
+param_versus_iter_plot(test_eki)
+
 @testset "Test Calibration on $backend" begin
     minimal_eki_test(test_eki)
 end
@@ -147,50 +196,3 @@ end
         @test uu ≈ slurm_uu rtol = 0.02
     end
 end
-
-# Debug plots
-function scatter_plot(eki::EKP.EnsembleKalmanProcess)
-    f = CairoMakie.Figure(resolution = (800, 600))
-    ax = CairoMakie.Axis(
-        f[1, 1],
-        ylabel = "Absolute Parameter Value",
-        xlabel = "Top of atmosphere radiative SW flux",
-    )
-
-    g = vec.(EKP.get_g(eki; return_array = true))
-    params = map(x -> abs.(x), vec.((EKP.get_u(eki))))
-
-    for (gg, uu) in zip(g, params)
-        CairoMakie.scatter!(ax, gg, uu)
-    end
-
-    CairoMakie.hlines!(ax, [astronomical_unit], linestyle = :dash)
-    CairoMakie.vlines!(ax, observations, linestyle = :dash)
-
-    output = joinpath(output_dir, "scatter.png")
-    CairoMakie.save(output, f)
-    return output
-end
-
-scatter_plot(test_eki)
-
-function param_versus_iter_plot(eki::EKP.EnsembleKalmanProcess)
-    f = CairoMakie.Figure(resolution = (800, 600))
-    ax = CairoMakie.Axis(
-        f[1, 1],
-        ylabel = "Parameter Value",
-        xlabel = "Iteration",
-    )
-    params = EKP.get_u(eki)
-    for (i, param) in enumerate(params)
-        CairoMakie.scatter!(ax, fill(i, length(param)), vec(param))
-    end
-
-    CairoMakie.hlines!(ax, [astronomical_unit]; color = :red, linestyle = :dash)
-
-    output = joinpath(output_dir, "param_vs_iter.png")
-    CairoMakie.save(output, f)
-    return output
-end
-
-param_versus_iter_plot(test_eki)
