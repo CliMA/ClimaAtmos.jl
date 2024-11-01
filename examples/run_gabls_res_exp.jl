0a1d34c055236e5d2c0abe1110d262f7af816986
Julian Schmitt <jschmitt@caltech.edu>
2024-11-01 10:39:06 -0700

    gabls 9 hr with truncation

--- examples/run_gabls_res_exp.jl
+++ examples/run_gabls_res_exp.jl
@@ -3,13 +3,13 @@ using Revise
 import ClimaAtmos as CA
 import ClimaAnalysis: window
 
-include("../post_processing/ci_plots.jl")
+includet("../post_processing/ci_plots.jl")
 
-config_dict = YAML.load_file("../config/model_configs/prognostic_edmfx_gabls_column.yml")
+config_dict = YAML.load_file("prognostic_edmfx_gabls_column.yml")
 
-for resolution in [3,5,8]#[10, 20, 30, 40, 60, 80, 100, 120, 150, 200, 300]
+for resolution in [10, 20, 30, 40, 60, 80, 100, 120, 150, 200, 300, 500]
     
-    config_dict["output_dir"] = "output/gabls_test2/resolution_$resolution"
+    config_dict["output_dir"] = "output/gabls_test3/resolution_$resolution"
     # change cfsite number for each site for correct forcing
     config_dict["z_elem"] = resolution
     config_dict["netcdf_interpolation_num_points"] = [2, 2, resolution]
@@ -19,5 +19,5 @@ for resolution in [3,5,8]#[10, 20, 30, 40, 60, 80, 100, 120, 150, 200, 300]
     simulation = CA.get_simulation(config)
     sol_res = CA.solve_atmos!(simulation)
 
-    make_plots(Val{:gabls_test}(), "output/gabls_test2/resolution_$resolution/output_active")
+    make_plots(Val{:gabls_test}(), "output/gabls_test3/resolution_$resolution/output_active")
 end
