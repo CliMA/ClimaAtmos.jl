Fix derivative error in tendencies and integration time in s/lhf in forcing files and stabilize model

calibration/experiments/reanalysis_forcing/data_processing/era5_to_cfsite_forcing.py
@@ -2,7 +2,7 @@
 # Need to change file pathing/ define coszen data (see other scripts for computation)
 # also need to change short wave period to 1 hour for hourly data and 1 day for monthly data (ugh)
 
-tisr_resolution = 86400 # for monthly data; change to 3600 for hourly data
+time_resolution = 86400 # for monthly data; change to 3600 for hourly data
 
 import xarray as xr 
 import numpy as np
@@ -16,8 +16,9 @@ lon_max = 290
 
 # extract locations and pick sites (lets just look between midlatitudes for now)
 geo = xr.open_dataset("coszen_data.nc")
+pressure_avg = xr.open_dataset("~/Downloads/era5/monthly/pressure_avg_monthly_45.nc") # already preprocessed since so big
 surface = xr.open_mfdataset("/Users/julianschmitt/Downloads/era5/monthly/global_surface_20yr.nc")
-pressure = xr.open_mfdataset("/Users/julianschmitt/Downloads/era5/monthly/global_pressure_20yr.nc")
+# pressure = xr.open_mfdataset("/Users/julianschmitt/Downloads/era5/monthly/global_pressure_20yr.nc")
 
 
 def get_horizontal_tendencies(lon, lat, column_ds):
@@ -38,14 +39,14 @@ def get_horizontal_tendencies(lon, lat, column_ds):
     lat = np.deg2rad(lat)
     lon = np.deg2rad(lon - 360)
     coslat = np.cos(lat)
-    dx = 2 * np.pi * rearth * coslat / 360 # per degree longitude
-    dy = 2 * np.pi * rearth / 360 # per degree latitude
+    dx = 2 * np.pi * rearth * coslat / 360 * .25# per .25 degree longitude 
+    dy = 2 * np.pi * rearth / 360 * .25# per .25 degree latitude
 
     # compute advective tendencies
     # Temperature
-    tntha = center.u * (west.t - 2 * center.t + east.t) / (2 * dx / 4) + center.v * (south.t - 2 * center.t + north.t) / (2 * dy / 4)
+    tntha = center.u * (east.t - west.t) / (2 * dx) + center.v * (north.t - south.t) / (2 * dy)
     # specific humidity
-    tnhusha = center.u * (west.q - 2 * center.q + east.q) / (2 * dx / 4) + center.v * (south.q - 2 * center.q + north.q) / (2 * dy / 4)
+    tnhusha = center.u * (east.q - west.q) / (2 * dx) + center.v * (north.q - south.q) / (2 * dy)
 
     return tntha, tnhusha
 
@@ -54,7 +55,7 @@ def get_horizontal_tendencies(lon, lat, column_ds):
 def get_vertical_tendencies(column_ds, var, vertvar = "wa"):
     """
     Calculate the temperature and specific humidity vertical tendencies as a function of levels
-    using vertical advection.
+    using vertical advection. Here we take the tendency over the geopotential height not the height in meters
     """
     tntva_trend = []
     # Loop through each pressure level
@@ -62,13 +63,13 @@ def get_vertical_tendencies(column_ds, var, vertvar = "wa"):
 
     for i in range(num_levels):
         if i == 0:  # Bottom level (forward difference)
-            tntva = column_ds[vertvar][i] * (column_ds[var][i+1] - column_ds[var][i]) / (column_ds.z[i+1] - column_ds.z[i])
+            tntva = column_ds[vertvar][i] * (column_ds[var][i+1] - column_ds[var][i]) / (column_ds.zg[i+1] - column_ds.zg[i])
         
         elif i == num_levels - 1:  # Top level (backward difference)
-            tntva = column_ds[vertvar][i] * (column_ds[var][i] - column_ds[var][i-1]) / (column_ds.z[i] - column_ds.z[i-1])
+            tntva = column_ds[vertvar][i] * (column_ds[var][i] - column_ds[var][i-1]) / (column_ds.zg[i] - column_ds.zg[i-1])
         
         else:  # Middle levels (surface_dsed difference)
-            tntva = column_ds[vertvar][i] * (column_ds[var][i+1] - 2 * column_ds[var][i] +  column_ds[var][i-1]) / ((column_ds.z[i+1] - column_ds.z[i-1]))
+            tntva = column_ds[vertvar][i] * (column_ds[var][i+1] - column_ds[var][i-1]) / ((column_ds.zg[i+1] - column_ds.zg[i-1]))
 
         tntva = tntva.assign_coords(pressure_level=column_ds.pressure_level[i])
 
@@ -86,8 +87,8 @@ def get_forcing_data(cfsite, column_ds, surface_ds, geo = geo):
     lat = np.round(loc.lat.values[0] / .25) * .25
     lon = np.round(loc.lon.values[0] / .25) * .25
 
-    sitesf = surface_ds.where((surface_ds.latitude == lat) & (surface_ds.longitude == lon - 360), drop = True)
-    sitecol = column_ds.where((column_ds.latitude == lat) & (column_ds.longitude == lon - 360), drop = True)
+    sitesf = surface_ds.where((surface_ds.latitude == lat) & (surface_ds.longitude == lon), drop = True)
+    sitecol = column_ds.where((column_ds.latitude == lat) & (column_ds.longitude == lon), drop = True)
 
     ##### get column data #####
     # compute temperature
@@ -106,17 +107,21 @@ def get_forcing_data(cfsite, column_ds, surface_ds, geo = geo):
     coszen = geo.where(geo["site"] == cfsite, drop = True)
 
     # rescale TOA incident radiation to w/m2 by dividing by the time step of ERA5 (1 hour)
-    sitesf["tisr"] = sitesf["tisr"] / tisr_resolution
+    sitesf["tisr"] = sitesf["tisr"] / time_resolution
 
     #### Combine data ####
     sitedata = xr.merge([sitecol[["z", "t", "rho", "u", "v", "w", "q"]], 
                          sitesf[["slhf", "sshf", "tisr", "skt"]],
                          coszen[["coszen"]]])
+    
+    # convert sensible and latent heat fluxes to W/m2; flip sign to match climaatmos practices 
+    sitedata["slhf"] = - sitedata["slhf"] / time_resolution
+    sitedata["sshf"] = - sitedata["sshf"] / time_resolution
 
     sitedata = sitedata.rename({"t": "ta", "u": "ua", "v": "va", "w": "wa", "q": "hus", "slhf": "hfls", "sshf": "hfss", "skt": "ts", "tisr": "rsdt", "z": "zg"})
     sitedata["z"] = sitedata["zg"] / g # convert geopotential (zg) to height in meters (z)
 
-    # remove latitude/longitude dependence - not actually selecting a value on the meridian, just the first value of the array
+    # remove latitude/longitude dependence
     sitedata = sitedata.squeeze()
 
     # calculate tendency terms
@@ -136,22 +141,22 @@ def get_forcing_data(cfsite, column_ds, surface_ds, geo = geo):
     return sitedata
 
 # process data to region of interest for some computational efficiency (for now)
-pressure = pressure.where((pressure.latitude > lat_min) & (pressure.latitude < lat_max) & (pressure.longitude > lon_min) & (pressure.longitude < lon_max), drop =True)
+# pressure = pressure.where((pressure.latitude > lat_min) & (pressure.latitude < lat_max) & (pressure.longitude > lon_min) & (pressure.longitude < lon_max), drop =True)
 surface = surface.where((surface.latitude > lat_min) & (surface.latitude < lat_max) & (surface.longitude > lon_min) & (surface.longitude < lon_max), drop =True)
 geo = geo.where((geo.lat > lat_min) & (geo.lat < lat_max) & (geo.lon > lon_min) & (geo.lon < lon_max), drop =True)
 
-# monthly time mean
-pressure["date"] = pd.to_datetime(pressure["date"].astype(str), format="%Y%m%d")
+# # monthly time mean
+# pressure["date"] = pd.to_datetime(pressure["date"].astype(str), format="%Y%m%d")
 surface["date"] = pd.to_datetime(surface["date"].astype(str), format="%Y%m%d")
-pressure_avg = pressure.groupby("date.month").mean()
+# pressure_avg = pressure.groupby("date.month").mean()
 surface_avg = surface.groupby("date.month").mean()
 
-# compute
+# # compute
 surface_avg = surface_avg.load()
-pressure_avg = pressure_avg.load()
+# pressure_avg = pressure_avg.load()
 
 # loop through months
-for month in [1, 4, 7, 10]:
+for month in range(1, 2):
     output_file = f'era5_monthly_forcing_{month}.nc'
 
     pressure_ds = pressure_avg.sel(month = month, drop = True)
@@ -160,8 +165,11 @@ for month in [1, 4, 7, 10]:
 
     for site_id in geo_ds.site.values:
         print("Running site: ", site_id)
-        site_data = get_forcing_data(site_id, pressure_ds, surface_ds, geo_ds)
-        site_data.to_netcdf(output_file, mode='a', group=f'site{site_id}')
+        try:
+            site_data = get_forcing_data(site_id, pressure_ds, surface_ds, geo_ds)
+            site_data.to_netcdf(output_file, mode='a', group=f'site{site_id}')
+        except:
+            print("Error processing site: ", site_id)
 
 
 