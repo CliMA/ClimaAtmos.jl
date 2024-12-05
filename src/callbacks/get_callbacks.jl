6f7883dc8c3477752447592a12c86d6080612d9f
Gabriele Bozzola <gbozzola@caltech.edu>
2024-12-05 09:05:40 -0800

    Add dates to diagnostics
    Removing `p.start_date` meant that the diagnostics no longer have dates
    by default. Here, I add it back.
    
    Failure here: https://buildkite.com/clima/climacoupler-coarse-nightly-amip/builds/170#0193948c-b940-4532-b497-c3ce4ec2991e

--- src/callbacks/get_callbacks.jl
+++ src/callbacks/get_callbacks.jl
@@ -48,12 +48,18 @@ function get_diagnostics(
         parsed_args["netcdf_output_at_levels"] ? CAD.LevelsMethod() :
         CAD.FakePressureLevelsMethod()
 
+    # The start_date keyword was added in v0.2.9. For prior versions, the diagnostics will
+    # not contain the date
+    maybe_add_start_date =
+        pkgversion(CAD.ClimaDiagnostics) >= v"0.2.9" ? (; start_date) : (;)
+
     netcdf_writer = CAD.NetCDFWriter(
         axes(Y.c),
         output_dir,
         num_points = num_netcdf_points;
         z_sampling_method,
         sync_schedule = CAD.EveryStepSchedule(),
+        maybe_add_start_date...,
     )
     writers = (hdf5_writer, netcdf_writer)
 
