48b1d79339a0227c7df1f48827f10ca1206c0295
imreddyTeja <treddy0205@gmail.com>
2024-12-23 15:37:05 -0800

    Change topo_drag.res.nc to a ClimaArtifact

--- artifacts/download_artifacts.jl
+++ artifacts/download_artifacts.jl
@@ -4,7 +4,6 @@ include(joinpath(@__DIR__, "artifact_funcs.jl"))
 function trigger_download(lazy_download = true)
     @info "Era global dataset path:`$(era_global_dataset_path())`"
     @info "Era single column dataset path:`$(era_single_column_dataset_path())`"
-    @info "topo dataset path:`$(topo_res_path())`"
     @info "MiMA convective gravity wave path:`$(mima_gwf_path())`"
     @info "GFDL OGWD test data:`$(gfdl_ogw_data_path())`"
     return nothing
