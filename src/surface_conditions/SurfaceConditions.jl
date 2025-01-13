cd74d00edb7df1ef1d8eb7755df3fd402d0a1121
Julian Schmitt <jschmitt@caltech.edu>
2025-01-13 09:19:17 -0800

    adds era5 driven to pr
    hpc edits and formatter
    
    fix naming

--- src/surface_conditions/SurfaceConditions.jl
+++ src/surface_conditions/SurfaceConditions.jl
@@ -9,6 +9,7 @@ import ..RCEMIPIISST
 import ..PrognosticSurfaceTemperature
 import ..PrescribedSurfaceTemperature
 import ..gcm_driven_timeseries
+import ..era5_driven_timeseries
 
 import ..CT1, ..CT2, ..C12, ..CT12, ..C3
 import ..unit_basis_vector_data, ..projected_vector_data
