0a5db99d4ca2c0b97fcfe01a78b532848861d21a
nefrathenrici <nat.henrici@gmail.com>
2025-01-17 16:37:57 -0800

    Fix group tests

--- test/groups/conservation_check.jl
+++ test/groups/conservation_check.jl
@@ -1,14 +1,14 @@
 import ClimaAtmos as CA
 
 jobs = (
-    "config/model_config/sphere_baroclinic_wave_conservation.yml",
-    "sphere_baroclinic_wave_conservation",
-    "config/model_config/sphere_baroclinic_wave_equilmoist_conservation.yml",
-    "sphere_baroclinic_wave_equilmoist_conservation",
-    "config/model_config/sphere_baroclinic_wave_equilmoist_conservation_ft64.yml",
-    "sphere_baroclinic_wave_equilmoist_conservation_ft64",
-    "config/model_config/sphere_baroclinic_wave_equilmoist_conservation_source.yml",
-    "sphere_baroclinic_wave_equilmoist_conservation_source",
+    ("config/model_config/sphere_baroclinic_wave_conservation.yml",
+    "sphere_baroclinic_wave_conservation"),
+    ("config/model_config/sphere_baroclinic_wave_equilmoist_conservation.yml",
+    "sphere_baroclinic_wave_equilmoist_conservation"),
+    ("config/model_config/sphere_baroclinic_wave_equilmoist_conservation_ft64.yml",
+    "sphere_baroclinic_wave_equilmoist_conservation_ft64"),
+    ("config/model_config/sphere_baroclinic_wave_equilmoist_conservation_source.yml",
+    "sphere_baroclinic_wave_equilmoist_conservation_source"),
 )
 
 include(joinpath(pkgdir(CA), "test", "groups", "group_driver.jl"))
