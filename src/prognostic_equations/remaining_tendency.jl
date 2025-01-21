b2c8b3745d855c1b711d0d60444e91e66dbbed43
Dennis Yatunin <dyatun@gmail.com>
2025-01-21 14:34:30 -0800

    Test whether ClimaTimeSteppers update fixed DSS

--- src/prognostic_equations/remaining_tendency.jl
+++ src/prognostic_equations/remaining_tendency.jl
@@ -10,7 +10,45 @@ NVTX.@annotate function hyperdiffusion_tendency!(Yₜ, Yₜ_lim, Y, p, t)
     apply_hyperdiffusion_tendency!(Yₜ, Y, p, t)
 end
 
+using ClimaCore.MatrixFields
+scalar_field_names(fv) =
+    filtered_names(x -> x isa Fields.Field && eltype(x) == eltype(fv), fv)
+filtered_names(f::F, x) where {F} = filtered_names_at_name(f, x, @name())
+function filtered_names_at_name(f::F, x, name) where {F}
+    field = MatrixFields.get_field(x, name)
+    f(field) && return (name,)
+    internal_names = MatrixFields.top_level_names(field)
+    isempty(internal_names) && return ()
+    tuples_of_names = MatrixFields.unrolled_map(internal_names) do internal_name
+        Base.@_inline_meta
+        child_name = MatrixFields.append_internal_name(name, internal_name)
+        filtered_names_at_name(f, x, child_name)
+    end
+    return MatrixFields.unrolled_flatten(tuples_of_names)
+end
+if hasfield(Method, :recursion_relation)
+    dont_limit = (args...) -> true
+    for m in methods(filtered_names_at_name)
+        m.recursion_relation = dont_limit
+    end
+end
+
 NVTX.@annotate function remaining_tendency!(Yₜ, Yₜ_lim, Y, p, t)
+    Y_copy = Yₜ
+    Y_copy .= Y
+    dss!(Y_copy, p, t)
+    if Y_copy != Y
+        for name in scalar_field_names(Y)
+            field = MatrixFields.get_field(Y, name)
+            field_copy = MatrixFields.get_field(Y_copy, name)
+            max_error = maximum(abs.(field_copy .- field))
+            mean_value = mean(field)
+            @info "$(rpad(name, 32)) at $(rpad(t, 9)): $max_error ($mean_value)"
+        end
+        println()
+        t > 5 * p.dt && error("Stopping due to failed DSS check")
+    end
+
     Yₜ_lim .= zero(eltype(Yₜ_lim))
     Yₜ .= zero(eltype(Yₜ))
     horizontal_tracer_advection_tendency!(Yₜ_lim, Y, p, t)
