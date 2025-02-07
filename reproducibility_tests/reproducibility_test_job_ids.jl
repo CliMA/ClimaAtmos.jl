6dcf6b084ef747701fe5acc94ee2ab8fa137d73c
Zhaoyi Shen <11598433+szy21@users.noreply.github.com>
2025-02-06 20:13:18 -0800

    clean up configs

--- reproducibility_tests/reproducibility_test_job_ids.jl
+++ reproducibility_tests/reproducibility_test_job_ids.jl
@@ -3,7 +3,6 @@ reproducibility_test_job_ids() = [
     "sphere_held_suarez_rhoe_equilmoist_hightop_sponge",
     "sphere_aquaplanet_rhoe_nonequilmoist_allsky_gw_res",
     "sphere_aquaplanet_rhoe_equilmoist_allsky_gw_raw_zonallyasymmetric",
-    "deep_sphere_baroclinic_wave_rhoe_equilmoist",
     "diagnostic_edmfx_aquaplanet",
     "single_column_hydrostatic_balance_ft64",
 ]
