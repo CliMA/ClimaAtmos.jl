db7eff7cb187eb3d1728b85bb6631c22c9d9ec79
Anna Jaruga <ajaruga@caltech.edu>
2025-02-24 13:48:00 -0800

    comment out horizontal precip adv

--- src/prognostic_equations/advection.jl
+++ src/prognostic_equations/advection.jl
@@ -57,7 +57,11 @@ NVTX.@annotate function horizontal_tracer_advection_tendency!(Yₜ, Y, p, t)
     end
 
     for ρχ_name in filter(is_tracer_var, propertynames(Y.c))
-        @. Yₜ.c.:($$ρχ_name) -= wdivₕ(Y.c.:($$ρχ_name) * ᶜu)
+        if ρχ_name in (:ρq_rai, :ρq_sno)
+            nothing
+        else
+            @. Yₜ.c.:($$ρχ_name) -= wdivₕ(Y.c.:($$ρχ_name) * ᶜu)
+        end
     end
 
     if p.atmos.turbconv_model isa PrognosticEDMFX
