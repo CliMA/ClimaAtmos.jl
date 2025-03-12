848e0c11f9a7f0748a1501a0e445a2ec2df01f26
Akshay Sridhar <akshaysridhar@169-231-151-77.wireless.ucsb.edu>
2025-03-12 09:48:39 -0700

    modified:   config/model_configs/baroclinic_wave.yml 	modified:   src/initial_conditions/initial_conditions.jl

--- src/initial_conditions/initial_conditions.jl
+++ src/initial_conditions/initial_conditions.jl
@@ -516,8 +516,9 @@ function deep_atmos_baroclinic_wave_values(z, ϕ, λ, params, perturb)
     R_d = CAP.R_d(params)
     MSLP = CAP.MSLP(params)
     grav = CAP.grav(params)
-    Ω = CAP.Omega(params)
-    R = CAP.planet_radius(params)
+    𝒳 = FT(20)
+    Ω = CAP.Omega(params)  * 𝒳
+    R = CAP.planet_radius(params) / 𝒳
 
     # Constants from paper (See Table 1. in Ullrich et al (2014))
     k = 3         # Power for temperature field
