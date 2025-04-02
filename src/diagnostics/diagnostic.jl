c83173e719a55630daaaddf204ecf8157957ac7f
Anna Jaruga <ajaruga@caltech.edu>
2025-04-02 15:10:10 -0700

    tmp add microphysics tendencies diagnostics

--- src/diagnostics/diagnostic.jl
+++ src/diagnostics/diagnostic.jl
@@ -100,6 +100,7 @@ include("edmfx_diagnostics.jl")
 include("tracer_diagnostics.jl")
 include("gravitywave_diagnostics.jl")
 include("conservation_diagnostics.jl")
+include("tendency_diagnostics.jl")
 
 # Default diagnostics and higher level interfaces
 include("default_diagnostics.jl")
