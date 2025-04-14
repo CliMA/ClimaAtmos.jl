c9ab04936d27480c4f9b4286997f98e497e4ac1a
Olivia Alcabes <oliva.alcabes@runningtide.com>
2025-04-14 14:07:43 -0700

    changing to work specifically for baroclinic wave

--- calibration/experiments/microphysics_equil/generate_data.jl
+++ calibration/experiments/microphysics_equil/generate_data.jl
@@ -1,31 +1,33 @@
 ### to go in here:
 # - generate synthetic observation
+redirect_stderr(IOContext(stderr, :stacktrace_types_limited => Ref(false)))
+import ClimaComms
+ClimaComms.@import_required_backends
+import ClimaAtmos as CA
+import Random
+Random.seed!(1234)
 
+using NCDatasets
+using Dates
+using Statistics
+import ClimaAnalysis
+import ClimaAnalysis: Visualize as viz
 
 """
-Generate perfect model simulation. In initial test case, run baroclinic wave w/equil microphysics parameterization
+Generate perfect model simulation. In initial test case, run baroclinic wave
+w/equil microphysics parameterization. Will have pow_ice = 1 be our
+input parameter.
 """
-function generate()
+function generate_bw(config_file, job_id)
     # to do:
     #   - change toml to the updated parameter value (???)
     #   - otherwise looks mostly correct?
 
-    # generate wanted config
-    experiment_config_dict = YAML.load_file(joinpath(experiment_dir, model_config))
-    config_dict = YAML.load_file(joinpath(experiment_dir, model_config))
-    iter_path = CAL.path_to_iteration(output_dir, iteration)
-    eki = JLD2.load_object(joinpath(iter_path, "eki_file.jld2"))
-    member_path = path_to_ensemble_member(output_dir, iteration, member)
-    config_dict["output_dir"] = member_path
-    parameter_path = joinpath(member_path, "parameters.toml")
-    if haskey(config_dict, "toml")
-        push!(config_dict["toml"], parameter_path)
-    else
-        config_dict["toml"] = [parameter_path]
-    end
-    config_dict["output_default_diagnostics"] = false
+    config = CA.AtmosConfig(config_file; job_id)
+    simulation = CA.AtmosSimulation(config)
+    sol_res = CA.solve_atmos!(simulation)
 
-    # run it and save it somewhere?
+    # return sol_res?
 
 end
 
@@ -33,6 +35,65 @@ end
 """
 Add noise to model truth
 """
-function synthetic_observed_y(x_inputs; data_path = "data", apply_noise = false)
-    
+function synthetic_observed_y(sim_path, reduction)
+
+    # - load the model output
+
+    simdir = ClimaAnalysis.SimDir(sim_path)  
+    cli = get(simdir; short_name = "cli", reduction = reduction) 
+    clw = get(simdir; short_name = "clw", reduction = reduction)
+
+    # liquid fraction loss function
+    y = clw ./ (clw .+ cli)
+
+    # - create noise (I need to remember how to make this Gamma.)
+    Γ = FT(0.003)^2 * I * (maximum(y) - minimum(y))
+
+    noise_dist = MvNormal(zeros(1), Γ)
+    apply_noise!(y, noise_dist) = y + rand(noise_dist)[1]
+    # broadcast the noise to each element of y
+    y_noisy = apply_noise!.(y, Ref(noise_dist))
+
+    # my old version of this
+    #Γy = 0.01 * (maximum(y_t) - minimum(y_t)) + 0.0001# * sol[2,:] .+ I(size(sol[2,:], 1))*0.001
+    #Γy = convert(Array, LinearAlgebra.Diagonal([0.01 * sol[1], 0.01 * sol[2]]))
+    #μ = zeros(length(sol))
+
+    #for i in 1:n_samples
+
+        # do for each t in y????
+
+    #    for t in 1:length(sol)
+
+    #        y_t[t,i] = sol[t] + rand(Distributions.Normal(0, Γy))
+
+    #    end
+
+        #Γy = 0.01 * sol[2,:]
+        # generate a bunch of noise
+        #Distributions.MvNormal(μ, Γy)
+        #noise = randn(μ, Γy)
+        #y = sol[2,:] + noise
+        #push!(y_t,y)
+    #end
+
+    # - save noisey output
+    JLD2.save_object(
+        joinpath(sim_path, "bw_equil_slf_noisy.jld2"), # probably dont save that way
+        y_noisy,
+    )
+    return y, y_noisy
+
 end
+
+"""
+write script to run the baroclinic wave model and generate noise
+"""
+
+config_file = "baroclinic_wave_equil.yml"
+job_id = "baroclinic_wave_equil"
+
+generate_bw(config_file, job_id)
+
+#sim_path = ""
+#y, y_noisy = synthetic_observed_y(sim_path, "average")
