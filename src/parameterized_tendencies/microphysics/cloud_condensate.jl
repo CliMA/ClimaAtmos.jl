2a83552d44ef48ea2053ea03e379bf98dd0e3f31
Anna Jaruga <ajaruga@caltech.edu>
2025-04-16 14:27:42 -0700

    try printing columns

--- src/parameterized_tendencies/microphysics/cloud_condensate.jl
+++ src/parameterized_tendencies/microphysics/cloud_condensate.jl
@@ -1,6 +1,7 @@
 #####
 ##### DryModel, EquilMoistModel
 #####
+import ClimaCore: Spaces, Quadratures, Fields
 
 cloud_condensate_tendency!(Yₜ, Y, p, _, _) = nothing
 
@@ -19,6 +20,24 @@ function cloud_condensate_tendency!(
         "NonEquilMoistModel can only be run with Microphysics1Moment precipitation",
     )
 end
+
+function column_iterator_indices(field)
+    axes(field) isa Union{Spaces.PointSpace, Spaces.FiniteDifferenceSpace} &&
+        return ((1, 1, 1),)
+    horz_space = Spaces.horizontal_space(axes(field))
+    qs = 1:Quadratures.degrees_of_freedom(Spaces.quadrature_style(horz_space))
+    hs = Spaces.eachslabindex(horz_space)
+    return horz_space isa Spaces.SpectralElementSpace1D ?
+           Iterators.product(qs, hs) : Iterators.product(qs, qs, hs)
+end
+
+column_iterator_indices(field_vector::Fields.FieldVector) =
+    column_iterator_indices(first(Fields._values(field_vector)))
+
+column_iterator(iterable) =
+    Iterators.map(column_iterator_indices(iterable)) do (indices...,)
+        Fields.column(iterable, indices...)
+    end
 
 function cloud_condensate_tendency!(
     Yₜ,
@@ -43,23 +62,24 @@ function cloud_condensate_tendency!(
     T = p.scratch.ᶜtemp_scalar
     @. T = TD.air_temperature(thp, ᶜts)
     @assert sum(isnan, T) == 0
-    if minimum(T) < FT(200)
-        ρ = p.scratch.ᶜtemp_scalar_2
-        @. ρ = TD.air_density(thp, ᶜts)
 
-        q_tot = p.scratch.ᶜtemp_scalar_3
-        @. q_tot = TD.PhasePartition(thp, ᶜts).tot
+    #if minimum(T) < FT(200)
+    #    ρ = p.scratch.ᶜtemp_scalar_2
+    #    @. ρ = TD.air_density(thp, ᶜts)
 
-        q_liq = p.scratch.ᶜtemp_scalar_4
-        @. q_liq = TD.PhasePartition(thp, ᶜts).liq
+    #    q_tot = p.scratch.ᶜtemp_scalar_3
+    #    @. q_tot = TD.PhasePartition(thp, ᶜts).tot
 
-        q_ice = p.scratch.ᶜtemp_scalar_5
-        @. q_ice = TD.PhasePartition(thp, ᶜts).ice
+    #    q_liq = p.scratch.ᶜtemp_scalar_4
+    #    @. q_liq = TD.PhasePartition(thp, ᶜts).liq
 
-        @info(" ", extrema(ρ), extrema(T))
-        @info(" ", extrema(q_tot), extrema(q_liq), extrema(q_ice))
-        @info(" ", extrema(q_rai), extrema(q_sno))
-    end
+    #    q_ice = p.scratch.ᶜtemp_scalar_5
+    #    @. q_ice = TD.PhasePartition(thp, ᶜts).ice
+
+    #    @info(" ", extrema(ρ), extrema(T))
+    #    @info(" ", extrema(q_tot), extrema(q_liq), extrema(q_ice))
+    #    @info(" ", extrema(q_rai), extrema(q_sno))
+    #end
     @assert minimum(T) > FT(0)
 
     pᵥ_sat_liq = p.scratch.ᶜtemp_scalar_2
@@ -80,6 +100,27 @@ function cloud_condensate_tendency!(
 
     @. p.scratch.tmp_cloud_liquid_src = Y.c.ρ * cloud_sources(cmc.liquid, thp, ᶜts, q_rai, dt)
     @. p.scratch.tmp_cloud_ice_src = Y.c.ρ * cloud_sources(cmc.ice, thp, ᶜts, q_sno, dt)
+
+    for (Y_col, ql_src_col, qi_src_col ,ts_col, c_spec_col) in zip(
+        column_iterator(Y),
+        column_iterator(p.scratch.tmp_cloud_liquid_src),
+        column_iterator(p.scratch.tmp_cloud_ice_src),
+        column_iterator(ᶜts),
+        column_iterator(ᶜspecific),
+   )
+       if minimum(TD.air_temperature.(thp, ts_col)) =< FT(170)
+          @show(Fields.coordinate_field(Y_col.c.ρ))
+          @show(TD.air_temperature.(thp, ts_col))
+          @show(TD.air_density.(thp, ts_col))
+          @show(TD.PhasePartition.(thp, ts_col).tot)
+          @show(TD.PhasePartition.(thp, ts_col).liq)
+          @show(TD.PhasePartition.(thp, ts_col).ice)
+          @show(c_spec_col.q_rai)
+          @show(c_spec_col.q_sno)
+          @show(ql_src_col)
+          @show(qi_src_col)
+      end
+    end
 
     @. Yₜ.c.ρq_liq += Y.c.ρ * cloud_sources(cmc.liquid, thp, ᶜts, q_rai, dt)
     @. Yₜ.c.ρq_ice += Y.c.ρ * cloud_sources(cmc.ice, thp, ᶜts, q_sno, dt)
