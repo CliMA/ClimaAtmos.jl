8e651da035b7eb94d8f7b4702f9b02993f11014a
Olivia Alcabes <oliva.alcabes@runningtide.com>
2025-04-17 16:30:27 -0700

    updating to tutorial

--- calibration/experiments/microphysics_equil/model_interface.jl
+++ calibration/experiments/microphysics_equil/model_interface.jl
@@ -16,53 +16,52 @@ const model_config = experiment_config_dict["model_config"]
 function CAL.forward_model(iteration, member)
     base_config_dict = YAML.load_file(joinpath(@__DIR__, model_config))
 
-    iter_path = CAL.path_to_iteration(output_dir, iteration)
-    eki = JLD2.load_object(joinpath(iter_path, "eki_file.jld2"))
-    member_path = path_to_ensemble_member(output_dir, iteration, member)
-
-    config_dict = deepcopy(base_config_dict)
+    # VERSION COPIED FROM TUTORIAL
+    member_path = CAL.path_to_ensemble_member(output_dir, iteration, member)
     config_dict["output_dir"] = member_path
-    parameter_path = joinpath(member_path, "parameters.toml")
+
+    #md # Set the parameters for the current member
+    parameter_path = CAL.parameter_path(output_dir, iteration, member)
     if haskey(config_dict, "toml")
-        config_dict["toml"] = abspath.(config_dict["toml"])
         push!(config_dict["toml"], parameter_path)
     else
         config_dict["toml"] = [parameter_path]
     end
+
+    #md # Turn off default diagnostics
     config_dict["output_default_diagnostics"] = false
 
-    # ADD JULIANS VERSION ITS MORE UP TO DATE
-
-end
-
-
-
-
-
-
-
-@everywhere function run_atmos_simulation(atmos_config)
+    comms_ctx = ClimaComms.SingletonCommsContext()
+    atmos_config = CA.AtmosConfig(config_dict; comms_ctx)
     simulation = CA.get_simulation(atmos_config)
-    sol_res = CA.solve_atmos!(simulation)
-    if sol_res.ret_code == :simulation_crashed
-        !isnothing(sol_res.sol) && sol_res.sol .= eltype(sol_res.sol)(NaN)
-        error(
-            "The ClimaAtmos simulation has crashed. See the stack trace for details.",
-        )
-    end
+    CA.solve_atmos!(simulation)
+    return simulation
+
 end
 
 
-function run_forward_model(atmos_configs)
-    @info "Preparing to run $(length(atmos_configs)) model simulations in parallel."
-    println("Number of workers: ", nprocs())
+#@everywhere function run_atmos_simulation(atmos_config)
+#    simulation = CA.get_simulation(atmos_config)
+#    sol_res = CA.solve_atmos!(simulation)
+#    if sol_res.ret_code == :simulation_crashed
+#        !isnothing(sol_res.sol) && sol_res.sol .= eltype(sol_res.sol)(NaN)
+#        error(
+#            "The ClimaAtmos simulation has crashed. See the stack trace for details.",
+#        )
+#    end
+#end
 
-    start_time = time()
 
-    pmap(run_atmos_simulation, atmos_configs)
+#function run_forward_model(atmos_configs)
+#    @info "Preparing to run $(length(atmos_configs)) model simulations in parallel."
+#    println("Number of workers: ", nprocs())
 
-    end_time = time()
-    elapsed_time = (end_time - start_time) / 60.0
+#    start_time = time()
 
-    @info "Finished all model simulations. Total time taken: $(elapsed_time) minutes."
-end
+#    pmap(run_atmos_simulation, atmos_configs)
+
+#    end_time = time()
+#    elapsed_time = (end_time - start_time) / 60.0
+
+#    @info "Finished all model simulations. Total time taken: $(elapsed_time) minutes."
+#end
--- calibration/experiments/microphysics_equil/observation_map.jl
+++ calibration/experiments/microphysics_equil/observation_map.jl
@@ -2,6 +2,35 @@
 # - observation_map function
 
 
+
+"""
+version from the TUTORIAL
+"""
+const days = 86_400
+function CAL.observation_map(iteration)
+    single_member_dims = (1,)
+    G_ensemble = Array{Float64}(undef, single_member_dims..., ensemble_size)
+
+    for m in 1:ensemble_size
+        member_path = CAL.path_to_ensemble_member(output_dir, iteration, m)
+        simdir_path = joinpath(member_path, "output_active")
+        if isdir(simdir_path)
+            simdir = SimDir(simdir_path)
+            G_ensemble[:, m] .= process_member_data(simdir)
+        else
+            G_ensemble[:, m] .= NaN
+        end
+    end
+    return G_ensemble
+end
+
+function process_member_data(simdir::SimDir)
+    isempty(simdir.vars) && return NaN
+    rsut =
+        get(simdir; short_name = "rsut", reduction = "average", period = "30d")
+    return slice(average_xy(rsut); time = 30days).data
+end
+
 """
 comparing w the model ground truth. I think Is can probably just use this simple version for now?
 """
