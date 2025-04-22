a6d924f222d386270eeda39808ca1806f4693c1a
Olivia Alcabes <oalcabes@derecho2.hsn.de.hpc.ucar.edu>
2025-04-22 16:22:11 -0600

    updating and fixing typos

--- calibration/experiments/microphysics_equil/run_calibration.jl
+++ calibration/experiments/microphysics_equil/run_calibration.jl
@@ -12,21 +12,30 @@ using Logging
 import JLD2
 using LinearAlgebra
 
-include("model_interface.jl")
-include("observation_map.jl")
+@everywhere include("calibration/experiments/microphysics_equil/model_interface.jl")
+include("calibration/experiments/microphysics_equil/observation_map.jl")
+
+addprocs(
+    CAL.PBSManager(ensemble_size), # simulations in parallel. User may change this to use less GPUs.
+    q = "main",
+    A = "UCIT0011",
+    l_select = "1:ngpus=1:ncpus=4",
+    l_walltime = "11:30:00",
+    l_job_priority = "premium",
+)
 
 """
 FROM TUTORIAL
 """
 
-ensemble_size = 10
+ensemble_size = 10 #around 10x the amnt of params calibrating
 n_iterations = 3
 
 # change?
-noise = 0.1 * I
+noise = 0.001 * I
 
 # change this
-prior = constrained_gaussian("pow_ice", 5, 4, 0.01, 10)
+prior = constrained_gaussian("pow_icenuc", 5, 4, 0.01, 10)
 
 @info "Generating observations"
 parameter_file = CAL.parameter_path(output_dir, 0, 0)
