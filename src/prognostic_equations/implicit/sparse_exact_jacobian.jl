77b3801d20ebbb51c9643f25dd40bc9b3b280924
Dennis Yatunin <dyatun@gmail.com>
2025-05-16 13:22:59 -0700

    Fix GPU compatibility of SparseExactJacobian

--- src/prognostic_equations/implicit/sparse_exact_jacobian.jl
+++ src/prognostic_equations/implicit/sparse_exact_jacobian.jl
@@ -37,36 +37,51 @@ function dense_matrix_to_field_matrix!(field_matrix, dense_matrix, Y)
 
     for ((row_name, col_name), sparse_matrix_block) in sparse_matrix
         sparse_matrix_block isa LinearAlgebra.UniformScaling && continue
-        sparse_column_blocks = column_iterator(sparse_matrix_block)
 
         # TODO: This may not be the best way to represent tensors as scalars.
         is_child_name_of_row = Base.Fix2(MatrixFields.is_child_name, row_name)
         is_child_name_of_col = Base.Fix2(MatrixFields.is_child_name, col_name)
         row_indices = index_ranges[findfirst(is_child_name_of_row, field_names)]
         col_indices = index_ranges[findfirst(is_child_name_of_col, field_names)]
-        dense_column_blocks =
-            Iterators.map(axes(dense_matrix, 1)) do column_index
-                view(dense_matrix, column_index, row_indices, col_indices)
-            end
 
-        itr = zip(sparse_column_blocks, dense_column_blocks)
-        @threaded device for (sparse_column_block, dense_column_block) in itr
-            banded_matrix_transpose =
-                MatrixFields.column_field2array_view(sparse_column_block)'
-            band_indices =
-                (-banded_matrix_transpose.u):(banded_matrix_transpose.l)
-            @inbounds for band_index in band_indices
-                transposed_band = MatrixFields.band(-band_index)
-                sparse_column_block_view =
-                    dataview(view(banded_matrix_transpose, transposed_band))
-                dense_column_block_indices =
-                    diagind(dense_column_block, band_index)
-                dense_column_block_view =
-                    view(dense_column_block, dense_column_block_indices)
-                for level_index in axes(sparse_column_block_view, 1)
-                    sparse_column_block_view[level_index] =
-                        dense_column_block_view[level_index]
-                end
+        horz_space = Spaces.horizontal_space(axes(sparse_matrix_block))
+        Ni = Quadratures.degrees_of_freedom(Spaces.quadrature_style(horz_space))
+        sparse_matrix_block_size =
+            DataLayouts.farray_size(Fields.field_values(sparse_matrix_block))
+        cartesian_indices =
+            CartesianIndices(map(Base.OneTo, sparse_matrix_block_size))
+
+        @threaded device for cartesian_index in cartesian_indices
+            layout_type = Fields.field_values(sparse_matrix_block)
+            if layout_type isa DataLayouts.VIJFH
+                (v, i, j, f, h) = Tuple(cartesian_index)
+                sparse_column_index = (i, j, h)
+                dense_column_index = (h - 1) * Ni * Ni + (j - 1) * Ni + i
+            elseif layout_type isa DataLayouts.VIFH
+                (v, i, f, h) = Tuple(cartesian_index)
+                sparse_column_index = (i, h)
+                dense_column_index = (h - 1) * Ni + i
+            end
+            sparse_column_block =
+                Fields.column(sparse_matrix_block, sparse_column_index...)
+            dense_column_block =
+                view(dense_matrix, dense_column_index, row_indices, col_indices)
+
+            # Convert the sparse matrix diagonal index f to a dense matrix
+            # diagonal index, and determine whether the sparse matrix index
+            # (v, f) is a valid index into the dense matrix.
+            # Note: The dense matrix index (1, 1) corresponds to the diagonal
+            # index 0, and the dense matrix index (n_rows, n_cols) corresponds
+            # to the diagonal index n_cols - n_rows.
+            n_rows, n_cols, matrix_ld, _ =
+                MatrixFields.band_matrix_info(sparse_column_block)
+            matrix_d = matrix_ld + f - 1
+            first_row = matrix_d < 0 ? 1 - matrix_d : 1
+            last_row = matrix_d < n_cols - n_rows ? n_rows : n_cols - matrix_d
+
+            if first_row <= v <= last_row
+                parent(sparse_column_block)[v, f] =
+                    dense_column_block[v, v + matrix_d]
             end
         end
     end
