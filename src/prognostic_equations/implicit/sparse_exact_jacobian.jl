fc3ed9a80f8155532178c6f36ac9642334734db4
Dennis Yatunin <dyatun@gmail.com>
2025-05-17 01:17:55 -0700

    Improve numerical and type stability of SparseExactJacobian

--- src/prognostic_equations/implicit/sparse_exact_jacobian.jl
+++ src/prognostic_equations/implicit/sparse_exact_jacobian.jl
@@ -30,58 +30,68 @@ save_jacobian!(::SparseExactJacobian, cache, Y, dtγ, t) =
     save_jacobian!(alg.approx_jacobian_algorithm, cache, Y, dtγ, t)
 
 function dense_matrix_to_field_matrix!(field_matrix, dense_matrix, Y)
-    sparse_matrix = MatrixFields.scalar_fieldmatrix(field_matrix.matrix, Y)
+    device = ClimaComms.device(Y.c)
     field_names = scalar_field_names(Y)
     index_ranges = scalar_field_index_ranges(Y)
-    device = ClimaComms.device(Y.c)
+    sparse_matrix = MatrixFields.scalar_fieldmatrix(field_matrix.matrix, Y)
 
-    for ((row_name, col_name), sparse_matrix_block) in sparse_matrix
-        sparse_matrix_block isa LinearAlgebra.UniformScaling && continue
+    dense_row_ranges =
+        unrolled_map(keys(sparse_matrix).values) do (row_name, _)
+            is_child_name = Base.Fix2(MatrixFields.is_child_name, row_name)
+            index_ranges[unrolled_findfirst(is_child_name, field_names)]
+        end
+    dense_col_ranges =
+        unrolled_map(keys(sparse_matrix).values) do (_, col_name)
+            is_child_name = Base.Fix2(MatrixFields.is_child_name, col_name)
+            index_ranges[unrolled_findfirst(is_child_name, field_names)]
+        end
 
-        # TODO: This may not be the best way to represent tensors as scalars.
-        is_child_name_of_row = Base.Fix2(MatrixFields.is_child_name, row_name)
-        is_child_name_of_col = Base.Fix2(MatrixFields.is_child_name, col_name)
-        row_indices = index_ranges[findfirst(is_child_name_of_row, field_names)]
-        col_indices = index_ranges[findfirst(is_child_name_of_col, field_names)]
+    unrolled_foreach(
+        dense_row_ranges,
+        dense_col_ranges,
+        values(sparse_matrix),
+    ) do dense_rows, dense_cols, sparse_matrix_block
+        sparse_matrix_block isa Fields.Field || return
 
+        n_rows, n_cols, lower_band, _ =
+            MatrixFields.band_matrix_info(sparse_matrix_block)
         horz_space = Spaces.horizontal_space(axes(sparse_matrix_block))
-        Ni = Quadratures.degrees_of_freedom(Spaces.quadrature_style(horz_space))
+        Nq = Quadratures.degrees_of_freedom(Spaces.quadrature_style(horz_space))
         sparse_matrix_block_size =
             DataLayouts.farray_size(Fields.field_values(sparse_matrix_block))
         cartesian_indices =
             CartesianIndices(map(Base.OneTo, sparse_matrix_block_size))
 
         @threaded device for cartesian_index in cartesian_indices
-            layout_type = Fields.field_values(sparse_matrix_block)
-            if layout_type isa DataLayouts.VIJFH
-                (v, i, j, f, h) = Tuple(cartesian_index)
-                sparse_column_index = (i, j, h)
-                dense_column_index = (h - 1) * Ni * Ni + (j - 1) * Ni + i
-            elseif layout_type isa DataLayouts.VIFH
-                (v, i, f, h) = Tuple(cartesian_index)
-                sparse_column_index = (i, h)
-                dense_column_index = (h - 1) * Ni + i
-            end
-            sparse_column_block =
-                Fields.column(sparse_matrix_block, sparse_column_index...)
-            dense_column_block =
-                view(dense_matrix, dense_column_index, row_indices, col_indices)
+            layout_type = typeof(Fields.field_values(sparse_matrix_block))
+            v, f, sparse_horz_index, dense_horz_index =
+                if layout_type <: DataLayouts.VIJFH
+                    (v, i, j, f, h) = Tuple(cartesian_index)
+                    v, f, (i, j, h), (h - 1) * Nq * Nq + (j - 1) * Nq + i
+                elseif layout_type <: DataLayouts.VIFH
+                    (v, i, f, h) = Tuple(cartesian_index)
+                    v, f, (i, h), (h - 1) * Nq + i
+                elseif layout_type <: DataLayouts.VF
+                    (v, f) = Tuple(cartesian_index)
+                    v, f, (1, 1, 1), 1
+                else
+                    error("Unsupported DataLayout type: $layout_type")
+                end
+            sparse_column_array =
+                parent(Fields.column(sparse_matrix_block, sparse_horz_index...))
+            dense_column_array =
+                view(dense_matrix, dense_horz_index, dense_rows, dense_cols)
 
-            # Convert the sparse matrix diagonal index f to a dense matrix
-            # diagonal index, and determine whether the sparse matrix index
-            # (v, f) is a valid index into the dense matrix.
-            # Note: The dense matrix index (1, 1) corresponds to the diagonal
-            # index 0, and the dense matrix index (n_rows, n_cols) corresponds
-            # to the diagonal index n_cols - n_rows.
-            n_rows, n_cols, matrix_ld, _ =
-                MatrixFields.band_matrix_info(sparse_column_block)
-            matrix_d = matrix_ld + f - 1
-            first_row = matrix_d < 0 ? 1 - matrix_d : 1
-            last_row = matrix_d < n_cols - n_rows ? n_rows : n_cols - matrix_d
-
-            if first_row <= v <= last_row
-                parent(sparse_column_block)[v, f] =
-                    dense_column_block[v, v + matrix_d]
+            # Convert the field index f to a dense matrix band index. The dense
+            # array index (row, col) corresponds to the band index col - row.
+            band = lower_band + f - 1
+            
+            # If the sparse array index (v, f) corresponds to a valid index into
+            # the dense array, set the value at that index.
+            v_min = band < 0 ? 1 - band : 1
+            v_max = band < n_cols - n_rows ? n_rows : n_cols - band
+            if v_min <= v <= v_max
+                sparse_column_array[v, f] = dense_column_array[v, v + band]
             end
         end
     end
