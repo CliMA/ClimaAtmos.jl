11dc635bf01f37cc7deeab6daf3198e30090873d
Sajjad Azimi <azimi@caltech.edu>
2025-06-10 09:54:20 -0700

    fix failing 2-moment reproducibility test

--- reproducibility_tests/reproducibility_test_job_ids.jl
+++ reproducibility_tests/reproducibility_test_job_ids.jl
@@ -2,6 +2,7 @@ reproducibility_test_job_ids() = [
     "baroclinic_wave_equil",
     "held_suarez_equil",
     "aquaplanet_nonequil_allsky_gw_res",
+    "aquaplanet_nonequil_allsky_gw_res_2M",
     "aquaplanet_equil_allsky_gw_raw_zonalasym",
     "diagnostic_edmfx_aquaplanet",
     "single_column_hydrostatic_balance_ft64",
