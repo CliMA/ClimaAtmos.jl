b4d159629644081a7b41acc20e15787f9dddbf53
Dennis Yatunin <dyatun@gmail.com>
2025-06-13 18:03:16 -0700

    Add AutoSparseJacobian algorithm for implicit solver

--- src/prognostic_equations/implicit/jacobian.jl
+++ src/prognostic_equations/implicit/jacobian.jl
@@ -7,10 +7,14 @@ this abstract type should define 3 methods:
  - `jacobian_cache(alg::JacobianAlgorithm, Y, atmos)`
  - `update_jacobian!(alg::JacobianAlgorithm, cache, Y, p, dtγ, t)`
  - `invert_jacobian!(alg::JacobianAlgorithm, cache, ΔY, R)`
+To facilitate debugging, concrete implementations should also define
+ - `first_column_block_arrays(alg::JacobianAlgorithm, Y, p, dtγ, t)`
 
 See [Implicit Solver](@ref) for additional background information.
 """
 abstract type JacobianAlgorithm end
+
+abstract type SparseJacobian <: JacobianAlgorithm end
 
 """
     Jacobian(alg, Y, atmos)
@@ -58,4 +62,63 @@ function LinearAlgebra.ldiv!(
     R_krylov .= R
     LinearAlgebra.ldiv!(ΔY_krylov, jacobian, R_krylov)
     ΔY .= ΔY_krylov
+end
+
+# This generates a standardized output for comparing different sparse Jacobians.
+function first_column_block_arrays(alg::SparseJacobian, Y, p, dtγ, t)
+    scalar_names = scalar_field_names(Y)
+    column_Y = first_column_view(Y)
+    column_p = first_column_view(p)
+    column_cache = jacobian_cache(alg, column_Y, p.atmos)
+
+    update_jacobian!(alg, column_cache, column_Y, column_p, dtγ, t)
+    column_∂R_∂Y = column_cache.matrix
+
+    block_arrays = Dict()
+    for block_key in Iterators.product(scalar_names, scalar_names)
+        block_key in keys(column_∂R_∂Y) || continue
+        block_value = Base.materialize(column_∂R_∂Y[block_key])
+        block_arrays[block_key] =
+            block_value isa Fields.Field ?
+            MatrixFields.column_field2array(block_value) : block_value
+    end
+    return block_arrays
+end
+
+first_column_view(x::Union{Fields.Field, Fields.FieldVector}) =
+    Fields.column(x, 1, 1, 1)
+first_column_view(x::Union{Tuple, NamedTuple}) =
+    unrolled_map(first_column_view, x)
+function first_column_view(x)
+    contains_any_fields(x) =
+        x isa Fields.Field ||
+        unrolled_any(StaticOneTo(fieldcount(typeof(x)))) do i
+            contains_any_fields(getfield(x, i))
+        end
+    contains_any_fields(x) || return x
+    constructor = typeof(x).name.wrapper # Assume x has a default constructor.
+    constructor_args = ntuple(i -> getfield(x, i), Val(fieldcount(typeof(x))))
+    return constructor(unrolled_map(first_column_view, constructor_args)...)
+end
+
+function first_column_rescaling_arrays(Y, p, t)
+    scalar_names = scalar_field_names(Y)
+    column_Y = first_column_view(Y)
+    column_p = first_column_view(p)
+    column_Yₜ = similar(column_Y)
+
+    implicit_tendency!(column_Yₜ, column_Y, column_p, t)
+
+    rescaling_arrays = Dict()
+    for block_key in Iterators.product(scalar_names, scalar_names)
+        (block_row_name, block_column_name) = block_key
+        block_row_Yₜ_values =
+            parent(MatrixFields.get_field(column_Yₜ, block_row_name))
+        block_column_Yₜ_values =
+            parent(MatrixFields.get_field(column_Yₜ, block_column_name))
+        safe_inverse = x -> iszero(x) || issubnormal(x) ? zero(x) : inv(x)
+        rescaling_arrays[block_key] =
+            Array(safe_inverse.(block_row_Yₜ_values) .* block_column_Yₜ_values')
+    end
+    return rescaling_arrays
 end
