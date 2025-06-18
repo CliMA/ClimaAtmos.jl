db37300e4c5eafd68322baebc5542a189b3772f5
Olivia Alcabes <olivia.alcabes@gmail.com>
2025-06-18 10:46:06 -0700

    Remove ts from nonequilibrium cloud sources

--- src/parameterized_tendencies/microphysics/cloud_condensate.jl
+++ src/parameterized_tendencies/microphysics/cloud_condensate.jl
@@ -34,8 +34,34 @@ function cloud_condensate_tendency!(
     thp = CAP.thermodynamics_params(params)
     cmc = CAP.microphysics_cloud_params(params)
 
-    @. Yₜ.c.ρq_liq += Y.c.ρ * cloud_sources(cmc.liquid, thp, ᶜts, q_rai, dt)
-    @. Yₜ.c.ρq_ice += Y.c.ρ * cloud_sources(cmc.ice, thp, ᶜts, q_sno, dt)
+    Tₐ = @. lazy(TD.air_temperature(thp, ᶜts))
+
+    @. Yₜ.c.ρq_liq +=
+        Y.c.ρ * cloud_sources(
+            cmc.liquid,
+            thp,
+            specific(Y.c.ρq_tot, Y.c.ρ),
+            specific(Y.c.ρq_liq, Y.c.ρ),
+            specific(Y.c.ρq_ice, Y.c.ρ),
+            specific(Y.c.ρq_rai, Y.c.ρ),
+            specific(Y.c.ρq_sno, Y.c.ρ),
+            Y.c.ρ,
+            Tₐ,
+            dt,
+        )
+    @. Yₜ.c.ρq_ice +=
+        Y.c.ρ * cloud_sources(
+            cmc.ice,
+            thp,
+            specific(Y.c.ρq_tot, Y.c.ρ),
+            specific(Y.c.ρq_liq, Y.c.ρ),
+            specific(Y.c.ρq_ice, Y.c.ρ),
+            specific(Y.c.ρq_rai, Y.c.ρ),
+            specific(Y.c.ρq_sno, Y.c.ρ),
+            Y.c.ρ,
+            Tₐ,
+            dt,
+        )
 end
 
 function cloud_condensate_tendency!(
@@ -52,16 +78,47 @@ function cloud_condensate_tendency!(
     thp = CAP.thermodynamics_params(params)
     cmc = CAP.microphysics_cloud_params(params)
 
-    @. Yₜ.c.ρq_liq += Y.c.ρ * cloud_sources(cmc.liquid, thp, ᶜts, q_rai, dt)
-    @. Yₜ.c.ρq_ice += Y.c.ρ * cloud_sources(cmc.ice, thp, ᶜts, q_sno, dt)
+    Tₐ = @. lazy(TD.air_temperature(thp, ᶜts))
+
+    @. Yₜ.c.ρq_liq +=
+        Y.c.ρ * cloud_sources(
+            cmc.liquid,
+            thp,
+            specific(Y.c.ρq_tot, Y.c.ρ),
+            specific(Y.c.ρq_liq, Y.c.ρ),
+            specific(Y.c.ρq_ice, Y.c.ρ),
+            specific(Y.c.ρq_rai, Y.c.ρ),
+            specific(Y.c.ρq_sno, Y.c.ρ),
+            Y.c.ρ,
+            Tₐ,
+            dt,
+        )
+    @. Yₜ.c.ρq_ice +=
+        Y.c.ρ * cloud_sources(
+            cmc.ice,
+            thp,
+            specific(Y.c.ρq_tot, Y.c.ρ),
+            specific(Y.c.ρq_liq, Y.c.ρ),
+            specific(Y.c.ρq_ice, Y.c.ρ),
+            specific(Y.c.ρq_rai, Y.c.ρ),
+            specific(Y.c.ρq_sno, Y.c.ρ),
+            Y.c.ρ,
+            Tₐ,
+            dt,
+        )
 
     @. Yₜ.c.ρn_liq +=
         Y.c.ρ * aerosol_activation_sources(
             cmc.liquid,
             thp,
-            ᶜts,
-            q_rai,
-            n_liq,
+            Y.c.ρ,
+            Tₐ,
+            specific(Y.c.ρq_tot, Y.c.ρ),
+            specific(Y.c.ρq_liq, Y.c.ρ),
+            specific(Y.c.ρq_ice, Y.c.ρ),
+            specific(Y.c.ρq_rai, Y.c.ρ),
+            specific(Y.c.ρq_sno, Y.c.ρ),
+            specific(Y.c.ρn_liq, Y.c.ρ),
             cmc.N_cloud_liquid_droplets,
             dt,
         )
