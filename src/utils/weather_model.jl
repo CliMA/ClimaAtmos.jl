886a1f4dff17e16ff7239d98d3d132baf5214c34
Julians42 <jschmitt@caltech.edu>
2025-07-02 18:20:02 -0700

    Increase dt by zeroing vertical velocity IC, automates data processing via artifacts, updates config

--- src/utils/weather_model.jl
+++ src/utils/weather_model.jl
@@ -1,23 +1,36 @@
 using NCDatasets
+using Dates
+import ClimaInterpolations.Interpolation1D: interpolate1d!, Linear, Flat
 
+"""
+    weather_model_data_path(start_date, start_time)
 
+Get the path to the weather model data for a given start date and time.
+If the data is not found, will attempt to generate it from raw data. If 
+the raw data is not found, throw an error
+
+# Arguments
+- `start_date`: Start date as string YYYYMMDD
+- `start_time`: Start time as string HHMM
+"""
 function weather_model_data_path(start_date, start_time)
     # get data path
-    rootdir = "/home/jschmitt/experiments/ecmwf/initial_conditions"
-    ic_data_path = joinpath(rootdir, "era5_init_$(start_date)_$(start_time).nc")
-    source_path = joinpath(rootdir, "era5_raw_$(start_date)_$(start_time).nc")
+    ic_data_path = joinpath(@clima_artifact("weather_model_ic"), "init", "era5_init_$(start_date)_$(start_time).nc")
+    raw_data_path = joinpath(@clima_artifact("weather_model_ic"), "raw", "era5_raw_$(start_date)_$(start_time).nc")
+
     if !isfile(ic_data_path)
-        # process using utilities
-        # TODO: implement
-        if !isfile(source_path)
-            error("Source file $(source_path) does not exist")
+        @info "Initial condition file $ic_data_path does not exist. Attempting to generate it now..."
+        if !isfile(raw_data_path)
+            day = Dates.format(Date(start_date, "yyyymmdd"), "yyyy-mm-dd")
+            time = Dates.format(Time(start_time, "HHMM"), "HH:MM")
+            error("Source file $(raw_data_path) does not exist. Please run `python get_initial_conditions.py --output-dir $(@clima_artifact("weather_model_ic"))/raw --date $(day) --time $(time)` to download the data.")
         end
+        # get target levels - TODO: make this more flexible
+        target_level_path = joinpath(@clima_artifact("weather_model_ic"), "target_levels.txt")
+        target_levels = parse.(Float32, readlines(target_level_path))
+
         @info "Interpolating raw weather model data onto z-levels"
-        # interpolate raw data onto z-levels
-        # save to target path
-        # return target path
-        error("Interpolation from raw not automated yet - see to_z_levels")
-        return target_path
+        to_z_levels(raw_data_path, ic_data_path, target_levels, Float32)
     end
     return ic_data_path
 end
@@ -36,6 +49,9 @@ for consistency if we write a new initial condition dispatch.
 
 """
 function to_z_levels(source_file, target_file, target_levels, FT)
+
+    param_set = TD.Parameters.ThermodynamicsParameters(FT)
+    grav = TD.Parameters.grav(param_set)
 
     ncin = Dataset(source_file)
     
@@ -56,7 +72,7 @@ function to_z_levels(source_file, target_file, target_levels, FT)
     defDim(ncout, "z", length(target_levels))
     
     # Define coordinate variables with clean attributes
-    lon_attrib = OrderedDict(
+    lon_attrib = Dict(
         "Datatype" => string(FT),
         "standard_name" => "longitude",
         "long_name" => "longitude",
@@ -65,7 +81,7 @@ function to_z_levels(source_file, target_file, target_levels, FT)
     lon_var = defVar(ncout, "lon", FT, ("lon",), attrib = lon_attrib)
     lon_var[:] = lon
     
-    lat_attrib = OrderedDict(
+    lat_attrib = Dict(
         "Datatype" => string(FT),
         "standard_name" => "latitude",
         "long_name" => "latitude",
@@ -75,7 +91,7 @@ function to_z_levels(source_file, target_file, target_levels, FT)
     lat_var = defVar(ncout, "lat", FT, ("lat",), attrib = lat_attrib)
     lat_var[:] = lat
     
-    z_attrib = OrderedDict(
+    z_attrib = Dict(
         "Datatype" => string(FT),
         "standard_name" => "altitude",
         "long_name" => "altitude",
@@ -91,8 +107,10 @@ function to_z_levels(source_file, target_file, target_levels, FT)
     v_var = defVar(ncout, "v", FT, ("lon", "lat", "z",), attrib = ncin["v"].attrib)
     v_var[:, :, :] = interpz_3d(target_levels, source_z, FT.(ncin["v"][:, :, :, 1]))
     
+    # ERA5 w is from a hydrostatic model and so isn't meaningful for ClimaAtmos
+    # See https://agupubs.onlinelibrary.wiley.com/doi/full/10.1002/2017MS001059
     w_var = defVar(ncout, "w", FT, ("lon", "lat", "z",), attrib = ncin["w"].attrib)
-    w_var[:, :, :] = interpz_3d(target_levels, source_z, FT.(ncin["w"][:, :, :, 1]))
+    w_var[:, :, :] .= FT(0)
     
     t_var = defVar(ncout, "t", FT, ("lon", "lat", "z",), attrib = ncin["t"].attrib)
     t_var[:, :, :] = interpz_3d(target_levels, source_z, FT.(ncin["t"][:, :, :, 1]))
@@ -107,7 +125,7 @@ function to_z_levels(source_file, target_file, target_levels, FT)
         skt_var[:, :, k] = FT.(ncin["skt"][:, :, 1])
     end
     
-    # TODO: rename p to sp in MoistFromFile dispatch
+    # TODO: rename p to sp in MoistFromFile for consistency
     sp_var = defVar(ncout, "p", FT, ("lon", "lat", "z",), attrib = ncin["sp"].attrib)
     for k in 1:length(target_levels)
         sp_var[:, :, k] = FT.(ncin["sp"][:, :, 1])
@@ -118,7 +136,11 @@ function to_z_levels(source_file, target_file, target_levels, FT)
     close(ncout)
 end
 
+"""
+    interpz_3d(ztarget, zsource, fsource)
 
+Interpolate 3D field `fsource` from 3D source levels `zsource` to 1D target levels `ztarget`.
+"""
 function interpz_3d(ztarget, zsource, fsource)
     nx, ny, nz = size(zsource)
     # permute dimensions from (nx, ny, nz) to (nz, nx, ny) if needed
