01ca0f837a235bfc3fd9d3ed2503b2407cec9cd3
jefftyoder <jeffreyyoder@unr.edu>
2025-07-14 11:16:46 -0700

    WIP larcform1 ci plots

--- post_processing/ci_plots.jl
+++ post_processing/ci_plots.jl
@@ -587,6 +587,7 @@ ColumnPlots = Union{
     Val{:single_column_radiative_equilibrium_clearsky},
     Val{:single_column_radiative_equilibrium_clearsky_prognostic_surface_temp},
     Val{:single_column_radiative_equilibrium_allsky_idealized_clouds},
+    Val{:larcform1}
 }
 
 function make_plots(::ColumnPlots, output_paths::Vector{<:AbstractString})
@@ -1558,6 +1559,7 @@ function make_plots(
     )
 end
 
+<<<<<<< HEAD
 function make_plots(::Val{:kinematic_driver}, output_paths::Vector{<:AbstractString})
     function rescale_time_to_min(var)
         if haskey(var.dims, "time")
@@ -1594,3 +1596,43 @@ function make_plots(::Val{:kinematic_driver}, output_paths::Vector{<:AbstractStr
     end
     make_plots_generic(output_paths, vars_lines; summary_files = [file_contour])
 end
+=======
+Larcform1Plots = Val{:larcform1}
+
+function make_plots(::Larcform1Plots, output_paths::Vector{<:AbstractString})
+    simdirs = SimDir.(output_paths)
+
+    reduction_avg = "average"
+    reduction_inst = "inst"
+
+    short_names_0D = [
+        "rsut",
+    ]
+    short_names_1D = ["ta", "wa"]
+    #vars_0D = map_comparison(get, simdirs, short_names_0D)
+    #vars_1D = map_comparison(get, simdirs, short_names_1D)
+    vars_0D = map_comparison(simdirs, short_names_0D) do simdir, short_name
+        get(simdir; short_name, reduction)
+    end
+
+    #=
+    # Vertical profiles at time = LAST_SNAP
+    make_plots_generic(
+        output_paths,
+        vars_1D,
+        time = LAST_SNAP,
+        x = 0.0,
+        y = 0.0,
+        more_kwargs = Dict(:axis => [:dim_on_y => true]),
+    )
+    =#
+    # Time series
+    make_plots_generic(
+        output_paths,
+        vars_0D,
+        x = 0.0,
+        y = 0.0,
+        more_kwargs = Dict(:axis => [:dim_on_y => true]),
+    )
+end
+>>>>>>> b3c22d658 (WIP larcform1 ci plots)
