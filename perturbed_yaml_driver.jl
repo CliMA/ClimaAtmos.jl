7421ad941bb2e36524c3a5257cb7ac1994944c63
Eric Pham <erpham@ucsd.edu>
2025-07-30 11:09:39 -0700

    change default, add driver

--- perturbed_yaml_driver.jl
+++ perturbed_yaml_driver.jl
@@ -4,10 +4,15 @@
 
 import ClimaAtmos as CA
 
+# Compile once, so that next runs will be faster.
+config = CA.AtmosConfig("prognostic_edmfx_dycoms_rf02_column.yml")
+include("./examples/hybrid/driver.jl")
+
+# Directory with yaml files.
 LWP_N_config_dir = "LWP_N_config"
 LWP_N_config_files = readdir(LWP_N_config_dir)
 
-
+# Run simulation for each file in the directory.
 for config_yaml in LWP_N_config_files
     config = CA.AtmosConfig(joinpath(LWP_N_config_dir, config_yaml))
     include("./examples/hybrid/driver.jl")
