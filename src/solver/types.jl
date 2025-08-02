452fb2e06e1fc9637fee2c39107a9635a039e8e9
ray-chew <chew@caltech.edu>
2025-08-01 17:50:42 -0700

    hughes2023 test runs on the GPU with the Clima pipeline

--- src/solver/types.jl
+++ src/solver/types.jl
@@ -277,7 +277,7 @@ Base.@kwdef struct LinearOrographicGravityWave{FT, S} <: OrographicGravityWave
     # topo_info::S = "linear"
 end
 
-Base.@kwdef struct FullOrographicGravityWave{FT, S} <: OrographicGravityWave
+Base.@kwdef struct FullOrographicGravityWave{FT, S, T} <: OrographicGravityWave
     γ::FT
     ϵ::FT
     β::FT
@@ -288,7 +288,7 @@ Base.@kwdef struct FullOrographicGravityWave{FT, S} <: OrographicGravityWave
     a1::FT
     Fr_crit::FT
     topo_info::S
-    topography::S
+    topography::T
 end
 
 abstract type AbstractForcing end
