df487c4b7d7373f9a9d537bfc2a86ea91358bc52
ray-chew <chew@caltech.edu>
2025-09-09 02:51:30 -0700

    Use wcurl in exp. tendency reconstruction

--- src/prognostic_equations/advection.jl
+++ src/prognostic_equations/advection.jl
@@ -241,7 +241,7 @@ NVTX.@annotate function explicit_vertical_advection_tendency!(Yₜ, Y, p, t)
     for j in 1:n
         @. ᶠω¹²ʲs.:($$j) = ᶠω¹²
     end
-    @. ᶠω¹² += CT12(curlₕ(Y.f.u₃))
+    @. ᶠω¹² += CT12(wcurlₕ(Y.f.u₃))
     for j in 1:n
         @. ᶠω¹²ʲs.:($$j) += CT12(curlₕ(Y.f.sgsʲs.:($$j).u₃))
     end
