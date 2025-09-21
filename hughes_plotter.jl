bde85614d1027460bbae8440c0f93e5837c80f87
raychew <chew@iau.uni-frankfurt.de>
2025-09-21 14:00:09 -0700

    Updated plotter
    Remove topography function
    
    s

--- hughes_plotter.jl
+++ hughes_plotter.jl
@@ -8,45 +8,140 @@ using Colors, ColorSchemes
 import ClimaInterpolations.Interpolation1D: Linear, interpolate1d!, Flat
 
 ### USER ENTERS DIRECTORY
-#dir = "/Users/akshaysridhar/Research/Data/paper_data/test_hughes/moist/output_0003"
-dir_old_pgf = "./output/baroclinic_wave_hughes2023/output_00xx"
-dir_exner_pgf = "./output/baroclinic_wave_hughes2023/output_0089"
-dir_exner_ref_pgf = "./output/baroclinic_wave_hughes2023/output_0088"
+# Note: I switched the namings here, exner is the ref, and ref is the exner!
+dir_exner_pgf = "/home/ray/Documents/2025/dycore_paper_review/hughes_jablonowski/exner_pgf/nc_files-exner"
+dir_old_pgf = "/home/ray/Documents/2025/dycore_paper_review/hughes_jablonowski/ref_pgf/nc_files-ref"
 
-function topography_hughes2023(lat, long)
-    λ, ϕ = long, lat
-    FT = eltype(λ)
-    h₀ = FT(2e3)
-    # Angles in degrees
-    ϕ₁ = FT(45)
-    ϕ₂ = FT(45)
-    λ_min = minimum(λ)
-    λ₁ = FT(72)
-    λ₂ = FT(140)
-    λₘ = FT(7)
-    ϕₘ = FT(40)
-    d = ϕₘ / 2 * (-log(0.1))^(-1 / 6)
-    c = λₘ / 2 * (-log(0.1))^(-1 / 2)
-    d₁ = (λ - λ_min) - λ₁
-    d₂ = (λ - λ_min) - λ₂
-    l₁ = λ - λ₁
-    l₂ = λ - λ₂
-    zₛ = FT(
-        h₀ * (
-            exp(-(((ϕ - ϕ₁) / d)^6 + (l₁ / c)^2)) +
-            exp(-(((ϕ - ϕ₂) / d)^6 + (l₂ / c)^2))
-        ),
+function get_dat(dir_labels, dir_paths, time_slcs)
+    figsize = (1500, 1200);
+    fig = Figure(; size = figsize)
+    fontsize = 20
+    
+    # Store data for difference calculation
+    all_data = Dict()
+    
+    # First, collect all the data
+    for (dir_idx, dir_name) in enumerate(dir_names)
+        all_data[dir_idx] = Dict()
+        for (time_idx, time_slc) in enumerate(time_slcs)
+            simdir = ClimaAnalysis.SimDir(dir_name)
+            dat = get(simdir; short_name = "pfull", reduction = "inst");
+            zg = get(simdir; short_name = "zg", reduction = "inst");
+            arr = dat.data
+            arr = reverse(arr, dims=4)
+            arr = arr[time_slc, :, :, :] - arr[1, :, : ,:]
+            arr = permutedims(arr, (3, 1, 2))
+            arr = arr[43, : , :]
+            arr = arr ./ 100.0 # convert to hectopascal
+            
+            all_data[dir_idx][time_idx] = (; dat, arr, zg)
+        end
+    end
+    
+    # Plot original data (first two rows)
+    for (fig_iy, dir_name) in enumerate(dir_names)
+        levels = nothing
+        ticks = nothing
+        plt = nothing
+		subtitle = dir_labels[fig_iy]
+        for (fig_ix, time_slc) in enumerate(time_slcs)
+            dat_items = all_data[fig_iy][fig_ix]
+            arr = dat_items.arr
+            
+            if isnothing(levels)
+                levels = range(minimum(arr)-1, maximum(arr), 8)
+                ticks  = range(minimum(arr)-1, maximum(arr), 8)
+            end
+            println("Row $(fig_iy), Col $(fig_ix): ", levels)
+
+            fig_items = (; fig, fig_ix, fig_iy, fontsize)
+            plt = plot_panel(fig_items, dat_items, time_slc, subtitle, levels)
+        end
+        Colorbar(
+            fig[fig_iy, 3],
+            plt,
+            label = "Sfc Pressure Perturbation (hPa)",
+            labelsize = fontsize,
+            ticklabelsize = fontsize,
+        )
+    end
+    
+    # Plot differences (third row)
+    @show "Plotting Pressure Differences between rows"
+    diff_levels = nothing
+    diff_plt = nothing
+    
+    for (fig_ix, time_slc) in enumerate(time_slcs)
+        # Calculate difference: row 2 - row 1
+        dat_items_1 = all_data[1][fig_ix]  # First directory
+        dat_items_2 = all_data[2][fig_ix]  # Second directory
+        
+        arr_diff = dat_items_2.arr - dat_items_1.arr
+        
+        if isnothing(diff_levels)
+            diff_levels = range(minimum(arr_diff), maximum(arr_diff), 8)
+        end
+        println("Difference Row, Col $(fig_ix): ", diff_levels)
+        
+        # Create modified dat_items for difference plot
+        diff_dat_items = (; dat = dat_items_1.dat, arr = arr_diff, zg = dat_items_1.zg)
+        fig_items = (; fig, fig_ix, fig_iy = 3, fontsize)  # Third row
+        diff_plt = plot_panel(fig_items, diff_dat_items, time_slc, diff_levels, is_difference=true)
+    end
+    
+    # Add colorbar for differences
+    Colorbar(
+        fig[3, 3],
+        diff_plt,
+        label = "Pressure Difference (old - Exner) (hPa)",
+        labelsize = fontsize,
+        ticklabelsize = fontsize,
+    )
+    
+    # for (fig_iy, dir_name) in enumerate(dir_names)
+    #     levels = nothing
+    #     ticks = nothing
+    #     plt = nothing
+    #     for (fig_ix, time_slc) in enumerate(time_slcs)
+    #         simdir = ClimaAnalysis.SimDir(dir_name)
+    #         dat = get(simdir; short_name = "pfull", reduction = "inst");
+    #         zg = get(simdir; short_name = "zg", reduction = "inst");
+    #         arr = dat.data
+    #         arr = reverse(arr, dims=4)
+    #         arr = arr[time_slc, :, :, :] - arr[1, :, : ,:]
+    #         arr = permutedims(arr, (3, 1, 2))
+    #         arr = arr[43, : , :]
+    #         arr = arr ./ 100.0 # convert to hectopascal
+
+    #         if isnothing(levels)
+    #             levels = range(minimum(arr)-1, maximum(arr), 8)
+#   #              levels = filter(!=(0), levels)
+    #             ticks  = range(minimum(arr)-1, maximum(arr), 8)
+    #         end
+    #         println(fig_ix, levels)
+
+    #         fig_items = (; fig, fig_ix, fig_iy, fontsize)
+    #         dat_items = (; dat, arr, zg)
+    #         plt = plot_panel(fig_items, dat_items, time_slc, levels)
+    #     end
+    #     Colorbar(
+    #         fig[fig_iy, 3],
+    #         plt,
+    #         label = "Sfc Pressure Perturbation (hPa)",
+    #         labelsize = fontsize,
+    #         ticklabelsize = fontsize,
+    #         # ticks = ticks,
+    #     )
+    # end
+    ###### PRESSURE DIFFERENCE (SURFACE)
+    @show "Plotting Pressure Difference"
+    fig
+    CairoMakie.save(
+        "paperrevision_mbw_hughes_air_pressure.pdf",
+        fig,
     )
 end
 
-simdir = ClimaAnalysis.SimDir(dir)
-pfull = get(simdir; short_name = "pfull", reduction = "inst");
-# @Main.infiltrate
-pfull = window(pfull, "time", by = Index(), left = 8, right = 21);
-pfull0 = average_time(window(pfull, "time", by = Index(), right = 1));
-pfull = average_time(pfull);
-zg = get(simdir; short_name = "zg", reduction = "average");
-pressure = pfull.data - pfull0.data
 
 function offset_longitudes(longitude)
     new_longitude = mod.(longitude .+ 360, 360)
@@ -58,69 +153,118 @@ function offset_data(data)
     return new_data
 end
 
-# @Main.infiltrate
+function plot_panel(fig_items, dat_items, time_slc, subtitle, levels; is_difference=false)
+    (;fig , fig_ix, fig_iy, fontsize) = fig_items
+    (;dat, arr, zg) = dat_items
+    
+    # Spatial Data
+    lat = dat.dims["lat"];
+    lon = dat.dims["lon"];
+    xvariable = lon
+    yvariable = lat
 
-#Data
+    # Create different titles based on whether it's a difference plot
+    if is_difference
+        title_text = "Difference - Day $(time_slc-1)"
+    else
+        if fig_iy == 1
+            title_text = "$(subtitle) - Day $(time_slc-1)"
+        else
+            title_text = "$(subtitle) - Day $(time_slc-1)"
+        end
+    end
+    
+    ax = Axis(
+        fig[fig_iy, fig_ix],
+        limits = (0, 245, 0, 90),
+        title = title_text,
+        xlabel = "Longitude (°E)",
+        ylabel = "Latitude (°N)",
+        titlesize = fontsize,
+        xlabelsize = 20,
+        ylabelsize = 20,
+        xticklabelsize = 20,
+        yticklabelsize = 20,
+        xgridvisible = false,
+        ygridvisible = false,
+    )
+    
+    # Use different colorscheme for difference plots
+    if is_difference
+        plt = CairoMakie.contourf!(
+            offset_longitudes(xvariable),
+            yvariable,
+            offset_data(arr),
+            levels = 10,
+            colormap = Reverse(:RdBu),  # Red-Blue colormap for differences
+        )
+    else
+        plt = CairoMakie.contourf!(
+            offset_longitudes(xvariable),
+            yvariable,
+            offset_data(arr),
+            levels = 10,
+        )
+    end
+    
+    # Add topography contours
+    CairoMakie.contour!(
+        xvariable,
+        yvariable,
+        zg.data[1, :, :, 1] .- 15.0,
+        alpha = 1,
+        levels = 2,
+        color = :gray,
+    )
+    return plt
+end
 
-pressure = permutedims(pressure, (3, 1, 2))
+# function plot_panel(fig_items, dat_items, time_slc, levels)
+#     (;fig , fig_ix, fig_iy, fontsize) = fig_items
+#     (;dat, arr, zg) = dat_items
+#     #SpatialData
+#     lat = dat.dims["lat"]; # Index 540 for 45 degree north
+#     lon = dat.dims["lon"];
+#     xvariable = lon
+#     yvariable = lat
+# 
+#     # mycolors = reverse(colormap("RdBu"; logscale = false, mid = 0.5))
+#     ax = Axis(
+#         fig[fig_ix, fig_iy],
+#         limits = (0, 245, 0, 90),
+#         title = "Day $(time_slc-1)",
+#         xlabel = "Longitude (°E)",
+#         ylabel = "Latitude (°N)",
+#         titlesize = fontsize,
+#         xlabelsize = 20,
+#         ylabelsize = 20,
+#         xticklabelsize = 20,
+#         yticklabelsize = 20,
+#         xgridvisible = false,
+#         ygridvisible = false,
+#     )
+#     plt = CairoMakie.contourf!(
+#         offset_longitudes(xvariable),
+#         yvariable,
+#         offset_data(arr),
+#         levels = 10,
+#         # extendhigh = :auto,
+#         # extendlow = :auto
+#     )
+#     # @Main.infiltrate
+#     CairoMakie.contour!(
+#         xvariable,
+#         yvariable,
+#         zg.data[1, :, :, 1] .- 15.0,
+#         alpha = 1,
+#         levels = 2,
+#         color = :gray,
+#     )
+#     return plt
+# end
 
-#SpatialData
-lat = pfull.dims["lat"]; # Index 540 for 45 degree north
-lon = pfull.dims["lon"];
-xvariable = lon
-yvariable = lat
+dir_labels = ("old PGF", "Exner PGF + ref. prof.")
+dir_paths = (dir_old_pgf, dir_exner_pgf)
+time_slcs  = (5, 7)
 
-fontsize = 20
-
-###### PRESSURE DIFFERENCE (SURFACE)
-@show "Plotting Pressure Difference"
-figsize = (1500, 400);
-vertind = 49;
-mycolors = reverse(colormap("RdBu"; logscale = false, mid = 0.5))
-pressurelevels = -20:1:20
-pressurelevels = filter(!=(0), pressurelevels)
-pressureticks = -20:1:20
-fig = Figure(; size = figsize)
-ax = Axis(
-    fig[1, 1],
-    limits = (0, 245, 0, 90),
-    title = "Day 4",
-    xlabel = "Longitude (°E)",
-    ylabel = "Latitude (°N)",
-    titlesize = fontsize,
-    xlabelsize = 20,
-    ylabelsize = 20,
-    xticklabelsize = 20,
-    yticklabelsize = 20,
-    xgridvisible = false,
-    ygridvisible = false,
-)
-# @Main.infiltrate
-hm1 = CairoMakie.contourf!(
-    offset_longitudes(xvariable),
-    yvariable,
-    offset_data(pressure[1, :, :] ./ 100),
-    # colormap = mycolors,
-    # levels = pressurelevels,
-)
-CairoMakie.contour!(
-    xvariable,
-    yvariable,
-    zg.data[1, :, :, 1] .- 15.0,
-    alpha = 1,
-    levels = 2,
-    color = :gray,
-)
-Colorbar(
-    fig[1, 3],
-    hm1,
-    label = "Sfc Pressure Perturbation (hPa)",
-    labelsize = fontsize,
-    ticklabelsize = fontsize,
-    # ticks = pressureticks,
-)
-fig
-CairoMakie.save(
-    "paperrevision_mbw_hughes_air_pressure.pdf",
-    fig,
-)
+get_dat(dir_labels, dir_paths, time_slcs)
--- plane_plotter.jl
+++ plane_plotter.jl
@@ -8,57 +8,123 @@ using Colors, ColorSchemes
 import ClimaInterpolations.Interpolation1D: Linear, interpolate1d!, Flat
 
 ### USER ENTERS DIRECTORY
-#dir = "/Users/akshaysridhar/Research/Data/paper_data/test_hughes/moist/output_0003"
 dir_old_pgf = "./output/plane_schar_mountain_float64_test/output_ref"
-dir_exner_ref_pgf = "./output/plane_schar_mountain_float64_test/output_0072"
+dir_exner_ref_pgf = "./output/plane_schar_mountain_float64_test/output_0104"
 
-vardir = ClimaAnalysis.SimDir(dir_exner_ref_pgf)
-refdir = ClimaAnalysis.SimDir(dir_old_pgf)
-
-fontsize = 20
-
-dirs = (refdir, vardir)
-dir_names = ("Old PGF", "Exner+Ref. Prof.")
-
-function gen_panel(fig_ix, dirs, dir_names, short_name, reduction, time_idx=nothing; cmap=nothing)
-
-    plt = nothing
-    levels = nothing
-    for (fig_iy, dir) in enumerate(dirs)
-        var = get(dir; short_name = short_name, reduction = reduction);
+function get_dat_2d(dir_labels, dir_paths, short_name, reduction; time_idx=nothing)
+    figsize = (1500, 1200);
+    fig = Figure(; size = figsize)
+    fontsize = 20
+    
+    # Store data for difference calculation
+    all_data = Dict()
+    
+    # First, collect all the data
+    for (dir_idx, dir_path) in enumerate(dir_paths)
+        simdir = ClimaAnalysis.SimDir(dir_path)
+        var = get(simdir; short_name = short_name, reduction = reduction);
+        
         if isnothing(time_idx)
             vardat = var.data[end, :, :]
         else
             vardat = var.data[time_idx, :, :]
         end
-
-        if isnothing(levels)
-            levels = range(minimum(vardat), maximum(vardat), length=20)
-            levels = filter(!=(0), levels)
-        end
-
-        x = var.dims["x"]; # Index 540 for 45 degree north
-        z_ref = var.dims["z_reference"];
-        xvariable = x
-        yvariable = z_ref
-
-        subtitle = string(dir_names[fig_iy]) * " ($short_name)"
-        plt = panel(fig_ix, fig_iy, xvariable, yvariable, vardat, subtitle, levels; cmap=cmap)
+        
+        all_data[dir_idx] = (; var, vardat)
     end
-
+    
+    # Determine common levels for consistent coloring (symmetric around zero)
+    all_arrays = [all_data[i].vardat for i in 1:length(dir_paths)]
+    global_min = minimum(minimum.(all_arrays))
+    global_max = maximum(maximum.(all_arrays))
+    global_extreme = max(abs(global_min), abs(global_max))
+    levels = range(-global_extreme, global_extreme, length=20)
+    levels = filter(!=(0), levels)
+    
+    # Plot original data (first two rows)
+    plt = nothing
+    for (fig_iy, dir_path) in enumerate(dir_paths)
+        subtitle = dir_labels[fig_iy]
+        var_items = all_data[fig_iy]
+        
+        println("Row $(fig_iy): ", extrema(var_items.vardat))
+        
+        fig_items = (; fig, fig_ix = 1, fig_iy, fontsize)
+        plt = plot_panel_2d(fig_items, var_items, subtitle, levels, short_name)
+    end
+    
+    # Add colorbar for original data (with proper spacing)
+    unit_label = get_unit_label(short_name)
     Colorbar(
-    fig[fig_ix, length(dirs)+1],
-    plt,
-    label = "$short_name [m/s]",
-    labelsize = fontsize,
-    ticklabelsize = fontsize,
+        fig[1:2, 2],  # Move to column 3 to avoid overlap
+        plt,
+        label = "$(short_name) $(unit_label)",
+        labelsize = fontsize,
+        ticklabelsize = fontsize,
     )
+    
+    # Plot differences (third row)
+    @show "Plotting $(short_name) Differences between methods"
+    
+    # Calculate difference: method 2 - method 1
+    var_items_1 = all_data[1]  # First directory
+    var_items_2 = all_data[2]  # Second directory
+    
+    vardat_diff = var_items_2.vardat - var_items_1.vardat
+    diff_extreme = max(abs(minimum(vardat_diff)), abs(maximum(vardat_diff)))
+    diff_levels = range(-diff_extreme, diff_extreme, length=20)
+    
+    println("Difference Row: ", extrema(vardat_diff))
+    
+    # Create modified var_items for difference plot
+    diff_var_items = (; var = var_items_1.var, vardat = vardat_diff)
+    fig_items = (; fig, fig_ix = 1, fig_iy = 3, fontsize)  # Third row
+    subtitle = "Difference ($(dir_labels[2]) - $(dir_labels[1]))"
+    diff_plt = plot_panel_2d(fig_items, diff_var_items, subtitle, diff_levels, short_name, is_difference=true)
+    
+    # Add colorbar for differences (with proper spacing)
+    Colorbar(
+        fig[3, 2],  # Move to column 3 to avoid overlap
+        diff_plt,
+        label = "$(short_name) Difference $(unit_label)",
+        labelsize = fontsize,
+        ticklabelsize = fontsize,
+    )
+    
+    return fig
 end
 
-function panel(fig_ix, fig_iy, xvariable, yvariable, var, title, levels; cmap=nothing)
+function get_unit_label(short_name)
+    # Map variable names to appropriate units
+    unit_map = Dict(
+        "ua" => "[m/s]",
+        "wa" => "[m/s]",
+        "va" => "[m/s]",
+        "pfull" => "[Pa]",
+        "ta" => "[K]",
+        "theta" => "[K]",
+        "uaerror" => "[m/s]",
+        "waerror" => "[m/s]"
+    )
+    return get(unit_map, short_name, "[units]")
+end
+
+function plot_panel_2d(fig_items, var_items, subtitle, levels, short_name; is_difference=false)
+    (;fig, fig_ix, fig_iy, fontsize) = fig_items
+    (;var, vardat) = var_items
+    
+    # Spatial Data
+    x = var.dims["x"]
+    z_ref = var.dims["z_reference"]
+    xvariable = x
+    yvariable = z_ref
+    
+    # Create title
+    title_text = subtitle
+    
     ax = Axis(
-        fig[fig_ix, fig_iy],
-        title = title,
+        fig[fig_iy, fig_ix],
+        title = title_text,
         xlabel = "x [m]",
         ylabel = "z [m]",
         titlesize = fontsize,
@@ -69,49 +135,50 @@ function panel(fig_ix, fig_iy, xvariable, yvariable, var, title, levels; cmap=no
         xgridvisible = false,
         ygridvisible = false,
     )
-
-    if isnothing(cmap)
-        cmap = :viridis
-    end
-
-    plt = CairoMakie.contourf!(
-        xvariable,
-        yvariable,
-        var,
-        levels = levels,
-        colormap = cmap,
+    
+    # Use different colorscheme for difference plots
+    if is_difference
+        plt = CairoMakie.contourf!(
+            xvariable,
+            yvariable,
+            vardat,
+            levels = levels,
+            colormap = Reverse(:RdBu),  # Red-Blue colormap for differences (reversed)
         )
-
+    else
+        plt = CairoMakie.contourf!(
+            xvariable,
+            yvariable,
+            vardat,
+            levels = levels,
+            colormap = Reverse(:RdBu),  # Use symmetric colormap for all plots
+        )
+    end
+    
     tightlimits!(ax)
-
+    
     return plt
 end
 
+# Usage examples
+dir_labels = ("Old PGF", "Exner PGF + ref. prof.")
+dir_paths = (dir_old_pgf, dir_exner_ref_pgf)
 
-@show "Plotting"
-figsize = (1500, 1000);
-fig = Figure(; size = figsize)
-fig[0, 1:3] = Label(fig, "Eastward wind, instantaneous time = 48 Hours", fontsize = 24)
+# Plot eastward wind
+@show "Plotting Eastward Wind"
+fig1 = get_dat_2d(dir_labels, dir_paths, "uaerror", "inst")
+fig1[0, 1:2] = Label(fig1, "Error in the eastward wind, instantaneous time = 6 Days", fontsize = 24)
+CairoMakie.save("ua_comparison_adapted.pdf", fig1)
 
-plt1 = gen_panel(1, dirs, dir_names, "ua", "inst")
-plt2 = gen_panel(2, dirs, dir_names, "uaerror", "inst"; cmap=:seaborn_colorblind6)
+# Plot upward air velocity
+@show "Plotting Upward Air Velocity"
+fig2 = get_dat_2d(dir_labels, dir_paths, "waerror", "inst")
+fig2[0, 1:2] = Label(fig2, "Error in the upward air velocity, instantaneous time = 6 Days", fontsize = 24)
+CairoMakie.save("wa_comparison_adapted.pdf", fig2)
 
-fig
-CairoMakie.save(
-    "ua_comparison.pdf",
-    fig,
-)
+# If you want to plot at a specific time index instead of the last time step:
+# fig3 = get_dat_2d(dir_labels, dir_paths, "ua", "inst"; time_idx=10)
 
-@show "Plotting"
-figsize = (1500, 1000);
-fig = Figure(; size = figsize)
-fig[0, 1:3] = Label(fig, "Upward air velocity, instantaneous time = 48 Hours", fontsize = 24)
-
-plt1 = gen_panel(1, dirs, dir_names, "wa", "inst")
-plt2 = gen_panel(2, dirs, dir_names, "waerror", "inst", cmap=:seaborn_colorblind6)
-
-fig
-CairoMakie.save(
-    "wa_comparison.pdf",
-    fig,
-)
+# You can also easily add more variables:
+# fig4 = get_dat_2d(dir_labels, dir_paths, "ta", "inst")  # temperature
+# fig5 = get_dat_2d(dir_labels, dir_paths, "pfull", "inst")  # pressure
