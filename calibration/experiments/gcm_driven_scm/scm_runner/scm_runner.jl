e7fdec5e07a4b3bd307c9e70e079f92903f61382
costachris <christopouloscosta@gmail.com>
2025-10-03 11:36:27 -0700

    Update runner and configs

--- calibration/experiments/gcm_driven_scm/scm_runner/scm_runner.jl
+++ calibration/experiments/gcm_driven_scm/scm_runner/scm_runner.jl
@@ -16,13 +16,7 @@ using LinearAlgebra
 include("../helper_funcs.jl")
 include("runner_helper.jl")
 
-# Load experiment configuration to find forcing TOML files
-const experiment_config_dict =
-    YAML.load_file(joinpath(@__DIR__, "..", "experiment_config.yml"))
-const calib_output_dir = experiment_config_dict["output_dir"] # Base directory for calibration outputs/configs
-
 using Distributed
-
 using ArgParse
 
 NUM_WORKERS = 10
@@ -34,23 +28,38 @@ NUM_WORKERS = 10
 function main()
     s = ArgParseSettings()
     @add_arg_table! s begin
-        "--run_output_dir"
-        help = "Directory to output SCM simulations"
+        "--cal_output_dir"
+        help = "Base calibration output directory that contains a 'configs' subfolder"
         required = true
 
         "--parameter_path"
         help = "Path to parameter TOML file"
         required = true
+
+        "--run_output_dir"
+        help = "Directory to output SCM simulations. If omitted, defaults to <cal_output_dir>/scm_runs/<basename(parameter_path)>"
+        required = false
     end
 
     args = parse_args(s)
 
-    base_config_path = "./model_config_prognostic_runner.yml"
-    run_output_dir = args["run_output_dir"]
-    parameter_path = args["parameter_path"]
+    cal_output_dir = abspath(args["cal_output_dir"]) # renamed from calib_output_dir
+    parameter_path = abspath(args["parameter_path"])
+
+    # Determine default run_output_dir if not provided
+    if haskey(args, "run_output_dir") && !isnothing(args["run_output_dir"]) 
+        run_output_dir = abspath(args["run_output_dir"]) 
+    else
+        param_base = splitext(basename(parameter_path))[1] # strip .toml
+        run_output_dir = joinpath(cal_output_dir, "scm_runs", param_base)
+    end
+
+    base_config_path = joinpath(cal_output_dir, "configs", "model_config.yml")
+
+    mkpath(run_output_dir)
 
     all_configs =
-        generate_atmos_configs(base_config_path, parameter_path, run_output_dir)
+        generate_atmos_configs(base_config_path, parameter_path, run_output_dir, cal_output_dir)
     run_all_simluations(all_configs)
 end
 
@@ -58,11 +67,16 @@ function generate_atmos_configs(
     base_config_path::String,
     parameter_path::String,
     run_output_dir::String,
+    cal_output_dir::String,
 )
     config_dict = YAML.load_file(base_config_path)
     config_dict["output_dir"] = run_output_dir
     config_dict["toml"] = [abspath(parameter_path)]
     config_dict["output_default_diagnostics"] = false
+
+    # Load experiment config from cal_output_dir/configs
+    exp_config_path = joinpath(cal_output_dir, "configs", "experiment_config.yml")
+    experiment_config_dict = isfile(exp_config_path) ? YAML.load_file(exp_config_path) : Dict{String,Any}()
 
     ref_paths, cfsite_numbers = get_les_calibration_library_runner()
     num_cases = length(ref_paths)
@@ -80,7 +94,6 @@ function generate_atmos_configs(
             run_output_dir,
             "cfsite_$(cfsite_number)_$(forcing_model)_$(experiment)_$(month)",
         )
-        # config["external_forcing_type"] = get_cfsite_type(i, cfsite_numbers) # Removed: No longer used
 
         # Add forcing-specific TOML file if specified
         forcing_type = get_cfsite_type(i, cfsite_numbers)
@@ -90,21 +103,27 @@ function generate_atmos_configs(
         )
             forcing_config_file =
                 experiment_config_dict["forcing_toml_files"][forcing_type]
-            # Assume forcing configs are in a 'configs' subdir relative to the main calibration output dir
-            forcing_config_path =
-                joinpath(calib_output_dir, "configs", forcing_config_file)
+            # Prefer configs/scm_tomls/<file>, fallback to configs/<file>
+            forcing_path_candidate_1 = joinpath(cal_output_dir, "configs", "scm_tomls", forcing_config_file)
+            forcing_path_candidate_2 = joinpath(cal_output_dir, "configs", forcing_config_file)
+            forcing_config_path = if isfile(forcing_path_candidate_1)
+                forcing_path_candidate_1
+            else
+                forcing_path_candidate_2
+            end
 
             if isfile(forcing_config_path)
                 forcing_config_path = abspath(forcing_config_path)
-                # config["toml"] should already exist and contain parameter_path
                 push!(config["toml"], forcing_config_path)
                 @info "Added forcing config: $forcing_config_path for case $i (type: $forcing_type)"
             else
-                @warn "Forcing config file not found at $forcing_config_path for case $i (type: $forcing_type)."
+                @warn "Forcing config file not found for case $i (type: $forcing_type). Looked at $forcing_path_candidate_1 and $forcing_path_candidate_2."
             end
         else
             @warn "No forcing config file specified in experiment_config.yml for type '$forcing_type' for case $i."
         end
+
+        @info "Prepared SCM config for case $i" output_dir=config["output_dir"] toml=config["toml"] forcing_type=forcing_type cfsite=config["cfsite_number"]
 
         comms_ctx = ClimaComms.SingletonCommsContext()
         CA.AtmosConfig(config; comms_ctx)
