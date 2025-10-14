5653c34a60660b58fd153924c636db0e5334aab0
nefrathenrici <nat.henrici@gmail.com>
2025-10-14 12:09:07 -0600

    update paths

--- src/solver/type_getters.jl
+++ src/solver/type_getters.jl
@@ -762,7 +762,6 @@ function get_simulation(config::AtmosConfig)
     CP.log_parameter_information(
         config.toml_dict,
         joinpath(output_dir, "$(job_id)_parameters.toml"),
-        strict = true,
     )
     YAML.write_file(joinpath(output_dir, "$job_id.yml"), config.parsed_args)
 
