32a501c4b56d52a95a521b4cc7af2e0f16cfc7bf
Akshay Sridhar <asridhar@clima.gps.caltech.edu>
2025-10-28 17:16:22 -0700

    modified:   src/surface_conditions/surface_conditions.jl

--- src/surface_conditions/surface_conditions.jl
+++ src/surface_conditions/surface_conditions.jl
@@ -277,6 +277,7 @@ function surface_state_to_conditions(
                 parameterization.z0b,
                 gustiness,
                 beta,
+                SF.CharnockRoughness()
             )
         else
             if parameterization.fluxes isa HeatFluxes
