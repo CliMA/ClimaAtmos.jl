91116e9306398a197a420fca1b4ba6136e294548
Dave Rumph <60712318+daverumph@users.noreply.github.com>
2025-11-10 16:55:03 -0800

    Update src/prognostic_equations/implicit/manual_sparse_jacobian.jl
    Co-authored-by: Teja Reddy <treddy0205@gmail.com>

--- src/prognostic_equations/implicit/manual_sparse_jacobian.jl
+++ src/prognostic_equations/implicit/manual_sparse_jacobian.jl
@@ -626,7 +626,7 @@ function update_jacobian!(alg::ManualSparseJacobian, cache, Y, p, dtγ, t)
             ᶜK_u = ᶜK_h
         elseif vertical_diffusion isa VerticalDiffusion
             ᶜK_h .= ᶜcompute_eddy_diffusivity_coefficient(Y.c.uₕ, ᶜp, vertical_diffusion)
-            ᶜK_u .= ᶜK_h
+            ᶜK_u = ᶜK_h
         elseif is_smagorinsky_vertical(smagorinsky_lilly)
             set_smagorinsky_lilly_precomputed_quantities!(Y, p, smagorinsky_lilly)
             ᶜK_u .= p.precomputed.ᶜνₜ_v
