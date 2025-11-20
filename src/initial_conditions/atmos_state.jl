7a75dfb56aef02bab7b8de308cc8bfe8291a039b
jefftyoder <jeffreyyoder@unr.edu>
2025-11-19 16:34:03 -0800

    begin implementation of EisenmanIceModel logic

--- src/initial_conditions/atmos_state.jl
+++ src/initial_conditions/atmos_state.jl
@@ -82,7 +82,7 @@ function atmos_surface_field(surface_space, ::SlabOceanSST)
             sfc = map(
                 coord -> (;
                     T = Geometry.float_type(coord)(
-                        271 + 29 * exp(-coord.lat^2 / (2 * 26^2)), # changed for use in Larcform1 case
+                        271 + 29 * exp(-coord.lat^2 / (2 * 26^2)), 
                     ),
                     water = Geometry.float_type(coord)(0),
                 ),
