0a6c2ceac8f13bb45eebcb4be80aa39a24a94e4c
Tapio Schneider <tapio@caltech.edu>
2026-01-19 19:31:04 -0800

    Fix precip test error

--- test/parameterized_tendencies/microphysics/precipitation.jl
+++ test/parameterized_tendencies/microphysics/precipitation.jl
@@ -156,7 +156,7 @@ end
         @test !any(isnan, ᶜYₜ.c.ρq_liq)
         @test !any(isnan, ᶜYₜ.c.ρq_ice)
 
-        @test ᶜYₜ.c.ρq_tot == FT(0)
+        @test all(iszero, ᶜYₜ.c.ρq_tot)
 
         # Cloud fraction bounds
         test_cloud_fraction_bounds(p, FT)
@@ -214,7 +214,7 @@ end
         @test !any(isnan, ᶜYₜ.c.ρq_ice)
         @test !any(isnan, ᶜYₜ.c.ρn_liq)
 
-        @test ᶜYₜ.c.ρq_tot == FT(0)
+        @test all(iszero, ᶜYₜ.c.ρq_tot)
 
         # Cloud fraction bounds
         test_cloud_fraction_bounds(p, FT)
