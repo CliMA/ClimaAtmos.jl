ef3804cec063c2e8d131937eead96e35d514277a
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2026-01-29 11:53:43 -0800

    Add functionality to change compute schedule (#4243)
    * Refactor frequency to schedule
    
    * Add functionality to change compute schedule

--- src/callbacks/get_callbacks.jl
+++ src/callbacks/get_callbacks.jl
@@ -109,33 +109,10 @@ function get_diagnostics(
                 error("period keyword required for diagnostics")
 
             period_str = yaml_diag["period"]
-
-            if occursin("months", period_str)
-                months = match(r"^(\d+)months$", period_str)
-                isnothing(months) && error(
-                    "$(period_str) has to be of the form <NUM>months, e.g. 2months for 2 months",
-                )
-                period_dates = Dates.Month(parse(Int, first(months)))
-            else
-                period_seconds = FT(time_to_seconds(period_str))
-                period_dates =
-                    CA.promote_period.(Dates.Second(period_seconds))
-            end
-
-            date_last =
-                t_start isa ITime ?
-                ClimaUtilities.TimeManager.date(t_start) :
-                start_date + Dates.Second(t_start)
-            output_schedule = CAD.EveryCalendarDtSchedule(
-                period_dates;
-                reference_date = start_date,
-                date_last = date_last,
-            )
-            compute_schedule = CAD.EveryCalendarDtSchedule(
-                period_dates;
-                reference_date = start_date,
-                date_last = date_last,
-            )
+            output_schedule =
+                parse_frequency_to_schedule(FT, period_str, start_date, t_start)
+            compute_schedule =
+                parse_frequency_to_schedule(FT, period_str, start_date, t_start)
 
             if isnothing(output_name)
                 output_short_name = CAD.descriptive_short_name(
@@ -148,8 +125,12 @@ function get_diagnostics(
 
             if isnothing(reduction_time_func)
                 compute_every = compute_schedule
-            else
+            elseif !("compute_every" in keys(yaml_diag))
                 compute_every = CAD.EveryStepSchedule()
+            else
+                compute_every_str = yaml_diag["compute_every"]
+                compute_every =
+                    parse_frequency_to_schedule(FT, compute_every_str, start_date, t_start)
             end
 
             CAD.ScheduledDiagnostic(
@@ -199,6 +180,55 @@ function get_diagnostics(
     end
 
     return diagnostics, writers, periods_reductions
+end
+
+"""
+    parse_frequency_to_schedule(
+        ::Type{FT},
+        frequency_str,
+        start_date,
+        t_start,
+    )
+
+Parse a frequency (e.g. "3months", "2steps", "10mins") into a schedule for
+diagnostics.
+"""
+function parse_frequency_to_schedule(
+    ::Type{FT},
+    frequency_str,
+    start_date,
+    t_start,
+) where {FT}
+    if occursin("steps", frequency_str)
+        steps = match(r"^(\d+)steps$", frequency_str)
+        isnothing(steps) && error(
+            "$(frequency_str) has to be of the form <NUM>steps, e.g. 2steps for 2 steps",
+        )
+        steps = parse(Int, first(steps))
+        return CAD.DivisorSchedule(steps)
+    end
+
+    if occursin("months", frequency_str)
+        months = match(r"^(\d+)months$", frequency_str)
+        isnothing(months) && error(
+            "$(frequency_str) has to be of the form <NUM>months, e.g. 2months for 2 months",
+        )
+        period_dates = Dates.Month(parse(Int, first(months)))
+    else
+        period_seconds = FT(time_to_seconds(frequency_str))
+        period_dates =
+            CA.promote_period.(Dates.Second(period_seconds))
+    end
+
+    date_last =
+        t_start isa ITime ?
+        ClimaUtilities.TimeManager.date(t_start) :
+        start_date + Dates.Second(t_start)
+    return CAD.EveryCalendarDtSchedule(
+        period_dates;
+        reference_date = start_date,
+        date_last = date_last,
+    )
 end
 
 function parse_checkpoint_frequency(period::Number)
