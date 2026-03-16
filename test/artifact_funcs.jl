8344e0c25d8c7fe060aa68f85b2cb921c171665a
nefrathenrici <nat.henrici@gmail.com>
2026-03-16 13:52:57 -0700

    Remove unneeded dependencies

--- test/artifact_funcs.jl
+++ test/artifact_funcs.jl
@@ -1,63 +1,61 @@
-import ArtifactWrappers as AW
+import Downloads
+
+const _artifact_cache = joinpath(tempdir(), "climaatmos_test_artifacts")
+
+"""
+    _download_artifact(name, url, filename)
+
+Download a file from `url` into a cached directory under `name/`.
+Returns the full path to the downloaded file.
+"""
+function _download_artifact(name, url, filename)
+    dir = joinpath(_artifact_cache, name)
+    filepath = joinpath(dir, filename)
+    if !isfile(filepath)
+        mkpath(dir)
+        Downloads.download(url, filepath)
+    end
+    return filepath
+end
 
 # ERA5 data 1973 Jan: downloaded from Caltech box
 function era_global_dataset_path()
-    era_dataset = AW.ArtifactWrapper(
-        @__DIR__,
+    return _download_artifact(
         "era-global",
-        AW.ArtifactFile[AW.ArtifactFile(
-            url = "https://caltech.box.com/shared/static/2489yvlwhnxsrl05jvqnch7fcrd0k1vw.nc",
-            filename = "box-era5-monthly.nc",
-        ),],
+        "https://caltech.box.com/shared/static/2489yvlwhnxsrl05jvqnch7fcrd0k1vw.nc",
+        "box-era5-monthly.nc",
     )
-    return AW.get_data_folder(era_dataset)
 end
 
 # download ERA5 nc data: wind, temperature, geopotential height
 function era_single_column_dataset_path()
-    era_dataset = AW.ArtifactWrapper(
-        @__DIR__,
+    return _download_artifact(
         "era-single-column",
-        AW.ArtifactFile[AW.ArtifactFile(
-            url = "https://caltech.box.com/shared/static/of5wi39o643a333yy9vbx5pnf0za503g.nc",
-            filename = "box-single_column_test.nc",
-        ),],
+        "https://caltech.box.com/shared/static/of5wi39o643a333yy9vbx5pnf0za503g.nc",
+        "box-single_column_test.nc",
     )
-    return AW.get_data_folder(era_dataset)
 end
 
 function topo_res_path()
-    topo_data = AW.ArtifactWrapper(
-        @__DIR__,
+    return _download_artifact(
         "topo-info",
-        AW.ArtifactFile[AW.ArtifactFile(
-            url = "https://caltech.box.com/shared/static/isa7l4ow4xvv9vs09bivdwttbnnw5tte.nc",
-            filename = "topo_drag.res.nc",
-        ),],
+        "https://caltech.box.com/shared/static/isa7l4ow4xvv9vs09bivdwttbnnw5tte.nc",
+        "topo_drag.res.nc",
     )
-    return AW.get_data_folder(topo_data)
 end
 
 function mima_gwf_path()
-    mima_data = AW.ArtifactWrapper(
-        @__DIR__,
+    return _download_artifact(
         "mima-gwf",
-        AW.ArtifactFile[AW.ArtifactFile(
-            url = "https://caltech.box.com/shared/static/6r566rv4631ibfbr5p5vtv4ls7vl5fge.nc",
-            filename = "mima_gwf.nc",
-        ),],
+        "https://caltech.box.com/shared/static/6r566rv4631ibfbr5p5vtv4ls7vl5fge.nc",
+        "mima_gwf.nc",
     )
-    return AW.get_data_folder(mima_data)
 end
 
 function gfdl_ogw_data_path()
-    gfdl_data = AW.ArtifactWrapper(
-        @__DIR__,
+    return _download_artifact(
         "gfdl-ogw-data",
-        AW.ArtifactFile[AW.ArtifactFile(
-            url = "https://caltech.box.com/shared/static/zubipz298q5ar5rpfais8c0ymtfyz2oc.nc",
-            filename = "gfdl_ogw.nc",
-        ),],
+        "https://caltech.box.com/shared/static/zubipz298q5ar5rpfais8c0ymtfyz2oc.nc",
+        "gfdl_ogw.nc",
     )
-    return AW.get_data_folder(gfdl_data)
 end
