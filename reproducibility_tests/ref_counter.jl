e932b59bc1c2ca136262e324afbd5465fb9ba895
Nat Efrat-Henrici <60049837+nefrathenrici@users.noreply.github.com>
2026-03-20 13:53:44 -0400

    Downgrade to 1.11 (#4374)

--- reproducibility_tests/ref_counter.jl
+++ reproducibility_tests/ref_counter.jl
@@ -1,4 +1,4 @@
-327
+328
 
 # **README**
 #
@@ -32,6 +32,9 @@
 # 3) (optional) leave a link to the buildkite run that prompted this ref counter bump.
 
 #=
+328
+- Downgrade to 1.11 due to CUDA misaligned memory error
+
 327
 - Refactor reproducibility infrastructure to use rms deviations in prognostic variables and only fail when rms deviation exceeds tolerance. 
 - Needs ref_counter update because reference table changes to rmse rather than mse. 
