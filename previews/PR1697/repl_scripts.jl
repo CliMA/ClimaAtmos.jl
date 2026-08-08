acdab813dbc19eb8c5ceba969d6d2d4cb5d72d1e
Documenter.jl <documenter@juliadocs.github.io>
2026-08-08 03:47:07 +0000

    build based on 1db5bdc

---
+++ previews/PR1697/repl_scripts.jl
@@ -0,0 +1,19 @@
+const ca_dir = joinpath(@__DIR__, "..", "..")
+include(joinpath(ca_dir, "src", "utils", "cli_options.jl"))
+include(joinpath(ca_dir, "src", "utils", "yaml_helper.jl"))
+using PrettyTables
+s = argparse_settings();
+parsed_args = parse_commandline(s);
+
+buildkite_yaml = joinpath(ca_dir, ".buildkite", "pipeline.yml");
+buildkite_commands =
+    commands_from_yaml(buildkite_yaml; filter_name = "driver.jl")
+@assert length(buildkite_commands) > 0
+
+buildkite_flags = Dict()
+for bkcs in buildkite_commands
+    job_id = first(split(last(split(bkcs, "--job_id ")), " "))
+    println("### Buildkite job `$job_id`")
+    print_repl_script(bkcs)
+    println()
+end
