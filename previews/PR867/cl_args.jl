a29359bce899488134299b67e30ae4605a484026
Documenter.jl <documenter@juliadocs.github.io>
2026-09-05 04:10:06 +0000

    build based on b786ffe

---
+++ previews/PR867/cl_args.jl
@@ -0,0 +1,17 @@
+const ca_dir = joinpath(@__DIR__, "..", "..")
+include(joinpath(ca_dir, "examples", "hybrid", "cli_options.jl"))
+using PrettyTables
+(s, parsed_args) = parse_commandline();
+flags = map(arg -> arg.dest_name, s.args_table.fields)
+flag_types = map(arg -> string(arg.arg_type), s.args_table.fields)
+flag_help = map(arg -> arg.help, s.args_table.fields)
+data = hcat(flags, flag_types, flag_help)
+
+pretty_table(
+    data;
+    title = "Command line arguments",
+    header = ["flag", "type", "help msg"],
+    alignment = :l,
+    crop = :none,
+)
+nothing
